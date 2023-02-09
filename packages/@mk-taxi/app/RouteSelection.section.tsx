623857d5b07bbaf69889cbe01da6ab80620f5d79
Mladen Knezović <mladen.knezovic.1993@gmail.com>
2023-02-09 11:25:17 +0100

    +

--- packages/@mk-taxi/app/RouteSelection.section.tsx
+++ packages/@mk-taxi/app/RouteSelection.section.tsx
@@ -1,7 +1,23 @@
+import { GoogleMap, Marker, LoadScript } from "@react-google-maps/api"
+
+
 export default function RouteSelection_section(){
   return (
-    <div>
-      RouteSelection
-    </div>
+    <LoadScript googleMapsApiKey="AIzaSyAlZmjA7GGwjG2A6b2lo6RmWE5FbIKu8eQ">
+      <GoogleMap
+        mapContainerStyle={{
+          width: '900px',
+          height: '600px'
+        }}
+        center={ {
+          lat: -3.745,
+          lng: -38.523
+        }}
+        zoom={10}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <Marker position={{ lat: -34.397, lng: 150.644 }} />
+      </GoogleMap>
+    </LoadScript>
   )
 }
