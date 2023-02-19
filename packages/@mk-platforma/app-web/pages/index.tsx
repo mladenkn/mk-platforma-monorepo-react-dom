e8a035c5cf2f5cc64c618dc616e8f9bbec4dec0e
Mladen Knezović <mladen.knezovic.1993@gmail.com>
2023-02-19 18:47:02 +0100

    renamed to mk-platforma

---
+++ packages/@mk-platforma/app-web/pages/index.tsx
@@ -0,0 +1,5 @@
+import RouteSelection_section from '@mk-platforma/app/RouteSelection.section'
+
+export default function Home() {
+  return <RouteSelection_section />
+}
--- packages/@mk-taxi/app-web/pages/index.tsx
+++
@@ -1,5 +0,0 @@
-import RouteSelection_section from '@mk-taxi/app/RouteSelection.section'
-
-export default function Home() {
-  return <RouteSelection_section />
-}
