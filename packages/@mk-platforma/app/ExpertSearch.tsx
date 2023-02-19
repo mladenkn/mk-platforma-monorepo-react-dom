7a9506faa989dc0e7e8c47ee7e41e8601cb2245f
Mladen Knezović <mladen.knezovic.1993@gmail.com>
2023-02-20 00:24:09 +0100

    +

--- packages/@mk-platforma/app/ExpertSearch.tsx
+++ packages/@mk-platforma/app/ExpertSearch.tsx
@@ -1,42 +1,48 @@
 import { Input, Box, Typography, List, ListItem, ListItemButton } from '@mui/joy'
 import SearchRounded from '@mui/icons-material/SearchRounded'
-import { ComponentProps } from 'react'
+import { ComponentProps, useState } from 'react'
 import LocationIcon from '@mui/icons-material/LocationOn'
 import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
 
 
 const experts = [
   {
+    id: 1,
     firstName: 'Mladen',
     lastName: 'Knezović',
     area: 'Split i okolica',
     occupations: ['računalni programer'],
   },
   {
+    id: 2,
     firstName: 'Ante',
     lastName: 'Klinčić',
     area: 'Split i okolica',
     occupations: ['odvjetnik'],
   },
   {
+    id: 3,
     firstName: 'Tomislav',
     lastName: 'Horaček',
     area: 'Zagreb',
     occupations: ['zidar'],
   },
   {
+    id: 4,
     firstName: 'Ante',
     lastName: 'Franić',
     area: 'Osijek',
     occupations: ['vodoinstalater'],
   },
   {
+    id: 5,
     firstName: 'Jure',
     lastName: 'Matinović',
     area: 'Osijek',
     occupations: ['fasader'],
   },
   // {
+  //   id: 6,
   //   firstName: 'Stipe',
   //   lastName: 'Jurić',
   //   area: 'Osijek',
@@ -45,6 +51,8 @@ const experts = [
 ]
 
 export default function ExpertSearch(){
+  const [selectedExpert, setSelectedExpert] = useState<number>()
+
   return (
     <Box>
       <Input
@@ -54,7 +62,7 @@ export default function ExpertSearch(){
       />
       <List sx={{ mt: 3, display: 'flex', flexDirection: 'column', gap: 2 }}>
         {experts.map(expert => (
-          <ListItemButton sx={{ mb: 2, }}>
+          <ListItemButton sx={{ mb: 2, }} onClick={() => setSelectedExpert(expert.id)}>
             <ListItem sx={{ flexDirection: 'column', alignItems: 'start' }}>
               <Text level="h2" sx={{ mb: 1 }}>{expert.firstName}{' '}{expert.lastName}</Text>
               <Box sx={{ display: 'flex', alignItems: 'center', mb: 0.3, }}>
