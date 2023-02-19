787be3aa54d6ef600f1844b098e663b09c8c78b6
Mladen Knezović <mladen.knezovic.1993@gmail.com>
2023-02-19 23:42:52 +0100

    +

--- packages/@mk-platforma/app/ExpertSearch.tsx
+++ packages/@mk-platforma/app/ExpertSearch.tsx
@@ -2,6 +2,34 @@ import { Input, Box } from '@mui/joy'
 import SearchRounded from '@mui/icons-material/SearchRounded'
 
 
+const experts = [
+  {
+    firstName: 'Mladen',
+    lastName: 'Knezović',
+    area: 'Split i okolica',
+  },
+  {
+    firstName: 'Ante',
+    lastName: 'Klinčić',
+    area: 'Split i okolica',
+  },
+  {
+    firstName: 'Tomislav',
+    lastName: 'Horaček',
+    area: 'Zagreb',
+  },
+  {
+    firstName: 'Ante',
+    lastName: 'Franić',
+    area: 'Osijek',
+  },
+  {
+    firstName: 'Jure',
+    lastName: 'Matinović',
+    area: 'Osijek',
+  },
+]
+
 export default function ExpertSearch(){
   return (
     <Box>
@@ -10,6 +38,14 @@ export default function ExpertSearch(){
         placeholder="Pretraži majstore"
         startDecorator={<SearchRounded />}
       />
+      <Box>
+        {experts.map(expert => (
+          <Box>
+            <Box>{expert.firstName}{' '}{expert.lastName}</Box>
+            <Box>{expert.area}</Box>
+          </Box>
+        ))}
+      </Box>
     </Box>
   )
 }
