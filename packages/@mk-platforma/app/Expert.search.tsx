1babf95e7147923782aa05177fd4b8917e3c0528
Mladen Knezović <mladen.knezovic.1993@gmail.com>
2023-02-20 21:39:32 +0100

    use-accordition +

--- packages/@mk-platforma/app/Expert.search.tsx
+++ packages/@mk-platforma/app/Expert.search.tsx
@@ -13,46 +13,49 @@ export default function Expert_search(){
   const selectedExpert = _selectedExpert ? asNonNil(data.experts.find(e => e.id === _selectedExpert)) : undefined
 
   return (
-    <Box sx={{ px: 4, pt: 10, display: 'flex', alignItems: 'start' }}>
-      <Input
-        sx={{ pb: 0.7, ml: 1, mb: 3, mt: 2, width: 350 }}
-        autoFocus
-        placeholder="Pretraži majstore"
-        startAdornment={<SearchRounded sx={{ mr: 2 }} />}
-      />
-      <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, maxWidth: 700, ml: 10 }}>
-        {data.experts.map(expert => (
-          <Accordion
-            expanded={expert.id === selectedExpert?.id}
-            onChange={(e, isExpanded) => setSelectedExpert(isExpanded ? expert.id : undefined)}
-          >
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel1bh-content"
-              id="panel1bh-header"
+    <Box sx={{ px: 4, pt: 10, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <Box>
+        <Input
+          sx={{ pb: 0.7, mb: 3, width: 350 }}
+          autoFocus
+          placeholder="Pretraži majstore"
+          startAdornment={<SearchRounded sx={{ mr: 2 }} />}
+        />
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, maxWidth: 700, }}>
+          {data.experts.map(expert => (
+            <Accordion
+              expanded={expert.id === selectedExpert?.id}
+              onChange={(e, isExpanded) => setSelectedExpert(isExpanded ? expert.id : undefined)}
             >
-              <Box>
-                <Typography sx={{ fontWeight: 600, fontSize: 16 }}>
-                  {expert.firstName}{' '}{expert.lastName}
-                </Typography>
-                <Box sx={{ mt: 1.2, ml: 0.5, }}>
-                  <Box sx={{ display: 'flex', alignItems: 'center', mb: 0.3, color: 'text.secondary' }}>
-                    <LocationIcon style={{ width: 17, height: 17 }} sx={{ mr: 1, }} />
-                    <Typography>{expert.area}</Typography>
-                  </Box>
-                  <Box sx={{ display: 'flex', alignItems: 'center', color: 'text.secondary' }}>
-                    <HandymanIcon style={{ width: 17, height: 17 }} sx={{ mr: 1 }} />
-                    <Typography>{expert.occupations.join(', ')}</Typography>
-                  </Box>
-                </Box>             
-              </Box>
-            </AccordionSummary>
-            <AccordionDetails sx={{ color: 'text.secondary' }}>
-              <Typography>{expert.description}</Typography>
-              <Typography sx={{ mt: 4 }}>Mobitel/telefon: {expert.phone}</Typography>                  
-            </AccordionDetails>
-          </Accordion>
-        ))}
+              <AccordionSummary
+                sx={{ p: 0 }}
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1bh-content"
+                id="panel1bh-header"
+              >
+                <Box>
+                  <Typography sx={{ fontWeight: 600, fontSize: 16 }}>
+                    {expert.firstName}{' '}{expert.lastName}
+                  </Typography>
+                  <Box sx={{ mt: 1.2, ml: 0.5, }}>
+                    <Box sx={{ display: 'flex', alignItems: 'center', mb: 0.3, color: 'text.secondary' }}>
+                      <LocationIcon style={{ width: 17, height: 17 }} sx={{ mr: 1, }} />
+                      <Typography>{expert.area}</Typography>
+                    </Box>
+                    <Box sx={{ display: 'flex', alignItems: 'center', color: 'text.secondary' }}>
+                      <HandymanIcon style={{ width: 17, height: 17 }} sx={{ mr: 1 }} />
+                      <Typography>{expert.occupations.join(', ')}</Typography>
+                    </Box>
+                  </Box>             
+                </Box>
+              </AccordionSummary>
+              <AccordionDetails sx={{ color: 'text.secondary' }}>
+                <Typography>{expert.description}</Typography>
+                <Typography sx={{ mt: 4 }}>Mobitel/telefon: {expert.phone}</Typography>                  
+              </AccordionDetails>
+            </Accordion>
+          ))}
+        </Box>
       </Box>
     </Box>
   )
