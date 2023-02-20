41d173e664bf9ca8aa134c17de676bf148c7745b
Mladen Knezović <mladen.knezovic.1993@gmail.com>
2023-02-20 21:29:35 +0100

    use-accordition +

--- packages/@mk-platforma/app/Expert.search.tsx
+++ packages/@mk-platforma/app/Expert.search.tsx
@@ -20,7 +20,7 @@ export default function Expert_search(){
         placeholder="Pretraži majstore"
         startAdornment={<SearchRounded sx={{ mr: 2 }} />}
       />
-      <Box sx={{ mt: 3, display: 'flex', flexDirection: 'column', gap: 2, maxWidth: 800 }}>
+      <Box sx={{ mt: 3, display: 'flex', flexDirection: 'column', gap: 2, maxWidth: 450 }}>
         {data.experts.map(expert => (
           <Accordion
             expanded={expert.id === selectedExpert?.id}
@@ -31,18 +31,20 @@ export default function Expert_search(){
               aria-controls="panel1bh-content"
               id="panel1bh-header"
             >
-              <Typography sx={{ width: '33%', flexShrink: 0 }}>
-                {expert.firstName}{' '}{expert.lastName}
-              </Typography>
-              <Box sx={{  }}>
-                <Box sx={{ display: 'flex', alignItems: 'center', mb: 0.3, mr: 2, color: 'text.secondary' }}>
-                  <LocationIcon style={{ width: 17, height: 17 }} sx={{ mr: 1, }} />
-                  <Typography>{expert.area}</Typography>
-                </Box>
-                <Box sx={{ display: 'flex', alignItems: 'center', color: 'text.secondary' }}>
-                  <HandymanIcon style={{ width: 17, height: 17 }} sx={{ mr: 1 }} />
-                  <Typography>{expert.occupations.join(', ')}</Typography>
-                </Box>
+              <Box>
+                <Typography sx={{ }}>
+                  {expert.firstName}{' '}{expert.lastName}
+                </Typography>
+                <Box sx={{ mt: 2, ml: 0.5, }}>
+                  <Box sx={{ display: 'flex', alignItems: 'center', mb: 0.3, color: 'text.secondary' }}>
+                    <LocationIcon style={{ width: 17, height: 17 }} sx={{ mr: 1, }} />
+                    <Typography>{expert.area}</Typography>
+                  </Box>
+                  <Box sx={{ display: 'flex', alignItems: 'center', color: 'text.secondary' }}>
+                    <HandymanIcon style={{ width: 17, height: 17 }} sx={{ mr: 1 }} />
+                    <Typography>{expert.occupations.join(', ')}</Typography>
+                  </Box>
+                </Box>             
               </Box>
             </AccordionSummary>
             <AccordionDetails sx={{ color: 'text.secondary' }}>
