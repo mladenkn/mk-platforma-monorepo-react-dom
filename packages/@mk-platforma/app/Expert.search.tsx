b358066c34723474f2f3013630e1224fb95107e9
Mladen Knezović <mladen.knezovic.1993@gmail.com>
2023-02-20 21:24:37 +0100

    use-accordition +

--- packages/@mk-platforma/app/Expert.search.tsx
+++ packages/@mk-platforma/app/Expert.search.tsx
@@ -1,10 +1,11 @@
-import { Input, Box, Typography, List, ListItem, ListItemButton, } from '@mui/material'
+import { Input, Box, Typography, List, ListItem, ListItemButton, Accordion, AccordionSummary, AccordionDetails } from '@mui/material'
 import SearchRounded from '@mui/icons-material/SearchRounded'
 import { useState } from 'react'
 import LocationIcon from '@mui/icons-material/LocationOn'
 import HandymanIcon from '@mui/icons-material/Handyman'
 import { asNonNil } from "@mk-libs/common/common"
 import data from "./data.json"
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
 
 
 export default function Expert_search(){
@@ -12,39 +13,44 @@ export default function Expert_search(){
   const selectedExpert = _selectedExpert ? asNonNil(data.experts.find(e => e.id === _selectedExpert)) : undefined
 
   return (
-    <Box sx={{ display: 'flex' }}>
-      <Box sx={{ minWidth: 500 }}>
-        <Input
-          sx={{ ml: 4, pb: 1 }}
-          autoFocus
-          placeholder="Pretraži majstore"
-          startAdornment={<SearchRounded sx={{ mr: 2 }} />}
-        />
-        <List sx={{ mt: 3, display: 'flex', flexDirection: 'column', gap: 2 }}>
-          {data.experts.map(expert => (
-            <ListItemButton selected={expert.id === selectedExpert.id} key={expert.id} sx={{ mb: 2, }} onClick={() => setSelectedExpert(expert.id)}>
-              <ListItem sx={{ flexDirection: 'column', alignItems: 'start' }}>
-                <Typography fontWeight={600} fontSize="lg" sx={{ mb: 1 }}>{expert.firstName}{' '}{expert.lastName}</Typography>
-                <Box sx={{ display: 'flex', alignItems: 'center', mb: 0.3, }}>
-                  <LocationIcon sx={{ mr: 1 }} />
+    <Box sx={{ px: 4, pt: 4 }}>
+      <Input
+        sx={{ pb: 0.7, ml: 1, mb: 3 }}
+        autoFocus
+        placeholder="Pretraži majstore"
+        startAdornment={<SearchRounded sx={{ mr: 2 }} />}
+      />
+      <Box sx={{ mt: 3, display: 'flex', flexDirection: 'column', gap: 2, maxWidth: 800 }}>
+        {data.experts.map(expert => (
+          <Accordion
+            expanded={expert.id === selectedExpert?.id}
+            onChange={(e, isExpanded) => setSelectedExpert(isExpanded ? expert.id : undefined)}
+          >
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1bh-content"
+              id="panel1bh-header"
+            >
+              <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                {expert.firstName}{' '}{expert.lastName}
+              </Typography>
+              <Box sx={{  }}>
+                <Box sx={{ display: 'flex', alignItems: 'center', mb: 0.3, mr: 2, color: 'text.secondary' }}>
+                  <LocationIcon style={{ width: 17, height: 17 }} sx={{ mr: 1, }} />
                   <Typography>{expert.area}</Typography>
                 </Box>
-                <Box sx={{ display: 'flex', alignItems: 'center', }}>
-                  <HandymanIcon sx={{ mr: 1 }} />
+                <Box sx={{ display: 'flex', alignItems: 'center', color: 'text.secondary' }}>
+                  <HandymanIcon style={{ width: 17, height: 17 }} sx={{ mr: 1 }} />
                   <Typography>{expert.occupations.join(', ')}</Typography>
                 </Box>
-              </ListItem>
-            </ListItemButton>
-          ))}
-        </List>
-      </Box>
-      <Box sx={{ mt: 9, ml: 3, }}>
-        {selectedExpert && (
-          <>
-            <Typography>{selectedExpert.description}</Typography>
-            <Typography sx={{ mt: 4 }}>Mobitel/telefon: {selectedExpert.phone}</Typography>
-          </>
-        )}
+              </Box>
+            </AccordionSummary>
+            <AccordionDetails sx={{ color: 'text.secondary' }}>
+              <Typography>{expert.description}</Typography>
+              <Typography sx={{ mt: 4 }}>Mobitel/telefon: {expert.phone}</Typography>                  
+            </AccordionDetails>
+          </Accordion>
+        ))}
       </Box>
     </Box>
   )
