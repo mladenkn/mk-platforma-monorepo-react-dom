b19f4a83e14266341a584b3036f2165607a1a2d0
Mladen Knezović <mladen.knezovic.1993@gmail.com>
2023-02-21 23:47:09 +0100

    use-accordation +

--- packages/@mk-platforma/app/Expert.search.tsx
+++ packages/@mk-platforma/app/Expert.search.tsx
@@ -22,34 +22,37 @@ export default function Expert_search(){
           startAdornment={<SearchRounded sx={{ mr: 2 }} />}
         />
         <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, maxWidth: 700, }}>
-          {data.experts.map(expert => (
-            <Accordion
-              expanded={expert.id === selectedExpert?.id}
-              onChange={(e, isExpanded) => setSelectedExpert(isExpanded ? expert.id : undefined)}
-            >
-              <AccordionSummary sx={{ pl: 1.5 }} expandIcon={<ExpandMoreIcon />}>
-                <Box sx={{ display: 'flex' }}>
-                  <Typography sx={{ fontWeight: 600, fontSize: 16, width: 160 }}>
-                    {expert.firstName}{' '}{expert.lastName}
-                  </Typography>
-                  <Box sx={{ pl: 1, ml: 10 }}>
-                    <Box sx={{ display: 'flex', alignItems: 'center', mb: 0.3, color: 'text.secondary' }}>
-                      <LocationIcon style={{ width: 17, height: 17 }} sx={{ mr: 1, }} />
-                      <Typography>{expert.area}</Typography>
+          {data.experts.map(expert => {
+            const isExpanded = expert.id === selectedExpert?.id
+            return (
+              <Accordion
+                expanded={expert.id === selectedExpert?.id}
+                onChange={(e, isExpanded) => setSelectedExpert(isExpanded ? expert.id : undefined)}
+              >
+                <AccordionSummary sx={{ pl: 1.5 }} expandIcon={<ExpandMoreIcon />}>
+                  <Box sx={{ display: 'flex' }}>
+                    <Typography sx={{ fontWeight: 600, fontSize: 16, width: 160 }}>
+                      {expert.firstName}{' '}{expert.lastName}
+                    </Typography>
+                    <Box sx={{ pl: 1, ml: 10, color: !isExpanded ? 'text.secondary' : undefined }}>
+                      <Box sx={{ display: 'flex', alignItems: 'center', mb: 0.3, }}>
+                        <LocationIcon style={{ width: 17, height: 17 }} sx={{ mr: 1, }} />
+                        <Typography>{expert.area}</Typography>
+                      </Box>
+                      <Box sx={{ display: 'flex', alignItems: 'center', }}>
+                        <HandymanIcon style={{ width: 17, height: 17 }} sx={{ mr: 1 }} />
+                        <Typography>{expert.occupations.join(', ')}</Typography>
+                      </Box>
                     </Box>
-                    <Box sx={{ display: 'flex', alignItems: 'center', color: 'text.secondary', }}>
-                      <HandymanIcon style={{ width: 17, height: 17 }} sx={{ mr: 1 }} />
-                      <Typography>{expert.occupations.join(', ')}</Typography>
-                    </Box>
-                  </Box>             
-                </Box>
-              </AccordionSummary>
-              <AccordionDetails sx={{ color: 'text.secondary' }}>
-                <Typography>{expert.description}</Typography>
-                <Typography sx={{ mt: 4 }}>Mobitel/telefon: {expert.phone}</Typography>                  
-              </AccordionDetails>
-            </Accordion>
-          ))}
+                  </Box>
+                </AccordionSummary>
+                <AccordionDetails sx={{ color: !isExpanded ? 'text.secondary' : undefined }}>
+                  <Typography>{expert.description}</Typography>
+                  <Typography sx={{ mt: 4 }}>Mobitel/telefon: {expert.phone}</Typography>
+                </AccordionDetails>
+              </Accordion>
+            )
+          })}
         </Box>
       </Box>
     </Box>
