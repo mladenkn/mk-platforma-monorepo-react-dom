0521bb280961f74c5a6d4abe072bd2a200a46797
Mladen Knezović <mladen.knezovic.1993@gmail.com>
2023-02-23 21:45:19 +0100

    +

--- packages/@mk-platforma/app-web/pages/_app.tsx
+++ packages/@mk-platforma/app-web/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 import "../styles.global.css"
+import '@fontsource/public-sans' // TODO: in pages/app.tsx
 
 
 export default function AppRoot({ Component, pageProps }: AppProps) {
--- packages/@mk-platforma/app-web/pages/index.tsx
+++ packages/@mk-platforma/app-web/pages/index.tsx
@@ -1,4 +1,3 @@
 import Home_section from '@mk-platforma/app/Home.section'
-import '@fontsource/public-sans' // TODO: in pages/app.tsx
 
 export default Home_section
