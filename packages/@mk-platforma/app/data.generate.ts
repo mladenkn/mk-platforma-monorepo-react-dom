5035238158ae1914a6c4893056cb528c33f09cee
Mladen Knezović <mladen.knezovic.1993@gmail.com>
2023-02-24 15:53:09 +0100

    +

--- packages/@mk-platforma/app/data.generate.ts
+++ packages/@mk-platforma/app/data.generate.ts
@@ -1,7 +1,7 @@
 import { faker } from "@faker-js/faker"
 import { generateArray } from "@mk-libs/common/common"
 import { writeFileSync } from "fs"
-import * as cro_dataset from "./cro.dataset"
+import * as cro_dataset from "./data.cro.dataset"
 
 
 let expertId = 1
