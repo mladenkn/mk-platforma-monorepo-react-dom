5b601b6cb04501b870029b4814382c2f48a05404
Mladen Knezović <mladen.knezovic.1993@gmail.com>
2023-02-24 09:43:54 +0100

    +

--- packages/@mk-platforma/app/Home.section.tsx
+++ packages/@mk-platforma/app/Home.section.tsx
@@ -2,15 +2,16 @@ import Box from '@mui/material/Box'
 import Tabs from '@mui/material/Tabs'
 import Tab_ from '@mui/material/Tab'
 import { useState } from 'react'
-import { styled, Typography } from '@mui/material'
+import { styled } from '@mui/material'
 import Expert_search from './Expert.search'
 import Item_sale from './Item.sale'
+import Job_offers from './Job.offers'
 
 
 type Tab = 'buying-selling' | 'experts' | 'jobs'
 
 export default function Home_section() {
-  const [value, setValue] = useState<Tab>('experts')
+  const [value, setValue] = useState<Tab>('jobs')
 
   return (
     <Box sx={{ width: '100%', bgcolor: 'background.paper', display: 'flex', flexDirection: 'column', flex: 1, mb: 4, }}>
@@ -21,11 +22,7 @@ export default function Home_section() {
       </Tabs>
       {value === 'buying-selling' && <Item_sale />}
       {value === 'experts' && <Expert_search />}
-      {value === 'jobs' && (
-        <Box>
-          <Typography>jobs tab</Typography>
-        </Box>
-      )}
+      {value === 'jobs' && <Job_offers />}
     </Box>
   )
 }
---
+++ packages/@mk-platforma/app/Job.offers.tsx
@@ -0,0 +1,63 @@
+import { Input, Box, Typography, Accordion, AccordionSummary, AccordionDetails } from '@mui/material'
+import SearchRounded from '@mui/icons-material/SearchRounded'
+import LocationIcon from '@mui/icons-material/LocationOn'
+import data from "./data.json"
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
+import { useState } from 'react'
+import { asNonNil } from '@mk-libs/common/common'
+import { Layout1_list_sx, Layout1_root_sx } from './layout1'
+
+const { jobs } = data
+
+
+export default function Job_offers(){
+  const [_selectedItem, setSelectedItem] = useState<number>()
+  const selectedItem = _selectedItem ? asNonNil(jobs.find(e => e.id === _selectedItem)) : undefined
+
+  return (
+    <Box sx={Layout1_root_sx}>
+      <Input
+        sx={{ pb: 0.7, mb: 6, width: 350 }}
+        autoFocus
+        placeholder="Pretraži"
+        startAdornment={<SearchRounded sx={{ mr: 2 }} />}
+      />
+      <Box sx={Layout1_list_sx}>
+        {jobs.map(item => {
+          return (
+            <Accordion
+              key={item.id}
+              expanded={item.id === selectedItem?.id}
+              onChange={(e, isExpanded) => setSelectedItem(isExpanded ? item.id : undefined)}
+            >
+              <AccordionSummary sx={{ pl: 1.5 }} expandIcon={<ExpandMoreIcon />}>
+                <Box sx={{ display: 'flex', flex: 1 }}>
+                  <img src={item.photos[0]} width={100} height={100} />
+                  <Box sx={{ marginLeft: 3 }}>
+                    <Typography sx={{ fontWeight: 600, fontSize: 16 }}>
+                      {item.title}
+                    </Typography>
+                    <Box sx={{ color: 'text.secondary', mt: 2 }}>
+                      <Box sx={{ display: 'flex', alignItems: 'center', mb: 0.3, }}>
+                        <LocationIcon style={{ width: 17, height: 17 }} sx={{ mr: 1, }} />
+                        <Typography>{item.location}</Typography>
+                      </Box>
+                    </Box>
+                  </Box>
+                </Box>
+              </AccordionSummary>
+              <AccordionDetails sx={{ ml: 16, }}>
+                <Typography>
+                  {item.description}
+                </Typography>
+                <Typography sx={{ mt: 4 }}>
+                  Phone number: {item.adOwner.phoneNumber}
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+          )
+        })}
+      </Box>
+    </Box>
+  )
+}
