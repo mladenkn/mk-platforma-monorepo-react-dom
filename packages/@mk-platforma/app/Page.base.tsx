e7b08c82bb5b5a40774683498ea128637444df08
Mladen Knezović <mladen.knezovic.1993@gmail.com>
2023-02-25 12:05:11 +0100

    .

--- packages/@mk-platforma/app/Page.base.tsx
+++ packages/@mk-platforma/app/Page.base.tsx
@@ -34,7 +34,7 @@ export default function Page_base<TItem extends Item>({ pageRootProps, items, re
         {items.map(item => (
           <Paper
             key={item.id}
-            sx={{ p: 3, display: 'flex', flex: 1, width: 600, }}
+            sx={{ p: 3, display: 'flex', flex: 1, width: 600, cursor: 'pointer' }}
             onClick={() => setSelectedItem(item.id)}
           >
             {renderListItem(item)}
