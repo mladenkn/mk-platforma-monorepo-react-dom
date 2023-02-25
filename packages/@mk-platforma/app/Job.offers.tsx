8ffb3a7ba90c7405d424c05d3d5fee914a51f8d7
Mladen Knezović <mladen.knezovic.1993@gmail.com>
2023-02-25 10:59:05 +0100

    +

--- packages/@mk-platforma/app/Job.offers.tsx
+++ packages/@mk-platforma/app/Job.offers.tsx
@@ -1,7 +1,6 @@
-import { Box, Typography, Accordion, AccordionSummary, AccordionDetails } from "@mui/material"
+import { Box, Typography } from "@mui/material"
 import LocationIcon from "@mui/icons-material/LocationOn"
 import data from "./data.json"
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
 import Page_base from "./Page.base"
 
 const { jobs } = data
@@ -14,56 +13,45 @@ export default function Job_offers() {
         activeTab: 'jobs'
       }}
       items={jobs}
-      renderItem={(item, isSelected, setIsSelected,) => {
-        return (
-          <Accordion
-            key={item.id}
+      renderAccorditionListItem={(item, isSelected) => (
+        <Box sx={{ display: "flex", flexDirection: "column", flex: 1 }}>
+          <Typography sx={{ fontWeight: 600, fontSize: 16 }}>{item.label}</Typography>
+          <Box
             sx={{
-              maxWidth: 600,
+              display: "flex",
+              justifyContent: "space-between",
+              flex: 1,
+              marginTop: 3,
+              marginRight: 3,
             }}
-            expanded={isSelected}
-            onChange={(e, isSelected) => setIsSelected(isSelected)}
           >
-            <AccordionSummary sx={{ pl: 1.5, pb: 1 }} expandIcon={<ExpandMoreIcon />}>
-              <Box sx={{ display: "flex", flexDirection: "column", flex: 1 }}>
-                <Typography sx={{ fontWeight: 600, fontSize: 16 }}>{item.label}</Typography>
-                <Box
-                  sx={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    flex: 1,
-                    marginTop: 3,
-                    marginRight: 3,
-                  }}
-                >
-                  <Box sx={{ color: "text.secondary", mt: 1, ml: 1 }}>
-                    <Box sx={{ display: "flex", alignItems: "center", mb: 0.3 }}>
-                      <LocationIcon style={{ width: 17, height: 17 }} sx={{ mr: 1 }} />
-                      <Typography>{item.location}</Typography>
-                    </Box>
-                  </Box>
-                  {!isSelected && (
-                    <Box sx={{ display: "flex", gap: 1 }}>
-                      {item.photos.map((photo, index) => (
-                        <img key={index} width={65} height={65} src={photo} />
-                      ))}
-                    </Box>
-                  )}
-                </Box>
+            <Box sx={{ color: "text.secondary", mt: 1, ml: 1 }}>
+              <Box sx={{ display: "flex", alignItems: "center", mb: 0.3 }}>
+                <LocationIcon style={{ width: 17, height: 17 }} sx={{ mr: 1 }} />
+                <Typography>{item.location}</Typography>
               </Box>
-            </AccordionSummary>
-            <AccordionDetails sx={{ ml: 16, pb: 3 }}>
-              <Typography>{item.description}</Typography>
-              <Typography sx={{ mt: 4 }}>Phone number: {item.adOwner.phoneNumber}</Typography>
-              <Box sx={{ display: "flex", gap: 1, marginTop: 4 }}>
+            </Box>
+            {!isSelected && (
+              <Box sx={{ display: "flex", gap: 1 }}>
                 {item.photos.map((photo, index) => (
-                  <img key={index} width={75} height={75} src={photo} />
+                  <img key={index} width={65} height={65} src={photo} />
                 ))}
               </Box>
-            </AccordionDetails>
-          </Accordion>
-        )
-      }}
+            )}
+          </Box>
+        </Box>
+      )}
+      renderAccorditionDetails={item => (
+        <>
+          <Typography>{item.description}</Typography>
+          <Typography sx={{ mt: 4 }}>Phone number: {item.adOwner.phoneNumber}</Typography>
+          <Box sx={{ display: "flex", gap: 1, marginTop: 4 }}>
+            {item.photos.map((photo, index) => (
+              <img key={index} width={75} height={75} src={photo} />
+            ))}
+          </Box>
+        </>
+      )}
     />
   )
 }
--- packages/@mk-platforma/app/Page.base.tsx
+++ packages/@mk-platforma/app/Page.base.tsx
@@ -1,10 +1,10 @@
-import { asNonNil } from "@mk-libs/common/common"
 import { OverridableProps } from "@mk-libs/react-common/types"
 import { SearchRounded as SearchRoundedIcon } from "@mui/icons-material"
-import { Box, Input } from "@mui/material"
+import { Accordion, AccordionSummary, AccordionDetails, Box, Input } from "@mui/material"
 import { Layout1_list_sx } from "./layout1"
 import { ReactElement, useState } from "react"
 import { PageRoot } from "./common"
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
 
 
 type Item = {
@@ -14,10 +14,11 @@ type Item = {
 type Props<TItem extends Item> = {
   pageRootProps: OverridableProps<typeof PageRoot, "activeTab">
   items: TItem[]
-  renderItem(items: TItem, isSelected: boolean, setIsSelected: (a: boolean) => void): ReactElement
+  renderAccorditionListItem(item: TItem, isSelected: boolean): ReactElement
+  renderAccorditionDetails(item: TItem): ReactElement
 }
 
-export default function Page_base<TItem extends Item>({ pageRootProps, items, renderItem }: Props<TItem>){
+export default function Page_base<TItem extends Item>({ pageRootProps, items, renderAccorditionListItem, renderAccorditionDetails }: Props<TItem>){
   const [selectedItem, setSelectedItem] = useState<number>()
 
   return (
@@ -29,11 +30,26 @@ export default function Page_base<TItem extends Item>({ pageRootProps, items, re
         startAdornment={<SearchRoundedIcon sx={{ mr: 2 }} />}
       />
       <Box sx={Layout1_list_sx}>
-        {items.map(item => renderItem(
-          item,
-          selectedItem === item.id,
-          (isSelected) => setSelectedItem(isSelected ? item.id : undefined)
-        ))}
+        {items.map(item => {
+          const isSelected = selectedItem === item.id
+          return (
+            <Accordion
+              key={item.id}
+              sx={{
+                maxWidth: 600,
+              }}
+              expanded={isSelected}
+              onChange={(e, isSelected) => setSelectedItem(isSelected ? item.id : undefined)}
+            >
+              <AccordionSummary sx={{ pl: 1.5, pb: 1 }} expandIcon={<ExpandMoreIcon />}>
+                {renderAccorditionListItem(item, isSelected)}
+              </AccordionSummary>
+              <AccordionDetails sx={{ ml: 16 }}>
+                {renderAccorditionDetails(item)}
+              </AccordionDetails>
+            </Accordion>
+          )
+        })}
       </Box>
     </PageRoot>
   )
