0ba8d5abf9198155a16e9c09a25fea7f7e755e81
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-03-14 21:46:58 +0100

    +

--- packages/@mk-platforma/app/Item.sale.tsx
+++ packages/@mk-platforma/app/Item.sale.tsx
@@ -1,57 +1,39 @@
-import { Input, Box, Typography, Accordion, AccordionSummary, AccordionDetails } from "@mui/material"
-import SearchRounded from "@mui/icons-material/SearchRounded"
+import { Box, Typography } from "@mui/material"
 import LocationIcon from "@mui/icons-material/LocationOn"
 import data from "./data.json"
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
-import { useState } from "react"
-import { asNonNil } from "@mk-libs/common/common"
-import { Layout1_list_sx, Layout1_root_sx } from "./layout1"
-import { ZaBrata_MK_root } from "./ZaBrata.MK.root"
+import Section_base from "./Section.base"
+
 
 const { sellableItems } = data
 
 export default function Item_sale() {
-  const [_selectedItem, setSelectedItem] = useState<number>()
-  const selectedItem = _selectedItem ? asNonNil(sellableItems.find(e => e.id === _selectedItem)) : undefined
-
   return (
-    <ZaBrata_MK_root activeTab="buying-selling">
-      <Input
-        sx={{ pb: 0.7, mb: 6, width: 350 }}
-        autoFocus
-        placeholder="Pretraži"
-        startAdornment={<SearchRounded sx={{ mr: 2 }} />}
-      />
-      <Box sx={Layout1_list_sx}>
-        {sellableItems.map(item => {
-          return (
-            <Accordion
-              key={item.id}
-              expanded={item.id === selectedItem?.id}
-              onChange={(e, isExpanded) => setSelectedItem(isExpanded ? item.id : undefined)}
-            >
-              <AccordionSummary sx={{ pl: 1.5 }} expandIcon={<ExpandMoreIcon />}>
-                <Box sx={{ display: "flex", flex: 1 }}>
-                  <img src={item.imageUrl} width={100} height={100} />
-                  <Box sx={{ marginLeft: 3 }}>
-                    <Typography sx={{ fontWeight: 600, fontSize: 16 }}>{item.title}</Typography>
-                    <Box sx={{ color: "text.secondary", mt: 2 }}>
-                      <Box sx={{ display: "flex", alignItems: "center", mb: 0.3 }}>
-                        <LocationIcon style={{ width: 17, height: 17 }} sx={{ mr: 1 }} />
-                        <Typography>{item.location}</Typography>
-                      </Box>
-                    </Box>
-                  </Box>
-                </Box>
-              </AccordionSummary>
-              <AccordionDetails sx={{ ml: 16 }}>
-                <Typography>{item.description}</Typography>
-                <Typography sx={{ mt: 4 }}>Phone number: {item.adOwner.phoneNumber}</Typography>
-              </AccordionDetails>
-            </Accordion>
-          )
-        })}
-      </Box>
-    </ZaBrata_MK_root>
+    <Section_base
+      pageRootProps={{
+        activeTab: 'experts'
+      }}
+      items={sellableItems}
+      renderListItem={item => (
+        <Box sx={{ display: "flex", flex: 1 }}>
+          <img src={item.imageUrl} width={100} height={100} />
+          <Box sx={{ marginLeft: 3 }}>
+            <Typography sx={{ fontWeight: 600, fontSize: 16 }}>{item.title}</Typography>
+            <Box sx={{ color: "text.secondary", mt: 2 }}>
+              <Box sx={{ display: "flex", alignItems: "center", mb: 0.3 }}>
+                <LocationIcon style={{ width: 17, height: 17 }} sx={{ mr: 1 }} />
+                <Typography>{item.location}</Typography>
+              </Box>
+            </Box>
+          </Box>
+        </Box>        
+      )}
+      renderDetails={item => (
+        <Box sx={{ p: 4 }}>
+          <Typography sx={{ fontWeight: 600, fontSize: 19, mb: 3.5 }}>{item.title}</Typography>
+          <Typography>{item.description}</Typography>
+          <Typography sx={{ mt: 4 }}>Phone number: {item.adOwner.phoneNumber}</Typography>
+        </Box>
+      )}
+    />
   )
 }
