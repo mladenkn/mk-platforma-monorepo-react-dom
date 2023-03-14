03663f50b2ba8b22c8ab4ed672c52112ad8ef6f4
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-03-14 21:30:29 +0100

    +

--- packages/@mk-platforma/app/Expert.search.tsx
+++ packages/@mk-platforma/app/Expert.search.tsx
@@ -1,14 +1,11 @@
-import { Input, Box, Typography, Accordion, AccordionSummary, AccordionDetails } from "@mui/material"
-import SearchRounded from "@mui/icons-material/SearchRounded"
+import { Box, Typography } from "@mui/material"
 import { useState } from "react"
 import LocationIcon from "@mui/icons-material/LocationOn"
 import HandymanIcon from "@mui/icons-material/Handyman"
 import { asNonNil } from "@mk-libs/common/common"
 import data from "./data.json"
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
-import { Layout1_list_sx } from "./layout1"
 import Avatar from "./avatar"
-import { ZaBrata_MK_root } from "./ZaBrata.MK.root"
+
 
 const { experts } = data
 
@@ -17,61 +14,47 @@ export default function Expert_search() {
   const selectedExpert = _selectedExpert ? asNonNil(experts.find(e => e.id === _selectedExpert)) : undefined
 
   return (
-    <ZaBrata_MK_root activeTab="experts">
-      <Input
-        sx={{ pb: 0.7, mb: 5, width: 350 }}
-        autoFocus
-        placeholder="Pretraži majstore"
-        startAdornment={<SearchRounded sx={{ mr: 2 }} />}
-      />
-      <Box sx={Layout1_list_sx}>
-        {experts.map(expert => {
-          const isExpanded = expert.id === selectedExpert?.id
-          return (
-            <Accordion
-              sx={{ width: "100%" }}
-              key={expert.id}
-              expanded={expert.id === selectedExpert?.id}
-              onChange={(e, isExpanded) => setSelectedExpert(isExpanded ? expert.id : undefined)}
-            >
-              <AccordionSummary sx={{ pl: 1.5 }} expandIcon={<ExpandMoreIcon />}>
-                <Box sx={{ display: "flex", alignItems: "center" }}>
-                  <Avatar
-                    sx={{
-                      marginRight: 4,
-                      ...expert.avatarStyle,
-                    }}
-                    letter={expert.firstName[0]}
-                  />
-                  <Typography sx={{ fontWeight: 600, fontSize: 16, width: 115 }}>
-                    {expert.firstName} {expert.lastName}
-                  </Typography>
-                  <Box
-                    sx={{
-                      pl: 1,
-                      ml: 10,
-                      color: !isExpanded ? "text.secondary" : undefined,
-                    }}
-                  >
-                    <Box sx={{ display: "flex", alignItems: "center", mb: 0.3 }}>
-                      <LocationIcon style={{ width: 17, height: 17 }} sx={{ mr: 1 }} />
-                      <Typography>{expert.location}</Typography>
-                    </Box>
-                    <Box sx={{ display: "flex", alignItems: "center" }}>
-                      <HandymanIcon style={{ width: 17, height: 17 }} sx={{ mr: 1 }} />
-                      <Typography>{expert.occupations.join(", ")}</Typography>
-                    </Box>
-                  </Box>
-                </Box>
-              </AccordionSummary>
-              <AccordionDetails sx={{ ml: 25, mt: 3 }}>
-                <Typography>{expert.description}</Typography>
-                <Typography sx={{ mt: 4 }}>Mobitel/telefon: {expert.phoneNumber}</Typography>
-              </AccordionDetails>
-            </Accordion>
-          )
-        })}
-      </Box>
-    </ZaBrata_MK_root>
+    <Section_base
+      pageRootProps={{
+        activeTab: 'experts'
+      }}
+      items={experts}
+      renderListItem={expert => (
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <Avatar
+            sx={{
+              marginRight: 4,
+              ...expert.avatarStyle,
+            }}
+            letter={expert.firstName[0]}
+          />
+          <Typography sx={{ fontWeight: 600, fontSize: 16, width: 115 }}>
+            {expert.firstName} {expert.lastName}
+          </Typography>
+          <Box
+            sx={{
+              pl: 1,
+              ml: 10,
+              color: 'text.secondary',
+            }}
+          >
+            <Box sx={{ display: "flex", alignItems: "center", mb: 0.3 }}>
+              <LocationIcon style={{ width: 17, height: 17 }} sx={{ mr: 1 }} />
+              <Typography>{expert.location}</Typography>
+            </Box>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <HandymanIcon style={{ width: 17, height: 17 }} sx={{ mr: 1 }} />
+              <Typography>{expert.occupations.join(", ")}</Typography>
+            </Box>
+          </Box>
+        </Box>
+      )}
+      renderDetails={expert => (
+        <>
+          <Typography>{expert.description}</Typography>
+          <Typography sx={{ mt: 4 }}>Mobitel/telefon: {expert.phoneNumber}</Typography>
+        </>
+      )}
+    />
   )
 }
