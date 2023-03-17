caafd4f547948e36d3070e691ccf066b6ac25087
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-03-18 00:52:15 +0100

    +

--- packages/@mk-platforma/api/index.ts
+++ packages/@mk-platforma/api/index.ts
@@ -1,12 +1,14 @@
-import { initTRPC } from '@trpc/server';
- 
-const t = initTRPC.create();
- 
-const router = t.router;
-const publicProcedure = t.procedure;
- 
-const appRouter = router({});
- 
+import { initTRPC } from '@trpc/server'
+
+const t = initTRPC.create()
+
+const router = t.router
+const publicProcedure = t.procedure
+
+const appRouter = router({
+  hello: publicProcedure.query(() => 'hello from trpc first query!')
+})
+
 // Export type router type signature,
 // NOT the router itself.
-export type AppRouter = typeof appRouter;
+export type AppRouter = typeof appRouter
