296b131afbf3e1fc2dcdc36ae365289cbe6b349d
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-03-18 22:36:19 +0100

    +

---
+++ packages/@mk-platforma/app/EventOrJob.tsx
@@ -0,0 +1,71 @@
+import { Box, Typography } from "@mui/material"
+import LocationIcon from "@mui/icons-material/LocationOn"
+
+
+type EventOrJob_listItem_Props = {
+  label: string
+  location: string
+  photos: string[]
+}
+
+export function EventOrJob_listItem({ label, location, photos }: EventOrJob_listItem_Props){
+  return (
+    <Box sx={{ display: "flex", flexDirection: "column", flex: 1 }}>
+      <Typography sx={{ fontWeight: 600, fontSize: 16 }}>{label}</Typography>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          flex: 1,
+          marginTop: 3,
+          marginRight: 3,
+        }}
+      >
+        <Box sx={{ color: "text.secondary", mt: 1, ml: 1 }}>
+          <Box sx={{ display: "flex", alignItems: "center", mb: 0.3 }}>
+            <LocationIcon style={{ width: 17, height: 17 }} sx={{ mr: 1 }} />
+            <Typography>{location}</Typography>
+          </Box>
+        </Box>
+        <Box sx={{ display: "flex", gap: 1 }}>
+          {photos.map((photo, index) => (
+            <img key={index} width={65} height={65} src={photo} />
+          ))}
+        </Box>
+      </Box>
+    </Box>
+  )
+}
+
+type EventOrJob_details_Props = {
+  label: string
+  location: string
+  photos: string[]
+  description: string
+  adOwner: {
+    phoneNumber: string
+  }
+}
+
+export function EventOrJob_details({ label, location, photos, description, adOwner }: EventOrJob_details_Props){
+  return (
+    <Box sx={{ p: 4 }}>
+      <Typography sx={{ fontWeight: 600, fontSize: 19, mb: 3, }}>{label}</Typography>
+      <Box sx={{ color: "text.secondary", mb: 6,}}>
+        <Box sx={{ display: "flex", alignItems: "center", mb: 0.3 }}>
+          <LocationIcon style={{ width: 17, height: 17 }} sx={{ mr: 1 }} />
+          <Typography>{location}</Typography>
+        </Box>
+      </Box>
+      <Typography>{description}</Typography>
+      <Typography sx={{ mt: 4 }}>Phone number: {adOwner.phoneNumber}</Typography>
+      <Box sx={{ display: 'flex', justifyContent: 'end' }}>
+        <Box sx={{ display: "flex", gap: 2, marginTop: 4 }}>
+          {photos.map((photo, index) => (
+            <img key={index} width={90} height={90} src={photo} />
+          ))}
+        </Box>
+      </Box>
+    </Box>    
+  )
+}
--- packages/@mk-platforma/app/Job.offers.tsx
+++ packages/@mk-platforma/app/Job.offers.tsx
@@ -1,7 +1,6 @@
-import { Box, Typography } from "@mui/material"
-import LocationIcon from "@mui/icons-material/LocationOn"
 import data from "./data.json"
 import Section_base from "./Section.base"
+import { EventOrJob_details, EventOrJob_listItem } from "./EventOrJob"
 
 const { jobs } = data
 
@@ -10,52 +9,8 @@ export default function Job_offers() {
   return (
     <Section_base
       items={jobs}
-      renderListItem={item => (
-        <Box sx={{ display: "flex", flexDirection: "column", flex: 1 }}>
-          <Typography sx={{ fontWeight: 600, fontSize: 16 }}>{item.label}</Typography>
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "space-between",
-              flex: 1,
-              marginTop: 3,
-              marginRight: 3,
-            }}
-          >
-            <Box sx={{ color: "text.secondary", mt: 1, ml: 1 }}>
-              <Box sx={{ display: "flex", alignItems: "center", mb: 0.3 }}>
-                <LocationIcon style={{ width: 17, height: 17 }} sx={{ mr: 1 }} />
-                <Typography>{item.location}</Typography>
-              </Box>
-            </Box>
-            <Box sx={{ display: "flex", gap: 1 }}>
-              {item.photos.map((photo, index) => (
-                <img key={index} width={65} height={65} src={photo} />
-              ))}
-            </Box>
-          </Box>
-        </Box>
-      )}
-      renderDetails={item => (
-        <Box sx={{ p: 4 }}>
-          <Typography sx={{ fontWeight: 600, fontSize: 19, mb: 3, }}>{item.label}</Typography>
-          <Box sx={{ color: "text.secondary", mb: 6,}}>
-            <Box sx={{ display: "flex", alignItems: "center", mb: 0.3 }}>
-              <LocationIcon style={{ width: 17, height: 17 }} sx={{ mr: 1 }} />
-              <Typography>{item.location}</Typography>
-            </Box>
-          </Box>
-          <Typography>{item.description}</Typography>
-          <Typography sx={{ mt: 4 }}>Phone number: {item.adOwner.phoneNumber}</Typography>
-          <Box sx={{ display: 'flex', justifyContent: 'end' }}>
-            <Box sx={{ display: "flex", gap: 2, marginTop: 4 }}>
-              {item.photos.map((photo, index) => (
-                <img key={index} width={90} height={90} src={photo} />
-              ))}
-            </Box>
-          </Box>
-        </Box>
-      )}
+      renderListItem={item => <EventOrJob_listItem {...item} />}
+      renderDetails={item => <EventOrJob_details {...item} />}
     />
   )
 }
