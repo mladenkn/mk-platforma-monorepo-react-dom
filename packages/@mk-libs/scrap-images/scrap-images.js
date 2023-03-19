e045ae6da1b17677f2e3228c5b70774e7f5b2f4b
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-03-19 13:45:00 +0100

    +

--- packages/@mk-libs/scrap-images/scrap-images.js
+++ packages/@mk-libs/scrap-images/scrap-images.js
@@ -1,4 +1,8 @@
+function onlyUnique(value, index, array) {
+  return array.indexOf(value) === index;
+}
+
 Array.from(document.querySelectorAll('img'))
     .filter(i => i.width > 60 && i.height > 60)
     .map(i => i.src)
-    
+    .filter(onlyUnique)
