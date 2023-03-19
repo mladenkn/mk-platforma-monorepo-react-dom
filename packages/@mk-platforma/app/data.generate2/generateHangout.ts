e67bc4e55f9d18a7a13c2203102c7e76ec03b3cc
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-03-19 04:21:21 +0100

    +

---
+++ packages/@mk-platforma/app/data.generate2/generateHangout.ts
@@ -0,0 +1,52 @@
+import { faker } from "@faker-js/faker"
+import { generateArray } from "@mk-libs/common/common"
+import * as cro_dataset from "../data.cro.dataset"
+
+
+const randomPhoto = () =>
+  faker.helpers.arrayElement([
+    faker.image.business,
+    faker.image.food,
+    faker.image.nature,
+    faker.image.transport,
+  ])()
+
+const hangouts = [
+  {
+    label: "Prekopavanje vrta",
+  },
+  {
+    label: "Obrezivanje maslina",
+  },
+  {
+    label: "Zidanje zida",
+  },
+  {
+    label: "Kopanje rupa za voćke",
+  },
+  {
+    label: "Gradnja drvene kuće",
+  },
+  {
+    label: "Sadnja povrtnjaka",
+  },
+  {
+    label: "Sadnja voćnjaka",
+  },
+  {
+    label: "Izdrada ograde",
+  },
+]
+
+export default function generateHangout(){
+  return faker.helpers.shuffle(hangouts).map(({ label }, index) => ({
+    id: index + 1,
+    label,
+    description: generateArray(() => 'opis oglasa ', 30).join(''),
+    location: faker.helpers.arrayElement(cro_dataset.cities),
+    photos: generateArray(randomPhoto, 3),
+    adOwner: {
+      phoneNumber: faker.phone.number(),
+    },
+  }))
+}
