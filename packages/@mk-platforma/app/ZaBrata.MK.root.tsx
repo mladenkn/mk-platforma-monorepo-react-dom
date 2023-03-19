194aa8f6e3764c3cbf011be45980eafd5651a0a3
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-03-19 03:29:38 +0100

    +

--- packages/@mk-platforma/app/ZaBrata.MK.root.tsx
+++ packages/@mk-platforma/app/ZaBrata.MK.root.tsx
@@ -25,12 +25,12 @@ export default function ZaBrata_MK_root(){
       }}
     >
       <TabsMui value={activeTab} centered onChange={(e, newValue) => setActiveTab(newValue)}>
-        <Tab sx={{ textTransform: "none" }} label="Poslovi" value="jobs" />
-        <Tab sx={{ textTransform: "none" }} label="Majstori" value="experts" />
-        <Tab sx={{ textTransform: "none" }} label="Nabava" value="buying-selling" />
-        <Tab sx={{ textTransform: "none" }} label="Okupljanje / Radna akcija" value="gathering/work-action" />
-        <Tab sx={{ textTransform: "none" }} label="Okupljanje / Druženje" value="gathering/hangout" />
-        <Tab sx={{ textTransform: "none" }} label="Okupljanje / Duhovna radionica" value="gathering/spiritual" />
+        <Tab sx={{ textTransform: "none" }} label="@Poslovi" value="jobs" />
+        <Tab sx={{ textTransform: "none" }} label="@Majstori" value="experts" />
+        <Tab sx={{ textTransform: "none" }} label="@Nabava" value="buying-selling" />
+        <Tab sx={{ textTransform: "none" }} label="@Okupljanje / @RadnaAkcija" value="gathering/work-action" />
+        <Tab sx={{ textTransform: "none" }} label="@Okupljanje, @Druženje" value="gathering/hangout" />
+        <Tab sx={{ textTransform: "none" }} label="@Okupljanje, @Duhovnost" value="gathering/spiritual" />
       </TabsMui>
       <Box sx={Layout1_root_sx}>
         {activeTab === 'jobs' ? <EventOrJobs_list items={data.jobs} /> : <></>}
