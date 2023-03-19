5302d6865729f5e878eaee65f4630cf2f01cca59
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-03-19 19:24:02 +0100

    +

--- packages/@mk-platforma/app/data/data.gathering.ts
+++ packages/@mk-platforma/app/data/data.gathering.ts
@@ -3,35 +3,29 @@ import { generateArray } from "@mk-libs/common/common"
 import * as cro_dataset from "./data.cro.dataset"
 import data_images from "./data.images.json"
 
-const hangouts = [
+const withRelatedProps = [
   {
-    label: "Prekopavanje vrta",
-  },
-  {
-    label: "Obrezivanje maslina",
-  },
-  {
-    label: "Zidanje zida od cob materijala",
+    label: "Zidanje od cob materijala",
   },
   {
     label: "Kopanje rupa za voćke",
   },
   {
-    label: "Gradnja drvene kuće",
-  },
-  {
     label: "Sadnja povrtnjaka",
-  },
-  {
-    label: "Sadnja voćnjaka",
   },
   {
     label: "Izrada ograde od šiblja",
   },
+  {
+    label: 'Duhovno okupljanje, meditacija...',
+  },
+  {
+    label: 'Druženje, proslava rođendana',
+  },
 ]
 
 export default function generateGatherings(){
-  return faker.helpers.shuffle(hangouts).map(({ label }, index) => ({
+  return faker.helpers.shuffle(withRelatedProps).map(({ label }, index) => ({
     label,
     id: index + 1,
     description: generateArray(() => 'opis oglasa ', 30).join(''),
