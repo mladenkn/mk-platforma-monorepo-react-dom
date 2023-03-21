e2364dd0abfe5512b32d1fc513e0a97961f25bd3
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-03-21 20:20:09 +0100

    +

--- packages/@mk-platforma/app/Post.details.section.tsx
+++ packages/@mk-platforma/app/Post.details.section.tsx
@@ -3,7 +3,8 @@ import data from "./data/data.json"
 import { useRouter } from 'next/router'
 import { asNonNil } from "@mk-libs/common/common"
 import Header from "./header"
-import { Box } from "@mui/material"
+import { Box, IconButton } from "@mui/material"
+import ListAltOutlinedIcon from '@mui/icons-material/ListAltOutlined'
 
 
 export default function Post_details_section(){
@@ -12,7 +13,16 @@ export default function Post_details_section(){
   const item = asNonNil(data.gatherings.find(g => g.id === itemId))
   return (
     <Box>
-      <Header />
+      <Header
+        right={
+          <a href="/" style={{ textDecoration: 'none', }}>
+            <IconButton sx={{ display: 'flex', gap: 1, }}>
+              <Box sx={{ color: 'white', fontSize: 20, }}>Oglasi</Box>
+              <ListAltOutlinedIcon sx={{ color: 'white' }} />
+            </IconButton>
+          </a>
+        }
+      />
       <Post_common_listItem_details {...item} />
     </Box>    
   )
