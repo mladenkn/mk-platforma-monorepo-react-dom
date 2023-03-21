177388c0c922a393b93607ca90b6bade522a9562
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-03-21 20:17:56 +0100

    +

--- packages/@mk-platforma/app/Post.details.section.tsx
+++ packages/@mk-platforma/app/Post.details.section.tsx
@@ -2,11 +2,18 @@ import { Post_common_listItem_details } from "./Post.common.listItem"
 import data from "./data/data.json"
 import { useRouter } from 'next/router'
 import { asNonNil } from "@mk-libs/common/common"
+import Header from "./header"
+import { Box } from "@mui/material"
 
 
 export default function Post_details_section(){
   const router = useRouter()
   const itemId = asNonNil(parseInt(router.query.id as string))
   const item = asNonNil(data.gatherings.find(g => g.id === itemId))
-  return <Post_common_listItem_details {...item} />
+  return (
+    <Box>
+      <Header />
+      <Post_common_listItem_details {...item} />
+    </Box>    
+  )
 }
