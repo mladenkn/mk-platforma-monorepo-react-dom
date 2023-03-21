fddecbffb34631ebead916a31b8f732bc0bc3d5d
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-03-21 12:49:33 +0100

    +

--- packages/@mk-platforma/app/Post.create.section.tsx
+++ packages/@mk-platforma/app/Post.create.section.tsx
@@ -1,12 +1,18 @@
-import { Box } from "@mui/material"
+import { Box, colors, useTheme } from "@mui/material"
 import PostForm from "./post.form"
 import Header from './header'
 
 
 export default function Post_create_section(){
+  const theme = useTheme()
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%' }}>
-      <Header sx={{ mb: 4, }} />
+      <Header
+        sx={{ mb: 4, }}
+        right={
+          <Box sx={{ fontSize: 25, color: colors.pink[100] }}>Novi oglas</Box>
+        }
+      />
       <PostForm />
     </Box> 
   ) 
--- packages/@mk-platforma/app/header.tsx
+++ packages/@mk-platforma/app/header.tsx
@@ -1,17 +1,30 @@
 import { Box, SxProps } from "@mui/material"
+import { ReactNode } from "react"
 
 
 type Props = {
   sx?: SxProps
+  right: ReactNode
 }
 
-export default function Header({ sx }: Props){
+export default function Header({ sx, right, }: Props){
   return (
-    <Box sx={{ background: '#2d5be3', width: '100%', pt: 1.5, pb: 2, px: 2, ...sx, }}>
+    <Box sx={{
+      background: '#2d5be3',
+      width: '100%',
+      pt: 1.5,
+      pb: 2,
+      px: 2,
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      ...sx,
+    }}>
       <Box sx={{ ml: 2, }}>
 	      <a style={{ color: 'white', fontSize: 25, textDecoration: 'none', }} href="https://za-brata.com/">ZaBrata</a>
         <Box sx={{ color: 'white' }}>Loza kontribucionizma</Box>
 	    </Box>
+      {right}
     </Box>
   )
 }
