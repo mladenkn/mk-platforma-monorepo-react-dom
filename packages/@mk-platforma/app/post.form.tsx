f0008065623778896ea6bedddcb0d52536c4cbff
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-03-21 12:23:38 +0100

    +

--- packages/@mk-platforma/app/post.form.tsx
+++ packages/@mk-platforma/app/post.form.tsx
@@ -1,83 +1,31 @@
-import React from 'react'
-import { useForm } from 'react-hook-form'
-import { TextField, Button } from '@material-ui/core'
+import { Box, TextField } from "@mui/material";
 
-interface FormInputs {
-  label: string
-  description: string
-  location: string
-  photos: string[]
-  phoneNumber: string
-}
-
-export const PostBaseForm: React.FC<{
-  onSubmit: (postBase: Post_base) => void
-}> = ({ onSubmit }) => {
-  const { register, handleSubmit, errors } = useForm<FormInputs>()
-
+export default function PostForm(){
   return (
-    <form
-      onSubmit={handleSubmit(data => {
-        const postBase = {
-          id: 0,
-          label: data.label,
-          description: data.description,
-          location: data.location,
-          photos: data.photos,
-          phoneNumber: data.phoneNumber
-        }
-        onSubmit(postBase)
-      })}
-      noValidate
-      autoComplete="off"
-    >
+    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 4, }}>
       <TextField
-        name="label"
-        label="Label"
+        label="Naziv"
         variant="outlined"
-        inputRef={register({ required: true })}
-        error={!!errors.label}
-        helperText={errors.label && "Label is required"}
-        sx={{ marginBottom: '20px' }}
       />
       <TextField
-        name="description"
-        label="Description"
+        label="Kategorije"
         variant="outlined"
-        inputRef={register({ required: true })}
-        error={!!errors.description}
-        helperText={errors.description && "Description is required"}
-        sx={{ marginBottom: '20px' }}
       />
       <TextField
-        name="location"
-        label="Location"
+        label="Lokacija"
         variant="outlined"
-        inputRef={register({ required: true })}
-        error={!!errors.location}
-        helperText={errors.location && "Location is required"}
-        sx={{ marginBottom: '20px' }}
+        multiline
       />
       <TextField
-        name="photos"
-        label="Photos"
+        label="Opis"
         variant="outlined"
-        inputRef={register({ required: true })}
-        error={!!errors.photos}
-        helperText={errors.photos && "Photos is required"}
-        sx={{ marginBottom: '20px' }}
+        multiline
       />
       <TextField
-        name="phoneNumber"
-        label="Phone Number"
+        label="Broj mobitela"
         variant="outlined"
-        inputRef={register({ required: true })}
-        error={!!errors.phoneNumber}
-        helperText={errors.phoneNumber && "Phone Number is required"}
-        sx={{ marginBottom: '20px' }}
+        multiline
       />
-      <Button type="submit" variant="contained" color="primary">
-        Submit
-      </Button>
-    </form>
+    </Box>
   )
+}
