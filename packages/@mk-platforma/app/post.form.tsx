75e63e4ba4dfa732029c5fbfa7c3fe7e965f70f4
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-03-21 12:01:41 +0100

    .

--- packages/@mk-platforma/app/post.form.tsx
+++ packages/@mk-platforma/app/post.form.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { useForm, Controller } from 'react-hook-form';
-import { TextField, Button } from '@mui/material';
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import { TextField, Button } from '@material-ui/core'
 
-export type Post_base = {
-  id: number
+interface FormInputs {
   label: string
   description: string
   location: string
@@ -11,75 +10,74 @@ export type Post_base = {
   phoneNumber: string
 }
 
-const PostForm: React.FC = () => {
-  const { control, handleSubmit } = useForm<Post_base>();
-  const onSubmit = handleSubmit((data) => console.log(JSON.stringify(data)));
+export const PostBaseForm: React.FC<{
+  onSubmit: (postBase: Post_base) => void
+}> = ({ onSubmit }) => {
+  const { register, handleSubmit, errors } = useForm<FormInputs>()
 
   return (
-    <form onSubmit={onSubmit} style={{ display: 'flex', flexDirection: 'column', minWidth: 400, maxWidth: 700 }}>
-      <Controller
-        render={() =>
-          <TextField
-            label="Label"
-            type="text"
-            variant="outlined"
-            sx={{ backgroundColor: '#f8f8f8', marginBottom: '20px' }}
-          />
+    <form
+      onSubmit={handleSubmit(data => {
+        const postBase = {
+          id: 0,
+          label: data.label,
+          description: data.description,
+          location: data.location,
+          photos: data.photos,
+          phoneNumber: data.phoneNumber
         }
-        control={control}
+        onSubmit(postBase)
+      })}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField
         name="label"
+        label="Label"
+        variant="outlined"
+        inputRef={register({ required: true })}
+        error={!!errors.label}
+        helperText={errors.label && "Label is required"}
+        sx={{ marginBottom: '20px' }}
       />
-      <Controller
-        render={() =>
-          <TextField
-            label="Description"
-            type="text"
-            variant="outlined"
-            sx={{ backgroundColor: '#f8f8f8', marginBottom: '20px' }}
-          />
-        }
-        control={control}
+      <TextField
         name="description"
+        label="Description"
+        variant="outlined"
+        inputRef={register({ required: true })}
+        error={!!errors.description}
+        helperText={errors.description && "Description is required"}
+        sx={{ marginBottom: '20px' }}
       />
-      <Controller
-        render={() =>
-          <TextField
-            label="Location"
-            type="text"
-            variant="outlined"
-            sx={{ backgroundColor: '#f8f8f8', marginBottom: '20px' }}
-          />
-        }
-        control={control}
+      <TextField
         name="location"
+        label="Location"
+        variant="outlined"
+        inputRef={register({ required: true })}
+        error={!!errors.location}
+        helperText={errors.location && "Location is required"}
+        sx={{ marginBottom: '20px' }}
       />
-      <Controller
-        render={() =>
-          <TextField
-            label="Photos"
-            type="text"
-            variant="outlined"
-            sx={{ backgroundColor: '#f8f8f8', marginBottom: '20px' }}
-          />
-        }
-        control={control}
+      <TextField
         name="photos"
+        label="Photos"
+        variant="outlined"
+        inputRef={register({ required: true })}
+        error={!!errors.photos}
+        helperText={errors.photos && "Photos is required"}
+        sx={{ marginBottom: '20px' }}
       />
-      <Controller
-        render={() =>
-          <TextField
-            label="Phone Number"
-            type="text"
-            variant="outlined"
-            sx={{ backgroundColor: '#f8f8f8', marginBottom: '20px' }}
-          />
-        }
-        control={control}
+      <TextField
         name="phoneNumber"
+        label="Phone Number"
+        variant="outlined"
+        inputRef={register({ required: true })}
+        error={!!errors.phoneNumber}
+        helperText={errors.phoneNumber && "Phone Number is required"}
+        sx={{ marginBottom: '20px' }}
       />
-      <Button type="submit" variant="contained" color="primary">Submit</Button>
+      <Button type="submit" variant="contained" color="primary">
+        Submit
+      </Button>
     </form>
-  );
-}
-
-export default PostForm;
+  )
