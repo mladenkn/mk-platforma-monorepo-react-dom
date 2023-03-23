3d32034d46c728f94bbf384e985df7c90992cf12
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-03-23 19:15:45 +0100

    +

--- packages/@mk-platforma/app/Post.form.tsx
+++ packages/@mk-platforma/app/Post.form.tsx
@@ -1,18 +1,31 @@
 import { Box, TextField, SxProps } from "@mui/material"
 import CategoriesDropdown from "./Categories.dropdown"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Post_base_zod, Post_base } from "./data/data.types"
+import { useEffect } from "react"
+
 
 type Props = {
   sx?: SxProps
 }
 
 export default function PostForm({ sx }: Props) {
+  const { register, getValues } = useForm<Post_base>({
+    resolver: zodResolver(Post_base_zod),
+  })
+
+  useEffect(() => {
+    // setInterval(() => console.log(getValues(), []), 1000)
+  })  
+
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: 4, ...sx }}>
-      <TextField label="Naziv" variant="outlined" />
+      <TextField label="Naziv" variant="outlined" {...register('label')} />
       <CategoriesDropdown />
-      <TextField label="Lokacija" variant="outlined" />
-      <TextField label="Opis" variant="outlined" multiline />
-      <TextField label="Broj mobitela" variant="outlined" />
+      <TextField label="Lokacija" variant="outlined" {...register('location')} />
+      <TextField label="Opis" variant="outlined" multiline {...register('description')} />
+      <TextField label="Broj mobitela" variant="outlined" {...register('phoneNumber')} />
     </Box>
   )
 }
--- packages/@mk-platforma/app/data/data.types.ts
+++ packages/@mk-platforma/app/data/data.types.ts
@@ -1,12 +1,17 @@
-export type Post_base = {
-  id: number
-  label: string
-  description: string
-  location: string
-  photos: string[]
-  phoneNumber: string
-}
+import { z } from 'zod'
+
 
 export type Category = "job" | "accommodation" | "personEndorsement" | "sellable" | "gathering"
 
 export const allCategories: Category[] = ["job", "accommodation", "personEndorsement", "sellable", "gathering"]
+
+export const Post_base_zod = z.object({
+  id: z.number(),
+  label: z.string(),
+  description: z.string(),
+  location: z.string(),
+  photos: z.array(z.string()),
+  phoneNumber: z.string(),
+})
+
+export type Post_base = z.infer<typeof Post_base_zod>
