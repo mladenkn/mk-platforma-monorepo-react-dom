42a00d3d27ace18f9387e1573f13d4158029398d
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-03-25 16:28:37 +0100

    +

--- packages/@mk-platforma/app/Post.form.2.base.tsx
+++ packages/@mk-platforma/app/Post.form.2.base.tsx
@@ -2,7 +2,7 @@ import { useFormik } from "formik"
 import { ComponentProps, useCallback } from "react"
 import { toFormikValidationSchema } from 'zod-formik-adapter'
 import { Post_base, Post_base_zod } from "./data/data.types"
-import { SxProps, TextField } from "@mui/material"
+import { SxProps, TextField, TextFieldProps } from "@mui/material"
 import CategoriesDropdown from "./Categories.dropdown"
 
 
@@ -33,7 +33,7 @@ export default function use_Post_form_base({ initialValues }: Props){
     name: "label",
     value: values.label,
     onChange: handleChange,
-  }
+  } satisfies TextFieldProps
 
   const descriptionProps = {
     label: "Opis",
@@ -42,7 +42,7 @@ export default function use_Post_form_base({ initialValues }: Props){
     name: "description",
     value: values.description,
     onChange: handleChange,
-  }
+  } satisfies TextFieldProps
 
   const categoriesProps = {
     value: values.categories,
@@ -55,7 +55,7 @@ export default function use_Post_form_base({ initialValues }: Props){
     name: "location",
     value: values.location,
     onChange: handleChange,
-  }
+  } satisfies TextFieldProps
 
   return {
     control: form,
