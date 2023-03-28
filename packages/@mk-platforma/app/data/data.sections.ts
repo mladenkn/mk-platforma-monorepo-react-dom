96019d037e68b1c4e086a4db5f3a03d962970fe3
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-03-28 22:00:43 +0200

    +

--- packages/@mk-platforma/app/data/data.sections.ts
+++ packages/@mk-platforma/app/data/data.sections.ts
@@ -5,31 +5,37 @@ const sections = [
     id: 1,
     label: 'Smještaji',
     iconName: 'accommodation' as Category,
+    categories: ['accommodation'] as Category[],
   },
   {
     id: 4,
     label: 'Poslovi',
     iconName: 'job' as Category,
+    categories: ['job'] as Category[],
   },
   {
     id: 3,
     label: 'Radne akcije',
     iconName: 'gathering' as Category,
+    categories: ['gathering', 'work'] as Category[],
   },
   {
     id: 2,
     label: 'Duhovna okupljanja',
     iconName: 'gathering' as Category,
+    categories: ['gathering', 'spirituality'] as Category[],
   },
   {
     id: 5,
     label: 'Majstori',
     iconName: 'personEndorsement' as Category,
+    categories: ['personEndorsement'] as Category[],
   },
   {
     id: 6,
     label: 'Nabava',
     iconName: 'sellable' as Category,
+    categories: ['sellable'] as Category[],
   }
 ]
 
