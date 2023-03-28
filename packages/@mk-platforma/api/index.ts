ab9e123e36cea7a012cb6610d4f8458bc796b465
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-03-29 00:33:58 +0200

    +

--- packages/@mk-platforma/api/index.ts
+++ packages/@mk-platforma/api/index.ts
@@ -1,4 +1,8 @@
 import { initTRPC } from "@trpc/server"
+import data from './data/data.json'
+import sections from "./data/data.sections"
+import { z } from "zod"
+
 
 const t = initTRPC.create()
 
@@ -6,7 +10,15 @@ const router = t.router
 const publicProcedure = t.procedure
 
 export const appRouter = router({
-  hello: publicProcedure.query(() => "hello from trpc first query!"),
+  posts: publicProcedure.query(() => data.allPosts),
+
+  post_sing: publicProcedure
+    .input(z.object({
+      id: z.number()
+    }))
+    .query(({ input }) => data.allPosts.find(post => post.id === input.id)),
+
+  sections: publicProcedure.query(() => sections),
 })
 
 // Export type router type signature,
