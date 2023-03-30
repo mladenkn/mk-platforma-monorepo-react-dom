ac7b4edcab41aa9d6fd42a0bccd0b9f702d53b29
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-03-30 15:17:22 +0200

    +

--- packages/@mk-platforma/api/data/data.types.ts
+++ packages/@mk-platforma/api/data/data.types.ts
@@ -1,16 +1,6 @@
 import { z } from "zod"
 
-export type Category =
-  | "job"
-  | "accommodation"
-  | "personEndorsement"
-  | "sellable"
-  | "gathering"
-  | "gathering/spirituality"
-  | "gathering/work"
-  | "gathering/hangout"
-
-export const allCategories: Category[] = [
+export const Category_zod = z.enum([
   "job",
   "accommodation",
   "personEndorsement",
@@ -19,23 +9,24 @@ export const allCategories: Category[] = [
   "gathering/spirituality",
   "gathering/work",
   "gathering/hangout",
-]
+])
+export type Category = z.infer<typeof Category_zod>
 
-export const Category_zod = z.enum(["job", "accommodation", "personEndorsement", "sellable", "gathering", "gathering/spirituality", "gathering/work", "gathering/hangout"])
-
-export const Post_base_zod = z.object({
-  id: z.number(),
-  label: z.string(),
-  description: z.string(),
-  photos: z.array(z.string()),
-  categories: z.array(Category_zod),
-  location: z.string().optional(),
-})
-
-export type Post_base = z.infer<typeof Post_base_zod>
+export type Post_base = {
+  id: number
+  label: string
+  description: string
+  photos: string[]
+  categories: Category[]
+  location?: string
+  mainImage?: string
+}
 
 export type Post_expert = Post_base & {
-  categories: ["personEndorsement"] | ["personEndorsement", Category] | ["personEndorsement", Category, Category]
+  categories:
+    | ["personEndorsement"]
+    | ["personEndorsement", Category]
+    | ["personEndorsement", Category, Category]
   firstName: string
   lastName: string
   skills: string[]
