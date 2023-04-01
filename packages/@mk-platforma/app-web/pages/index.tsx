92989e55d83d3141140fc0e7eaa4a9c4dd61ee9a
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-04-02 01:03:21 +0200

    +

--- packages/@mk-platforma/app-web/pages/index.tsx
+++ packages/@mk-platforma/app-web/pages/index.tsx
@@ -1,2 +1,10 @@
 import PostList_section from "@mk-platforma/app/Post.list.section"
-export default PostList_section
+import { useRouter } from "next/router"
+import { Category } from "../../api/data/data.types"
+
+export default function () {
+  const router = useRouter()
+  const { name } = router.query
+  const name_ = typeof name === "string" ? (name as Category) : undefined
+  return <PostList_section initialTab={name_} />
+}
