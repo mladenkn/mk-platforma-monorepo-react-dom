be62223e3d458b0d200cdcd2300cb1654b6fc4da
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-04-05 01:28:11 +0200

    +

--- packages/@mk-platforma/app-web/client/Post.create.section.tsx
+++ packages/@mk-platforma/app-web/client/Post.create.section.tsx
@@ -1,9 +1,10 @@
 import { Box, IconButton, SxProps, TextField, Button, Paper, Typography } from "@mui/material"
-import Header from "./Header"
-import ListAltOutlinedIcon from "@mui/icons-material/ListAltOutlined"
 import use_Post_form_expertOnly from "./Post.form.expertOnly"
 import CategoriesDropdown from "./Categories.dropdown"
 import use_Post_form_base from "./Post.form.base"
+import ArrowBackIosOutlinedIcon from "@mui/icons-material/ArrowBackIosOutlined"
+import { Header_root, Header_moreOptions } from "./Header"
+import { useRouter } from "next/router"
 
 type Props = {
   sx?: SxProps
@@ -17,21 +18,31 @@ export default function Post_create_section({ sx }: Props) {
 
   function onSubmit() {}
 
+  const goBack = useRouter().back
+
   return (
     <Box sx={{ display: "flex", flexDirection: "column", width: "100%", ...sx }}>
-      <Header
-        sx={{ mb: 2 }}
-        right={
-          <a href="/" style={{ textDecoration: "none" }}>
-            <IconButton sx={{ display: "flex", gap: 1 }}>
-              <Typography variant="h4" sx={{ color: "white" }}>
-                Oglasi
-              </Typography>
-              <ListAltOutlinedIcon sx={{ color: "white" }} />
-            </IconButton>
+      <Header_root sx={{ pr: 1.5 }}>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            color: "white",
+            gap: 2,
+          }}
+        >
+          <IconButton sx={{ color: "white" }} onClick={goBack}>
+            <ArrowBackIosOutlinedIcon />
+          </IconButton>
+          <a style={{ color: "white", textDecoration: "none" }} href="/">
+            <Typography variant="h3">ZaBrata</Typography>
+            <Box sx={{ color: "white" }}>
+              <Typography variant="h5">Loza kontribucionizma</Typography>
+            </Box>
           </a>
-        }
-      />
+        </Box>
+        <Header_moreOptions options={["profile", "post.list"]} />
+      </Header_root>
       <Paper sx={{ px: 3, display: "flex", flexDirection: "column", gap: 2, m: 1, p: 2 }}>
         <Typography variant="h3" sx={{ mb: 2 }}>
           Novi oglas
