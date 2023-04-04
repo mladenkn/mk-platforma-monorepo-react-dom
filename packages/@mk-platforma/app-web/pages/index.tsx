e408122337e8d03c117544892e0962e6c987e813
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-04-04 16:32:49 +0200

    +

--- packages/@mk-platforma/app-web/pages/index.tsx
+++ packages/@mk-platforma/app-web/pages/index.tsx
@@ -3,8 +3,9 @@ import { useRouter } from "next/router"
 import { Category } from "../data/data.types"
 
 export default function () {
-  const router = useRouter()
-  const { category } = router.query
-  const category_ = typeof category === "string" ? (category as Category) : "gathering"
-  return <PostList_section initialTab={category_} />
+  const { query, isReady } = useRouter()
+  if (isReady) {
+    const category = query.category ? (query.category as Category) : ("gathering" as "gathering")
+    return <PostList_section initialTab={category} />
+  } else return "Učitavanje..."
 }
