7a50b97cde7822ef85785bcb5f75847413381178
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-04-04 17:50:23 +0200

    +

--- packages/@mk-platforma/app-web/client/Categories.selector.aside.tsx
+++ packages/@mk-platforma/app-web/client/Categories.selector.aside.tsx
@@ -1,4 +1,13 @@
-import { List, ListItem, ListItemButton, ListItemIcon, ListItemText, useTheme } from "@mui/material"
+import {
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  useTheme,
+  Box,
+  Typography,
+} from "@mui/material"
 import { allCategories, getCategoryLabel, CategoryIcon } from "./Categories.common"
 
 type Props = {}
@@ -6,20 +15,32 @@ type Props = {}
 export default function Categories_selector_aside({}: Props) {
   const { palette, typography } = useTheme()
   return (
-    <List sx={{ background: palette.primary.main, pl: 1, pr: 3, height: "100%" }}>
-      {allCategories.map(category => (
-        <ListItem key={category} disablePadding>
-          <ListItemButton href={`?/category=${category}`}>
-            <ListItemIcon sx={{ color: "white" }}>
-              <CategoryIcon sx={{ fontSize: typography.h3 }} name={category} />
-            </ListItemIcon>
-            <ListItemText
-              sx={{ color: "white", ".MuiListItemText-primary": { fontSize: typography.h5 } }}
-              primary={getCategoryLabel(category)}
-            />
-          </ListItemButton>
-        </ListItem>
-      ))}
-    </List>
+    <Box sx={{ background: palette.primary.main, height: "100%", p: 3 }}>
+      <a style={{ color: "white", textDecoration: "none" }} href="/">
+        <Typography variant="h2" fontWeight={400}>
+          ZaBrata
+        </Typography>
+        <Box sx={{ color: "white" }}>
+          <Typography variant="h4" fontWeight={400}>
+            Loza kontribucionizma
+          </Typography>
+        </Box>
+      </a>
+      <List sx={{ mt: 4 }} disablePadding>
+        {allCategories.map(category => (
+          <ListItem key={category} disablePadding>
+            <ListItemButton href={`?/category=${category}`} sx={{ px: 0 }}>
+              <ListItemIcon sx={{ color: "white" }}>
+                <CategoryIcon sx={{ fontSize: typography.h3 }} name={category} />
+              </ListItemIcon>
+              <ListItemText
+                sx={{ color: "white", ".MuiListItemText-primary": { fontSize: typography.h5 } }}
+                primary={getCategoryLabel(category)}
+              />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
   )
 }
