4f318a4f0853d4ad9ee42c493627a0a5735f5040
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-04-11 10:59:21 +0200

    +

--- packages/@mk-platforma/app-web/data/data.experts.ts
+++ packages/@mk-platforma/app-web/data/data.experts.ts
@@ -1,10 +1,15 @@
 import { faker } from "@faker-js/faker"
+import { uniq } from "lodash"
 import { avatarStyles } from "./data.common"
 import * as cro_dataset from "./data.cro.dataset"
 
 export default function generateExpert() {
   const firstName = faker.helpers.arrayElement(cro_dataset.firstNames)
   const lastName = faker.helpers.arrayElement(cro_dataset.lastNames)
+
+  const skills = uniq(
+    faker.helpers.arrayElements(cro_dataset.skills, faker.datatype.number({ min: 1, max: 4 }))
+  )
 
   return {
     categories: ["personEndorsement" as "personEndorsement"],
@@ -21,9 +26,10 @@ export default function generateExpert() {
       firstName,
       lastName,
 
-      skills: faker.helpers
-        .arrayElements(cro_dataset.skills, faker.datatype.number({ min: 1, max: 4 }))
-        .map(skill => ({ label: skill, level: faker.datatype.number({ min: 2, max: 5 }) })),
+      skills: skills.map(skill => ({
+        label: skill,
+        level: faker.datatype.number({ min: 2, max: 5 }),
+      })),
 
       avatarStyle: faker.helpers.arrayElement(avatarStyles),
     },
