512b8f9d889a2793f49c407de5d56760bf6d6998
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-04-11 13:07:56 +0200

    +

--- packages/@mk-platforma/app-web/data/data.types.ts
+++ packages/@mk-platforma/app-web/data/data.types.ts
@@ -3,16 +3,4 @@ import { inferOutput } from "../trpc.types"
 
 export type Id = number | string
 
-export const Category_zod = z.enum([
-  "job",
-  "accommodation",
-  "personEndorsement",
-  "sellable",
-  "gathering",
-  "gathering_spirituality",
-  "gathering_work",
-  "gathering_hangout",
-])
-export type CategoryLabel = z.infer<typeof Category_zod>
-
 export type Post_base = NonNullable<inferOutput["post"]["single"]>
