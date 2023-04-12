c2efc24e4a7a2b6548dc73b43844565d1d81b5b9
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-04-12 16:01:33 +0200

    +

--- packages/@mk-platforma/app-web/api/Post.api.ts
+++ packages/@mk-platforma/app-web/api/Post.api.ts
@@ -1,12 +1,11 @@
 import { z } from "zod"
 import { publicProcedure, router } from "../trpc.utils"
 import Post_api_create from "./Post.api.create"
-import { Post, PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client"
 import { Post_category_labelSchema } from "../prisma/generated/zod"
 import { assertIsNonNil } from "@mk-libs/common/common"
 import { Post_single_details_PostSelect } from "../client/Post.single.details"
 import { PostList_section_PostSelect } from "../client/Post.list.section"
-import type { Prisma } from "@prisma/client"
 
 const db = new PrismaClient()
 
@@ -55,72 +54,5 @@ const Post_api = router({
 
   create: Post_api_create,
 })
-
-function Post_single<TMapped>(
-  select: Prisma.PostSelect,
-  map?: (post?: Partial<Post> | null) => TMapped
-) {
-  return publicProcedure
-    .input(
-      z.object({
-        id: z.number(),
-      })
-    )
-    .query(async ({ input }) => {
-      const post = await db.post.findUnique({
-        where: { id: input.id },
-        select,
-      })
-      return map ? map(post) : post
-    })
-}
-
-const single1 = Post_single(Post_single_details_PostSelect, () => ({}))
-
-function createMethod(inner: any) {
-  return async <TOutput>(select: any, map?: any): Promise<TOutput> => {
-    const data = await inner(select)
-    return map ? map(data) : data
-  }
-}
-
-// @ts-ignore
-const post_single2_base = createMethod(select =>
-  publicProcedure
-    .input(
-      z.object({
-        id: z.number(),
-      })
-    )
-    .query(async ({ input }) => {
-      const post = await db.post.findUnique({
-        where: { id: input.id },
-        select,
-      })
-      return post
-    })
-)
-
-const Database_abstract = {
-  post: {
-    findUnique:
-      (inner: any) =>
-      <TMoreArgs extends Partial<Prisma.PostFindUniqueArgs>>(args: Partial<TMoreArgs>) =>
-      (input: any) =>
-        createMethod(inner(args)) as any as Promise<Prisma.PostGetPayload<TMoreArgs>>,
-  },
-}
-
-const Database = {
-  post: Database_abstract.post.findUnique(post_single2_base)({
-    select: Post_single_details_PostSelect,
-  }),
-}
-
-const a = Database.post({})
-
-// const post_single_2 = createMethod(post_single2_base)<
-//   Prisma.PostGetPayload<{ select: typeof Post_single_details_PostSelect }>
-// >(Post_single_details_PostSelect)
 
 export default Post_api
