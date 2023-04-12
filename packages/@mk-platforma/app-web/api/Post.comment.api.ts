85c4e4779580922639411c7c883aaa74378d8316
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-04-12 19:27:32 +0200

    +

--- packages/@mk-platforma/app-web/api/Post.comment.api.ts
+++ packages/@mk-platforma/app-web/api/Post.comment.api.ts
@@ -2,8 +2,14 @@ import { z } from "zod"
 import { publicProcedure, router } from "../trpc.utils"
 import { PrismaClient } from "@prisma/client"
 import { Comment_listItem_CommentSelect } from "../client/Comment.common"
+import { Post_commentSchema } from "../prisma/generated/zod"
 
 const db = new PrismaClient()
+
+export const Post_commonent_create_input_zod = Post_commentSchema.pick({
+  content: true,
+  postId: true,
+})
 
 const Post_comment_api = router({
   many: publicProcedure
@@ -20,6 +26,10 @@ const Post_comment_api = router({
         select: Comment_listItem_CommentSelect,
       })
     }),
+
+  create: publicProcedure
+    .input(Post_commonent_create_input_zod)
+    .mutation(({ input }) => db.post_comment.create({ data: { ...input, authorId: 1 } })),
 })
 
 export default Post_comment_api
