ce1e40c426f3e08b333317718a1ae44b4259ce83
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-04-12 21:37:17 +0200

    +

--- packages/@mk-platforma/app-web/data/data.generete.toFile.ts
+++
@@ -1,6 +0,0 @@
-import generatePosts from "./data.generate"
-import { writeFileSync } from "fs"
-
-const allPosts = generatePosts()
-
-writeFileSync("./data/data.json", JSON.stringify({ allPosts }, null, 2))
