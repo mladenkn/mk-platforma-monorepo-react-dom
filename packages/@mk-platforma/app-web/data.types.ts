27123d14f0c652fd21c0bc89f289bd437b7b7ab5
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-04-13 14:30:58 +0200

    +

--- packages/@mk-platforma/app-web/data.types.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "zod"
-import { inferOutput } from "./trpc.types"
-
-export type Id = number | string
-
-export type Post_base = NonNullable<inferOutput["post"]["single"]>
--- packages/@mk-platforma/app-web/trpc.types.ts
+++
@@ -1,5 +0,0 @@
-import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server"
-import type { ApiRouter_type } from "./trpc.router"
-
-export type inferInput = inferRouterInputs<ApiRouter_type>
-export type inferOutput = inferRouterOutputs<ApiRouter_type>
