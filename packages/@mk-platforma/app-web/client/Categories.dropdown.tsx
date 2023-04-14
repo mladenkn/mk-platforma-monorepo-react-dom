84daba72e681124ad20f09c77bece4aa68190cc9
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-04-14 21:29:02 +0200

    +

--- packages/@mk-platforma/app-web/client/Categories.dropdown.tsx
+++ packages/@mk-platforma/app-web/client/Categories.dropdown.tsx
@@ -1,12 +1,4 @@
-import {
-  ThemeProvider,
-  createTheme,
-  Autocomplete,
-  Box,
-  TextField,
-  SxProps,
-  useTheme,
-} from "@mui/material"
+import { Autocomplete, Box, TextField, SxProps, useTheme } from "@mui/material"
 import { CategoryIcon, getCategoryLabel } from "./Categories.common"
 import React, { ReactElement } from "react"
 import Api from "./trpc.client"
@@ -46,53 +38,51 @@ export default function CategoryDropdown({
     value && categories.data ? getCategoryOption(findCategory(value)!) : undefined
 
   return (
-    <ThemeProvider theme={createTheme({ spacing: 8 })}>
-      <Autocomplete
-        fullWidth
-        sx={{
-          ".MuiAutocomplete-popupIndicator": {
-            mb: 2,
-          },
-          ".MuiAutocomplete-clearIndicator": {
-            mb: 2,
-          },
-          ".MuiAutocomplete-popupIndicator svg": {
-            fontSize: typography.h4,
-          },
-          ".MuiAutocomplete-clearIndicator svg": {
-            fontSize: typography.h4,
-          },
-          ...sx,
-        }}
-        loading={categories.isLoading}
-        options={categories.data?.map(getCategoryOption) || []}
-        renderOption={(props, option) => (
-          <Box component="li" sx={{ "& > img": { mr: 2, flexShrink: 0 } }} {...props}>
-            <CategoryIcon fontSize="medium" name={option.dbLabel} sx={{ mr: 2 }} />
-            {option.label}
-          </Box>
-        )}
-        renderInput={params => (
-          <TextField
-            {...params}
-            variant="outlined"
-            InputProps={{
-              ...params.InputProps,
-              startAdornment:
-                value && categories.data ? (
-                  <CategoryIcon sx={{ ml: 1, mr: 1.5 }} name={findCategory(value)!.label} />
-                ) : (
-                  <></>
-                ),
-            }}
-            placeholder="Kategorija"
-          />
-        )}
-        value={value_option || null}
-        onChange={(e, value) => onChange(value?.id)}
-        isOptionEqualToValue={(option, value) => option.id === value.id}
-        {...props}
-      />
-    </ThemeProvider>
+    <Autocomplete
+      fullWidth
+      sx={{
+        ".MuiAutocomplete-popupIndicator": {
+          mb: 2,
+        },
+        ".MuiAutocomplete-clearIndicator": {
+          mb: 2,
+        },
+        ".MuiAutocomplete-popupIndicator svg": {
+          fontSize: typography.h4,
+        },
+        ".MuiAutocomplete-clearIndicator svg": {
+          fontSize: typography.h4,
+        },
+        ...sx,
+      }}
+      loading={categories.isLoading}
+      options={categories.data?.map(getCategoryOption) || []}
+      renderOption={(props, option) => (
+        <Box component="li" sx={{ "& > img": { mr: 2, flexShrink: 0 } }} {...props}>
+          <CategoryIcon fontSize="medium" name={option.dbLabel} sx={{ mr: 2 }} />
+          {option.label}
+        </Box>
+      )}
+      renderInput={params => (
+        <TextField
+          {...params}
+          variant="outlined"
+          InputProps={{
+            ...params.InputProps,
+            startAdornment:
+              value && categories.data ? (
+                <CategoryIcon sx={{ ml: 1, mr: 1.5 }} name={findCategory(value)!.label} />
+              ) : (
+                <></>
+              ),
+          }}
+          placeholder="Kategorija"
+        />
+      )}
+      value={value_option || null}
+      onChange={(e, value) => onChange(value?.id)}
+      isOptionEqualToValue={(option, value) => option.id === value.id}
+      {...props}
+    />
   )
 }
