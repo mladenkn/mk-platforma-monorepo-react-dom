1451416c5fff958e8d8001ca8882371891774e2b
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-04-14 17:19:30 +0200

    +

--- packages/@mk-platforma/app-web/api/Location.api.ts
+++ packages/@mk-platforma/app-web/api/Location.api.ts
@@ -2,7 +2,7 @@ import { publicProcedure, router } from "../trpc.server.utils"
 import { Client } from "@googlemaps/google-maps-services-js"
 import { z } from "zod"
 import { Location, Prisma, PrismaClient } from "@prisma/client"
-import { asNonNil } from "@mk-libs/common/common"
+import { asNonNil, shallowPick } from "@mk-libs/common/common"
 
 const client = new Client({})
 
@@ -17,20 +17,29 @@ const Location_api = router({
       if (input.query) {
         const locations_googleSearch = await client
           .textSearch({
-            params: { query: input.query!, key: "AIzaSyAlZmjA7GGwjG2A6b2lo6RmWE5FbIKu8eQ" },
+            params: {
+              query: input.query!,
+              key: "AIzaSyAlZmjA7GGwjG2A6b2lo6RmWE5FbIKu8eQ",
+            },
           })
           .then(r =>
             r.data.results
               .filter(
-                p => p.place_id && p.geometry?.location.lng && p.geometry?.location.lat && p.name
+                p =>
+                  p.place_id &&
+                  p.geometry?.location.lng &&
+                  p.geometry?.location.lat &&
+                  p.name &&
+                  p.types?.includes("locality" as any)
               )
-              .map((p, i) => ({
+              .map(p => ({
                 google_id: asNonNil(p.place_id),
                 name: asNonNil(p.name),
                 longitude: new Prisma.Decimal(p.geometry?.location.lng!),
                 latitude: new Prisma.Decimal(p.geometry?.location.lat!),
               }))
           )
+        console.log(locations_googleSearch)
         const locations_saved = upsertLocations(ctx.db, locations_googleSearch)
         return locations_saved
       } else {
