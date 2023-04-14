443c84cdef6232c60ea10efa3d746f95b063b135
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-04-14 16:15:49 +0200

    +

--- packages/@mk-platforma/app-web/api/Location.api.ts
+++ packages/@mk-platforma/app-web/api/Location.api.ts
@@ -1,14 +1,31 @@
 import { publicProcedure, router } from "../trpc.server.utils"
+import { Client } from "@googlemaps/google-maps-services-js"
+import { z } from "zod"
+
+const client = new Client({})
 
 const Location_api = router({
-  many: publicProcedure.query(({ ctx }) =>
-    ctx.db.location.findMany({
-      select: {
-        id: true,
-        name: true,
-      },
-    })
-  ),
+  many: publicProcedure
+    .input(
+      z.object({
+        query: z.string().optional(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      if (input.query) {
+        const places_fromGoogleSearch = await client.textSearch({
+          params: { query: input.query! },
+          key: "",
+        })
+      } else {
+        return ctx.db.location.findMany({
+          select: {
+            id: true,
+            name: true,
+          },
+        })
+      }
+    }),
 })
 
 export default Location_api
