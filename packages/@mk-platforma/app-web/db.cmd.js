c572c57e6c2790336a8c4ff6cbb803b4ae9805b7
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-04-14 11:59:49 +0200

    +

--- packages/@mk-platforma/app-web/db.cmd.js
+++ packages/@mk-platforma/app-web/db.cmd.js
@@ -1,35 +1,32 @@
 const commandLineArgs = require('command-line-args')
 const { execSync } = require('child_process');
 
+const firstParse = commandLineArgs([{ name: 'command', defaultOption: true }], { stopAtFirstUnknown: true })
 
-const command = process.argv[2]
-
-const options = commandLineArgs([
-  { name: 'env', type: String }
-])
-
-const connectionString = (() => {
-  switch(options.name){
-    case 'dev': return 'postgresql://postgres:postgres@localhost:5432/za_brata?schema=public'
-    case 'test': return 'postgresql://postgres:s9Z4LVTQYpYvGdLdhzyJ@containers-us-west-3.railway.app:7548/railway'
-    default:
-      console.log('Unsupported env')
-  }
-})()
-
-switch(command){
+switch(firstParse.command){
   case 'prisma':
-    run(`DATABASE_URL='${connectionString}' prisma`)
+    const { env, _unknown } = commandLineArgs([{ name: 'env', type: String }], { argv: firstParse._unknown, stopAtFirstUnknown: true })
+    const connectionString = getConnectionString(env)
+    run(`DATABASE_URL=${connectionString} prisma ${_unknown.join(' ')}`)
     break;
   default:
     console.log('Unsupported subcommand')
 }
 
+function getConnectionString(env){
+  switch(env){
+    case 'dev': return 'postgresql://postgres:postgres@localhost:5432/za_brata?schema=public'
+    case 'test': return 'postgresql://postgres:s9Z4LVTQYpYvGdLdhzyJ@containers-us-west-3.railway.app:7548/railway'
+    default:
+      console.log(`Unsupported env: ${env}`)
+  }
+}
+
 function run(cmd){
   try {
-    const stdout = execSync(cmd).toString();
-    console.log(`stdout: ${stdout}`);
+    const stdout = execSync(cmd).toString()
+    console.log(stdout)
   } catch (error) {
-    console.error(`Error executing the command: ${error.message}`);
+    console.error(`Error executing the command: ${error.message}`)
   }
 }
