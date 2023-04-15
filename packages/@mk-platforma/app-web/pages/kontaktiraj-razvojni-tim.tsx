020b1aa01d3b82b49772cc763f2a5b18157b8faa
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-04-15 22:02:34 +0200

    +

--- packages/@mk-platforma/app-web/pages/kontaktiraj-razvojni-tim.tsx
+++ packages/@mk-platforma/app-web/pages/kontaktiraj-razvojni-tim.tsx
@@ -1,10 +1,2 @@
-import { Box, Typography } from "@mui/material"
-
-export default function DevContact() {
-  return (
-    <Box>
-      <Typography>Razvojni programer: Mladen Knezović</Typography>
-      <Typography>Kontakt: mladen.knezovic.1993@gmail.com</Typography>
-    </Box>
-  )
-}
+import DevContact from "../client/DevContact"
+export default DevContact
