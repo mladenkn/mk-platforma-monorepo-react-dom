f4fd2338af124867bf70280df290069ab11f9efb
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-04-18 22:42:18 +0200

    bottom-sheet-query +

--- packages/@mk-platforma/app-web/api/Post.Category.api.ts
+++ packages/@mk-platforma/app-web/api/Post.Category.api.ts
@@ -1,10 +1,18 @@
+import { z } from "zod"
 import { Categories_selector_aside_Category_queryParams } from "../client/Categories.selector.aside"
 import { publicProcedure, router } from "../trpc.server.utils"
 
 const Post_Category_api = router({
-  many: publicProcedure.query(({ ctx }) => {
-    return ctx.db.post_category.findMany({
-      where: {
+  many: publicProcedure
+    .input(
+      z
+        .object({
+          search: z.string().optional(),
+        })
+        .optional()
+    )
+    .query(({ ctx, input }) => {
+      const hasPosts = {
         OR: [
           {
             posts: {
@@ -21,10 +29,29 @@ const Post_Category_api = router({
             },
           },
         ],
-      },
-      ...Categories_selector_aside_Category_queryParams,
-    })
-  }),
+      }
+      return ctx.db.post_category.findMany({
+        where: {
+          ...hasPosts,
+          posts: {
+            some: {
+              OR: [
+                {
+                  title: { contains: input?.search },
+                },
+                {
+                  description: { contains: input?.search },
+                },
+                {
+                  contact: { contains: input?.search },
+                },
+              ],
+            },
+          },
+        },
+        ...Categories_selector_aside_Category_queryParams,
+      })
+    }),
 })
 
 export default Post_Category_api
