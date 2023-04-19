913638f3308103fe8dd6a43ed3ff4a925bdd40b4
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-04-19 21:30:36 +0200

    reverted categories selector

--- packages/@mk-platforma/app-web/pages/index.tsx
+++ packages/@mk-platforma/app-web/pages/index.tsx
@@ -13,6 +13,7 @@ export async function getServerSideProps({ query }: GetServerSidePropsContext) {
   const props: PostList_section_Props = {
     selectedCategory_initial: category,
     posts_initial: await api.post.many({ categories: category ? [category.id] : undefined }),
+    categories_initial: await api.post.category.many(),
   }
   return {
     props,
