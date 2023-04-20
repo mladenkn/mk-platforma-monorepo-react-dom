0eeb2e6dfe1b3fe268539d090d42226777e23f41
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-04-20 03:24:41 +0200

    yyy-db-trpc-advanced +

--- packages/@mk-platforma/app-web/api/Post.api.abstract.ts
+++ packages/@mk-platforma/app-web/api/Post.api.abstract.ts
@@ -2,24 +2,25 @@ import { z } from "zod"
 import { Post_queryChunks_search } from "./Post.api"
 import { SuperData_query } from "../SuperData"
 
-const input_zod = z.object({
-  categories: z.array(z.number()).optional(),
-  search: z.string().optional(),
-})
-
 export const Post_api_abstract = {
-  list: SuperData_query(input_zod, async ({ db }, input) => {
-    return {
-      where: {
-        categories: input.categories?.length
-          ? {
-              some: {
-                OR: [{ id: input.categories[0] }, { parent_id: input.categories[0] }],
-              },
-            }
-          : undefined,
-        ...(input?.search ? Post_queryChunks_search(input.search) : {}),
-      },
+  list: SuperData_query(
+    z.object({
+      categories: z.array(z.number()).optional(),
+      search: z.string().optional(),
+    }),
+    async ({ db }, input) => {
+      return {
+        where: {
+          categories: input.categories?.length
+            ? {
+                some: {
+                  OR: [{ id: input.categories[0] }, { parent_id: input.categories[0] }],
+                },
+              }
+            : undefined,
+          ...(input?.search ? Post_queryChunks_search(input.search) : {}),
+        },
+      }
     }
-  }),
+  ),
 }
