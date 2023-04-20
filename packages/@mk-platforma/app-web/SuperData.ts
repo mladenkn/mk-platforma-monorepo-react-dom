b0273904f2385134dbb18c9a208fd01ee9c52297
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-04-20 03:19:53 +0200

    yyy-db-trpc-advanced +

---
+++ packages/@mk-platforma/app-web/SuperData.ts
@@ -0,0 +1,22 @@
+import { z } from "zod"
+import { Api_context } from "./trpc.server"
+import { publicProcedure } from "./trpc.server.utils"
+
+export function SuperData_query<TInput, TFirstOutput>(
+  input_zod: z.ZodType<TInput>,
+  mapFirst: (ctx: Api_context, input: TInput) => Promise<TFirstOutput>
+) {
+  return function <TSecondOutput>(
+    mapSecond: (
+      ctx: Api_context,
+      input: TInput,
+      firstOutput: TFirstOutput
+    ) => Promise<TSecondOutput>
+  ) {
+    return publicProcedure.input(input_zod).query(async ({ ctx, input }) => {
+      const mapped1 = await mapFirst(ctx, input as any)
+      const mapped2 = mapSecond(ctx, input as any, mapped1)
+      return mapped2 as TSecondOutput
+    })
+  }
+}
--- packages/@mk-platforma/app-web/api/Post.api.advanced.2.ts
+++ packages/@mk-platforma/app-web/api/Post.api.advanced.2.ts
@@ -1,26 +1,6 @@
 import { z } from "zod"
-import { Api_context } from "../trpc.server"
-import { publicProcedure } from "../trpc.server.utils"
 import { Post_queryChunks_search } from "./Post.api"
-
-export function SuperData_query<TInput, TFirstOutput>(
-  input_zod: z.ZodType<TInput>,
-  mapFirst: (ctx: Api_context, input: TInput) => Promise<TFirstOutput>
-) {
-  return function <TSecondOutput>(
-    mapSecond: (
-      ctx: Api_context,
-      input: TInput,
-      firstOutput: TFirstOutput
-    ) => Promise<TSecondOutput>
-  ) {
-    return publicProcedure.input(input_zod).query(async ({ ctx, input }) => {
-      const mapped1 = await mapFirst(ctx, input as any)
-      const mapped2 = mapSecond(ctx, input as any, mapped1)
-      return mapped2 as TSecondOutput
-    })
-  }
-}
+import { SuperData_query } from "../SuperData"
 
 const input_zod = z.object({
   categories: z.array(z.number()).optional(),
@@ -43,7 +23,3 @@ export const Post_api_abstract = {
     }
   }),
 }
-
-// const Post_api_ss = Post_api.createCaller({ db: prisma_dbInstance, userId: 1 })
-
-// const posts = Post_api_ss.post.list({})
