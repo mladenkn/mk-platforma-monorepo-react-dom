34b9d9938611a49ecffcf64815c859292f29144a
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-04-24 20:24:21 +0200

    user-profile +

---
+++ packages/@mk-platforma/app-web/client/User.profile.tsx
@@ -0,0 +1,7 @@
+import { Box } from "@mui/material"
+
+type Props = {}
+
+export default function User_profile({}: Props) {
+  return <Box>User profile</Box>
+}
---
+++ packages/@mk-platforma/app-web/pages/user/[id].ts
@@ -0,0 +1,3 @@
+import User_profile from "../../client/User.profile"
+
+export default User_profile
