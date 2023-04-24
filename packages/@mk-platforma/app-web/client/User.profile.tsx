db7ccce27272b86a34b6f94e0b2083841db9f936
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-04-24 20:39:13 +0200

    user-profile +

--- packages/@mk-platforma/app-web/client/User.profile.tsx
+++ packages/@mk-platforma/app-web/client/User.profile.tsx
@@ -1,7 +1,18 @@
-import { Box } from "@mui/material"
+import { Box, SxProps, Container, Paper } from "@mui/material"
+import { Header_full_common } from "./Header"
+import React from "react"
 
-type Props = {}
+type Props = {
+  sx?: SxProps
+}
 
-export default function User_profile({}: Props) {
-  return <Box>User profile</Box>
+export default function Post_create_section({ sx }: Props) {
+  return (
+    <Box sx={{ display: "flex", flexDirection: "column", width: "100%", ...sx }}>
+      <Header_full_common />
+      <Container sx={{ px: 0 }} maxWidth="lg">
+        <Paper>user profile</Paper>
+      </Container>
+    </Box>
+  )
 }
