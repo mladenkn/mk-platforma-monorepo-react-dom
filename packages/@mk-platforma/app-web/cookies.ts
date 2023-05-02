31eb388597528cbb42bc04f91b655a22c81ebb60
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-05-03 00:03:39 +0200

    cookies +

--- packages/@mk-platforma/app-web/cookies.ts
+++ packages/@mk-platforma/app-web/cookies.ts
@@ -1,5 +1,42 @@
 import useCookie_ from "react-use-cookie"
 import cookie from "cookie"
+import { z, ZodNumber, ZodString } from "zod"
+import { match, P } from "ts-pattern"
+import { useCookies } from "react-cookie"
+
+const Cookies_zod = z.object({
+  Post_list__location: z.number().nullable(),
+  Post_list__location_radius: z.number().nullable(),
+})
+
+type Cookies = z.infer<typeof Cookies_zod>
+
+function useCookie2<TName extends keyof Cookies>(
+  name: TName,
+  defaultValue_: string | number | undefined | null
+) {
+  const defaultValue = match(defaultValue_)
+    .with(P.string, v => v)
+    .with(P.number, v => v.toString())
+    .with(P._, v => v)
+    .run()
+
+  const [cookies, setCookie] = useCookies([name])
+  const value_ = cookies[name]
+
+  // const [value_, setValue_] = useCookie_(name, defaultValue)
+
+  const b = match(Cookies_zod.shape[name])
+    .with(
+      P.when(v => v instanceof ZodNumber),
+      () => parseInt(value_)
+    )
+    .with(
+      P.when(v => v instanceof ZodString),
+      () => value_
+    )
+    .run()
+}
 
 type CookieName = "Post_list__location" | "Post_list__location_radius"
 
