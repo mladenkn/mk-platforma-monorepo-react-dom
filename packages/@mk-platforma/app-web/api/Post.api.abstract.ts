d1ca3794942111739a1be636bf8b5da531de6138
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-05-02 13:20:42 +0200

    location-query

--- packages/@mk-platforma/app-web/api/Post.api.abstract.ts
+++ packages/@mk-platforma/app-web/api/Post.api.abstract.ts
@@ -9,6 +9,9 @@ export const Post_api_abstract = {
       search: z.string().optional(),
     }),
     async ({ db }, input) => {
+      const locations = await db.$queryRaw<{ id: number }[]>`
+        SELECT id FROM "Location" WHERE ST_DWithin(ST_MakePoint(longitude, latitude), ST_MakePoint(16.4435148, 43.5147118)::geography, 50 * 1000)
+      `
       return {
         where: {
           categories: input.categories?.length
@@ -19,6 +22,11 @@ export const Post_api_abstract = {
               }
             : undefined,
           ...(input?.search ? Post_queryChunks_search(input.search) : {}),
+          location: {
+            id: {
+              in: locations.map(({ id }) => id),
+            },
+          },
         },
       }
     }
