d7bddefbace976dccfddd77cd3424fb6b7c9f1da
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-05-03 21:45:19 +0200

    cookies +

--- packages/@mk-platforma/app-web/cookies.ts
+++ packages/@mk-platforma/app-web/cookies.ts
@@ -1,8 +1,6 @@
-import { useState } from "react"
 import useCookie from "react-use-cookie"
 import { match, P } from "ts-pattern"
 import { z } from "zod"
-import { cookieString_to_object, object_to_cookieString } from "./cookies.convert"
 import cookie from "cookie"
 
 const Cookies_zod = z.object({
@@ -10,40 +8,6 @@ const Cookies_zod = z.object({
   Post_list__location_radius: z.number().nullable(),
 })
 type Cookies = z.infer<typeof Cookies_zod>
-
-export function use_cookie_2<TName extends keyof Cookies>(
-  name: TName,
-  defaultValue?: Cookies[TName]
-) {
-  const [value, setValue_] = useState<Cookies[TName]>(() => {
-    if (typeof window !== "undefined")
-      return (cookieString_to_object(document.cookie)[name] ?? defaultValue) as Cookies[TName]
-    else return defaultValue as Cookies[TName]
-  })
-
-  // možda bi treba postavit state na prvom renderu na klijentu?
-
-  function setValue(value: Cookies[TName]) {
-    const cookies_all_obj = cookieString_to_object(document.cookie)
-    const cookies_all_obj__updated = { ...cookies_all_obj, [name]: value }
-    document.cookie = object_to_cookieString(cookies_all_obj__updated)
-    setValue_(value)
-  }
-
-  return [value, setValue] as [typeof value, typeof setValue]
-}
-
-function mapValue(value: string | number | undefined | null) {
-  return match(value)
-    .with("undefined", () => undefined)
-    .with("null", () => null)
-    .with(
-      P.when(v => typeof v === "string" && isStringANumber(v)),
-      parseFloat
-    )
-    .with(P.union(P.nullish, P.string, P.number), v => v)
-    .exhaustive()
-}
 
 export function getCookie_ss<TName extends keyof Cookies>(allCookies: string, cookieName: TName) {
   const allCookies_parsed = cookie.parse(allCookies)
@@ -67,6 +31,18 @@ export function use_cookie<TName extends keyof Cookies>(
   }
 
   return [value_mapped, setValue] as [typeof value_mapped, typeof setValue]
+}
+
+function mapValue(value: string | number | undefined | null) {
+  return match(value)
+    .with("undefined", () => undefined)
+    .with("null", () => null)
+    .with(
+      P.when(v => typeof v === "string" && isStringANumber(v)),
+      parseFloat
+    )
+    .with(P.union(P.nullish, P.string, P.number), v => v)
+    .exhaustive()
 }
 
 function isStringANumber(str: string) {
