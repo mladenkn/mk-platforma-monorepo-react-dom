b3ec30ad6f0a599e55693f748b0ee61a967f0922
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-05-04 14:16:27 +0200

    location-select-refac +

--- packages/@mk-platforma/app-web/client/Location.select.screen.tsx
+++ packages/@mk-platforma/app-web/client/Location.select.screen.tsx
@@ -8,48 +8,45 @@ import SearchIcon from "@mui/icons-material/Search"
 import CloseIcon from "@mui/icons-material/Close"
 import React, { useState } from "react"
 import DoneIcon from "@mui/icons-material/Done"
+import { match, P } from "ts-pattern"
 
 type Props = {
-  selectedLocation: number | null
-  set_selectedLocation(l: number | null): void
-  selectedLocation_radius_km: number | null
-  set__selectedLocation_radius_km(v: number | null): void
-  onClose(): void
+  location_initial: number | null
+  location_radius_initial: number | null
+  onBack(): void
+  onDone(location: number | null, location_radius: number | null): void
 }
 
 export default function Location_select_screen({
-  selectedLocation: selectedLocation_id,
-  set_selectedLocation,
-  selectedLocation_radius_km,
-  set__selectedLocation_radius_km,
-  onClose,
+  location_initial,
+  location_radius_initial,
+  onBack,
+  onDone,
 }: Props) {
+  const [location_id, set_location] = useState<number | null>(location_initial)
+  const [location_radius, set_location_radius] = useState<number | null>(location_radius_initial)
+
   const [location_search, set__location_search] = useState("")
   const location_suggestions = Api.location.many.useQuery({ query: location_search })
   const selectedLocation = Api.location.single.useQuery(
-    { id: selectedLocation_id! },
-    { enabled: !!selectedLocation_id }
+    { id: location_id! },
+    { enabled: !!location_id }
   )
 
   const { typography } = useTheme()
 
-  function handleClose() {
-    set_selectedLocation(null)
-    onClose()
-  }
-
   function handle_location_unselect() {
-    set_selectedLocation(null)
-    set__selectedLocation_radius_km(null)
+    set_location(null)
+    set_location_radius(null)
   }
 
-  const selectedLocation_input_radius_value = selectedLocation_id
-    ? selectedLocation_radius_km ?? 50
-    : ""
+  const selectedLocation_input_radius_value = location_id ? location_radius ?? 50 : ""
 
-  function handle_setRadius(radius: string) {
-    const parsed = parseInt(radius)
-    if (parsed > 0) set__selectedLocation_radius_km(parsed)
+  function handleDone() {
+    // match({ selectedLocation_id, selectedLocation_radius })
+    //   .with({ selectedLocation_id: null }, () => onDone(null, null) )
+    //   .with({ selectedLocation_id: P.number, selectedLocation_radius: P.union(P.nullish, P.when(radius => radius !== null && radius < 1)) }, () => {})
+    //   .with({ selectedLocation_id: P.number, selectedLocation_radius: P.union(P.number, P.when(radius => radius > 0)) }, () => onDone())
   }
 
   return (
@@ -65,13 +62,13 @@ export default function Location_select_screen({
           }}
           maxWidth="md"
         >
-          <IconButton onClick={handleClose}>
+          <IconButton onClick={onBack}>
             <ArrowBackIosOutlinedIcon sx={{ color: "white" }} />
           </IconButton>
           <Typography sx={{ color: "white" }} variant="h4">
             Odaberi lokaciju
           </Typography>
-          <IconButton onClick={onClose}>
+          <IconButton onClick={handleDone}>
             <DoneIcon sx={{ color: "white" }} />
           </IconButton>
         </Container>
@@ -95,7 +92,7 @@ export default function Location_select_screen({
               ) : undefined
             }
             value={selectedLocation_input_radius_value}
-            onChange={e => handle_setRadius(e.target.value)}
+            onChange={e => set_location_radius(parseInt(e.target.value))}
           />
         </Box>
         {selectedLocation.data && (
@@ -121,12 +118,12 @@ export default function Location_select_screen({
             }}
           >
             {location_suggestions.data
-              ?.filter(l => l.id !== selectedLocation_id)
+              ?.filter(l => l.id !== location_id)
               .map(location => (
                 <Box
                   key={location.id}
                   sx={{ display: "flex", gap: 1, alignItems: "center", cursor: "pointer" }}
-                  onClick={() => set_selectedLocation(location.id)}
+                  onClick={() => set_location(location.id)}
                 >
                   <ArrowRightIcon />
                   <Typography variant="h4" key={location.id}>
