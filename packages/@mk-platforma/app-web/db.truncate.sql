89a2bd25a3739b5fa2521a6dd0b2137b2d5bc6c8
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-05-11 13:18:01 +0200

    +

--- packages/@mk-platforma/app-web/db.truncate.sql
+++ packages/@mk-platforma/app-web/db.truncate.sql
@@ -2,7 +2,7 @@ DO $$
 DECLARE 
     table_name text; 
 BEGIN 
-    FOR table_name IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public') 
+    FOR table_name IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public' AND tablename NOT IN ('spatial_ref_sys')) 
     LOOP 
         EXECUTE 'TRUNCATE TABLE ' || quote_ident(table_name) || ' CASCADE';
     END LOOP; 
