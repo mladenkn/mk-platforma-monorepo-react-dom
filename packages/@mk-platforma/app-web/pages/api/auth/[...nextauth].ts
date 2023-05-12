7101307c3e2e8483d751f878df2b01fec9dfb992
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-05-12 22:55:41 +0200

    next-auth-email +

--- packages/@mk-platforma/app-web/pages/api/auth/[...nextauth].ts
+++ packages/@mk-platforma/app-web/pages/api/auth/[...nextauth].ts
@@ -8,7 +8,6 @@ import { avatarStyles } from "../../../api/User.api"
 import { getRandomElement } from "@mk-libs/common/array"
 
 export const authOptions = {
-  // Configure one or more authentication providers
   adapter: eva(() => {
     const adapter = PrismaAdapter(db)
     const createUseer_wrapped = adapter.createUser
@@ -22,9 +21,8 @@ export const authOptions = {
     EmailProvider({
       server:
         "smtp://apikey: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@smtp.sendgrid.net",
-      from: "mladen.knezovic.1993@gmail.com",
+      from: "zabrata.app.login@gmail.com",
     }),
-    // ...add more providers here
   ],
 }
 
