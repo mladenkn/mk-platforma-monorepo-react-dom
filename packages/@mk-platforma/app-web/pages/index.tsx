500e192a3730249e6c1daf03851d4d686e4c5e44
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-05-13 23:30:28 +0200

    next-auth-email +

--- packages/@mk-platforma/app-web/pages/index.tsx
+++ packages/@mk-platforma/app-web/pages/index.tsx
@@ -1,6 +1,6 @@
 import { Post_list_page_data_initial } from "../api/Post.list.page.data.initial"
 import PostList_section from "../client/Post.list.section"
-import { create_getServerSideProps } from "../create.page"
 
-export const getServerSideProps = create_getServerSideProps(Post_list_page_data_initial)
+// export const getServerSideProps = create_getServerSideProps(Post_list_page_data_initial)
+export const getServerSideProps = Post_list_page_data_initial
 export default PostList_section
