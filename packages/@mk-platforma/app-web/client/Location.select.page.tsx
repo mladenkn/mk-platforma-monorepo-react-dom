3a7119cae6b9fefba84a143102478002ecedc059
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-05-14 21:35:09 +0200

    layout +

--- packages/@mk-platforma/app-web/client/Location.select.page.tsx
+++ packages/@mk-platforma/app-web/client/Location.select.page.tsx
@@ -1,6 +1,5 @@
-import { IconButton, Typography, Input, useTheme, Container } from "@mui/material"
+import { IconButton, Typography, Input, useTheme } from "@mui/material"
 import { Box } from "@mui/system"
-import { Header_root } from "./Header"
 import ArrowBackIosOutlinedIcon from "@mui/icons-material/ArrowBackIosOutlined"
 import ArrowRightIcon from "@mui/icons-material/ArrowRight"
 import Api from "../api.client"
@@ -12,6 +11,7 @@ import { useFormik } from "formik"
 import { z } from "zod"
 import { toFormikValidationSchema } from "zod-formik-adapter"
 import { match, P } from "ts-pattern"
+import Layout from "./Layout"
 
 type Props = {
   location_initial: number | null
@@ -77,18 +77,9 @@ export default function Location_select_page({
   }
 
   return (
-    <Box>
-      <Header_root sx={{}}>
-        <Container
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-            pl: 1,
-            pr: 1.5,
-          }}
-          maxWidth="md"
-        >
+    <Layout
+      header={
+        <Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-between", py: 2 }}>
           <IconButton onClick={onBack}>
             <ArrowBackIosOutlinedIcon sx={{ color: "white" }} />
           </IconButton>
@@ -98,86 +89,90 @@ export default function Location_select_page({
           <IconButton onClick={handleDone}>
             <DoneIcon sx={{ color: "white" }} />
           </IconButton>
-        </Container>
-      </Header_root>
-      <Container sx={{ px: 2, mt: 2, mb: 2 }} maxWidth="md">
-        <Box sx={{ display: "flex", gap: 3 }}>
-          <Input
-            sx={{ mb: 3, width: "100%", fontSize: typography.h5, flex: 4.55 }}
-            placeholder="Pretraži"
-            value={location_search}
-            onChange={e => set__location_search(e.target.value)}
-            startAdornment={<SearchIcon sx={{ mr: 1.5 }} />}
-          />
-          <Input
-            sx={{ flex: 1, mb: 3, fontSize: typography.h5 }}
-            placeholder="Radius"
-            type="number"
-            endAdornment={
-              selectedLocation_input_radius_value ? (
-                <Typography color="text.secondary">km</Typography>
-              ) : undefined
-            }
-            value={selectedLocation_input_radius_value}
-            name="radius"
-            onChange={form.handleChange}
-            inputProps={{
-              min: 1,
-            }}
-          />
         </Box>
-        {!form.isValid && (
-          <Box sx={{ mb: 4 }}>
-            <Typography variant="h5">Problemi sa unosom:</Typography>
-            <Box sx={{ ml: 2 }}>
-              {form.errors.location && <Typography variant="h5">{form.errors.location}</Typography>}
-              {form.errors.radius && (
-                <Typography sx={{ fontSize: 22, color: "red" }}>
-                  Radius: {form.errors.radius}
-                </Typography>
-              )}
-            </Box>
+      }
+      content={
+        <Box sx={{ px: 2, mt: 2, mb: 2 }}>
+          <Box sx={{ display: "flex", gap: 3 }}>
+            <Input
+              sx={{ mb: 3, width: "100%", fontSize: typography.h5, flex: 4.55 }}
+              placeholder="Pretraži"
+              value={location_search}
+              onChange={e => set__location_search(e.target.value)}
+              startAdornment={<SearchIcon sx={{ mr: 1.5 }} />}
+            />
+            <Input
+              sx={{ flex: 1, mb: 3, fontSize: typography.h5 }}
+              placeholder="Radius"
+              type="number"
+              endAdornment={
+                selectedLocation_input_radius_value ? (
+                  <Typography color="text.secondary">km</Typography>
+                ) : undefined
+              }
+              value={selectedLocation_input_radius_value}
+              name="radius"
+              onChange={form.handleChange}
+              inputProps={{
+                min: 1,
+              }}
+            />
           </Box>
-        )}
-        {selectedLocation.data && (
-          <Box sx={{ display: "flex", gap: 1, alignItems: "center", mb: 4 }}>
-            <ArrowRightIcon />
-            <Typography sx={{ mr: 6 }} variant="h4" key={selectedLocation.data?.id}>
-              {selectedLocation.data?.name}
-            </Typography>
-            <DoneIcon sx={{ mr: 3 }} />
-            <IconButton onClick={handle_location_unselect}>
-              <CloseIcon />
-            </IconButton>
-          </Box>
-        )}
-        {location_suggestions.isLoading ? (
-          <Typography>Loading...</Typography>
-        ) : (
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              gap: 2,
-            }}
-          >
-            {location_suggestions.data
-              ?.filter(l => l.id !== form.values.location)
-              .map(location => (
-                <Box
-                  key={location.id}
-                  sx={{ display: "flex", gap: 1, alignItems: "center", cursor: "pointer" }}
-                  onClick={() => form.setFieldValue("location", location.id)}
-                >
-                  <ArrowRightIcon />
-                  <Typography variant="h4" key={location.id}>
-                    {location.name}
+          {!form.isValid && (
+            <Box sx={{ mb: 4 }}>
+              <Typography variant="h5">Problemi sa unosom:</Typography>
+              <Box sx={{ ml: 2 }}>
+                {form.errors.location && (
+                  <Typography variant="h5">{form.errors.location}</Typography>
+                )}
+                {form.errors.radius && (
+                  <Typography sx={{ fontSize: 22, color: "red" }}>
+                    Radius: {form.errors.radius}
                   </Typography>
-                </Box>
-              ))}
-          </Box>
-        )}
-      </Container>
-    </Box>
+                )}
+              </Box>
+            </Box>
+          )}
+          {selectedLocation.data && (
+            <Box sx={{ display: "flex", gap: 1, alignItems: "center", mb: 4 }}>
+              <ArrowRightIcon />
+              <Typography sx={{ mr: 6 }} variant="h4" key={selectedLocation.data?.id}>
+                {selectedLocation.data?.name}
+              </Typography>
+              <DoneIcon sx={{ mr: 3 }} />
+              <IconButton onClick={handle_location_unselect}>
+                <CloseIcon />
+              </IconButton>
+            </Box>
+          )}
+          {location_suggestions.isLoading ? (
+            <Typography>Loading...</Typography>
+          ) : (
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                gap: 2,
+              }}
+            >
+              {location_suggestions.data
+                ?.filter(l => l.id !== form.values.location)
+                .map(location => (
+                  <Box
+                    key={location.id}
+                    sx={{ display: "flex", gap: 1, alignItems: "center", cursor: "pointer" }}
+                    onClick={() => form.setFieldValue("location", location.id)}
+                  >
+                    <ArrowRightIcon />
+                    <Typography variant="h4" key={location.id}>
+                      {location.name}
+                    </Typography>
+                  </Box>
+                ))}
+            </Box>
+          )}
+        </Box>
+      }
+    />
   )
 }
