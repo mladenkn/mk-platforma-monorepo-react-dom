5cc5a7949b50da9bcbd4f35dacf32cb1cb4cff69
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-05-14 21:52:45 +0200

    layout +

--- packages/@mk-platforma/app-web/client/DevContact.tsx
+++ packages/@mk-platforma/app-web/client/DevContact.tsx
@@ -1,14 +1,14 @@
-import { Box, IconButton, Typography, Paper, Container } from "@mui/material"
+import { Box, IconButton, Typography, Paper } from "@mui/material"
 import { useRouter } from "next/router"
-import { Header_root, Header_moreOptions } from "./Header"
+import { Header_moreOptions } from "./Header"
 import ArrowBackIosOutlinedIcon from "@mui/icons-material/ArrowBackIosOutlined"
+import Layout from "./Layout"
 
 export default function DevContact() {
-  const goBack = useRouter().back
   return (
-    <Box>
-      <Header_root sx={{ pr: 1.5 }}>
-        <Container sx={{ display: "flex", justifyContent: "space-between" }} maxWidth="md">
+    <Layout
+      header={
+        <Box sx={{ display: "flex", justifyContent: "space-between", py: 2 }}>
           <Box
             sx={{
               display: "flex",
@@ -17,22 +17,24 @@ export default function DevContact() {
               gap: 2,
             }}
           >
-            <IconButton sx={{ color: "white" }} onClick={goBack}>
+            <IconButton sx={{ color: "white" }} onClick={useRouter().back}>
               <ArrowBackIosOutlinedIcon />
             </IconButton>
             <Typography variant="h3">Kontakt</Typography>
           </Box>
           <Header_moreOptions options={["post.create", "profile", "post.list", "devContact"]} />
-        </Container>
-      </Header_root>
-      <Container maxWidth="md" sx={{ mt: 3 }}>
-        <Paper sx={{ px: 3, py: 3 }}>
-          <Typography variant="h5" sx={{ mb: 4 }}>
-            Razvojni programer: Mladen Knezović
-          </Typography>
-          <Typography variant="h5">Kontakt: mladen.knezovic.1993@gmail.com</Typography>
-        </Paper>
-      </Container>
-    </Box>
+        </Box>
+      }
+      content={
+        <Box sx={{ mt: 3 }}>
+          <Paper sx={{ px: 3, py: 3 }}>
+            <Typography variant="h5" sx={{ mb: 4 }}>
+              Razvojni programer: Mladen Knezović
+            </Typography>
+            <Typography variant="h5">Kontakt: mladen.knezovic.1993@gmail.com</Typography>
+          </Paper>
+        </Box>
+      }
+    />
   )
 }
