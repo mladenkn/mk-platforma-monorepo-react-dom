65840d3a87e094a2336075ce956465de29d1aa49
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-05-14 21:45:04 +0200

    layout +

--- packages/@mk-platforma/app-web/client/Post.create.page.tsx
+++ packages/@mk-platforma/app-web/client/Post.create.page.tsx
@@ -1,30 +1,25 @@
-import { Box, SxProps } from "@mui/material"
 import { Header_full_common } from "./Header"
 import { useRouter } from "next/router"
 import React from "react"
 import Post_form from "./Post.form"
 import Layout from "./Layout"
 
-type Props = {
-  sx?: SxProps
-}
+type Props = {}
 
-export default function Post_create_page({ sx }: Props) {
+export default function Post_create_page({}: Props) {
   function onSubmit() {}
 
   return (
-    <Box sx={{ display: "flex", flexDirection: "column", width: "100%", ...sx }}>
-      <Layout
-        header={<Header_full_common />}
-        content={
-          <Post_form
-            sx={{ gap: 2, p: 2, mt: 1 }}
-            title="Novi oglas"
-            onSubmit={onSubmit}
-            onCancel={useRouter().back}
-          />
-        }
-      />
-    </Box>
+    <Layout
+      header={<Header_full_common />}
+      content={
+        <Post_form
+          sx={{ gap: 2, p: 2, mt: 1 }}
+          title="Novi oglas"
+          onSubmit={onSubmit}
+          onCancel={useRouter().back}
+        />
+      }
+    />
   )
 }
