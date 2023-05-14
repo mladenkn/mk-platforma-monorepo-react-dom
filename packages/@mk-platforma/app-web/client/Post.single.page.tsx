4eab7dc20456e0110ae75d57ba1516fa36ae4389
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-05-14 21:50:19 +0200

    layout +

--- packages/@mk-platforma/app-web/client/Post.single.page.tsx
+++ packages/@mk-platforma/app-web/client/Post.single.page.tsx
@@ -1,12 +1,13 @@
 import Post_single_details, { Post_single_details_PostModel } from "./Post.single.details"
 import { Header_full_common } from "./Header"
-import { Box, IconButton, Container } from "@mui/material"
+import { Box, IconButton, Typography } from "@mui/material"
 import Api from "../api.client"
 import EditIcon from "@mui/icons-material/Edit"
 import { useState } from "react"
 import { asNonNil, nullsToUndefinedDeep } from "@mk-libs/common/common"
 import React from "react"
 import Post_form from "./Post.form"
+import Layout from "./Layout"
 
 export default function Post_single_page({
   post_initial,
@@ -19,39 +20,45 @@ export default function Post_single_page({
 
   return (
     <Box>
-      <Header_full_common
-        moreOptions_props={{
-          options: ["post.create", "profile", "post.list", "devContact"],
-        }}
+      <Layout
+        header={
+          <Header_full_common
+            moreOptions_props={{
+              options: ["post.create", "profile", "post.list", "devContact"],
+            }}
+          />
+        }
+        content={
+          <Box>
+            {postQuery.isLoading ? <Typography>Učitavanje...</Typography> : <></>}
+            {!isEdit ? (
+              <Post_single_details
+                sx={{ p: 1 }}
+                {...post}
+                usePaperSections
+                editAction={
+                  <IconButton onClick={() => setIsEdit(true)}>
+                    <EditIcon />
+                  </IconButton>
+                }
+              />
+            ) : (
+              <></>
+            )}
+            {post && isEdit ? (
+              <Post_form
+                sx={{ p: 2, m: 1 }}
+                title="Uredi objavu"
+                initialValues={nullsToUndefinedDeep(post)}
+                onSubmit={() => {}}
+                onCancel={() => setIsEdit(false)}
+              />
+            ) : (
+              <></>
+            )}
+          </Box>
+        }
       />
-      <Container sx={{ px: 0 }} maxWidth="lg">
-        {postQuery.isLoading ? <>Učitavanje...</> : <></>}
-        {!isEdit ? (
-          <Post_single_details
-            sx={{ p: 1 }}
-            {...post}
-            usePaperSections
-            editAction={
-              <IconButton onClick={() => setIsEdit(true)}>
-                <EditIcon />
-              </IconButton>
-            }
-          />
-        ) : (
-          <></>
-        )}
-        {post && isEdit ? (
-          <Post_form
-            sx={{ p: 2, m: 1 }}
-            title="Uredi objavu"
-            initialValues={nullsToUndefinedDeep(post)}
-            onSubmit={() => {}}
-            onCancel={() => setIsEdit(false)}
-          />
-        ) : (
-          <></>
-        )}
-      </Container>
     </Box>
   )
 }
