02103a927b01982a0c63a2a26478b86657fc255c
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-05-17 12:33:18 +0200

    +

--- packages/@mk-platforma/app-web/client/Post.create.page.tsx
+++ packages/@mk-platforma/app-web/client/Post.create.page.tsx
@@ -3,18 +3,28 @@ import { useRouter } from "next/router"
 import React from "react"
 import Post_form from "./Post.form"
 import Layout from "./Layout"
+import { useTheme } from "@mui/material"
 
 type Props = {}
 
 export default function Post_create_page({}: Props) {
   function onSubmit() {}
 
+  const { breakpoints } = useTheme()
+
   return (
     <Layout
       header={<Header_full_common />}
       content={
         <Post_form
-          sx={{ gap: 2, p: 2, mt: 1, width: "100%" }}
+          sx={{
+            gap: 2,
+            p: 2,
+            mx: 1,
+            mt: 1,
+            width: "100%",
+            [breakpoints.up("md")]: { mx: 0 },
+          }}
           title="Novi oglas"
           onSubmit={onSubmit}
           onCancel={useRouter().back}
