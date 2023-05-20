06b5984b613dd200471183d1505d1aa7fc5e5970
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-05-20 23:12:57 +0200

    +

--- packages/@mk-platforma/app-web/domain/user/User.profile.edit.tsx
+++ packages/@mk-platforma/app-web/domain/user/User.profile.edit.tsx
@@ -1,6 +1,8 @@
 import { Box } from "@mui/material"
 import Api from "~/api/api.client"
 import { Api_outputs } from "~/api/api.infer"
+import { asNonNil, undefinedToNullsDeep } from "~/../../@mk-libs/common/common"
+import { create_getServerSideProps } from "~/create.getServerSideProps"
 
 type User = NonNullable<Api_outputs["user"]["single"]>
 
@@ -12,3 +14,11 @@ export default function User_profile_edit({ user_initial }: Props) {
   const user = Api.user.single.useQuery(user_initial.id, { initialData: user_initial })
   return <Box>User_profile_edit {user.data?.id}</Box>
 }
+
+export const User_profile_edit__getServerSideProps = create_getServerSideProps(
+  async (_, session) => ({
+    props: {
+      user_initial: undefinedToNullsDeep(asNonNil(session.user)),
+    },
+  })
+)
--- packages/@mk-platforma/app-web/pages/profile/edit.ts
+++ packages/@mk-platforma/app-web/pages/profile/edit.ts
@@ -1,11 +1,6 @@
-import { asNonNil, undefinedToNullsDeep } from "~/../../@mk-libs/common/common"
-import { create_getServerSideProps } from "~/create.getServerSideProps"
-import User_profile_edit from "~/domain/user/User.profile.edit"
+import User_profile_edit, {
+  User_profile_edit__getServerSideProps,
+} from "~/domain/user/User.profile.edit"
 
-export const getServerSideProps = create_getServerSideProps(async (_, session) => ({
-  props: {
-    user_initial: undefinedToNullsDeep(asNonNil(session.user)),
-  },
-}))
-
+export const getServerSideProps = User_profile_edit__getServerSideProps
 export default User_profile_edit
