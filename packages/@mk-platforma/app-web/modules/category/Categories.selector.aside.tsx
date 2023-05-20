676f4f25231333ed22a8ed1c0ffd2847e71b7d19
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-05-20 21:10:45 +0200

    modules +

--- packages/@mk-platforma/app-web/modules/category/Categories.selector.aside.tsx
+++ packages/@mk-platforma/app-web/modules/category/Categories.selector.aside.tsx
@@ -15,7 +15,7 @@ import React, { ReactNode } from "react"
 import ArrowBackIosOutlinedIcon from "@mui/icons-material/ArrowBackIosOutlined"
 import { eva } from "@mk-libs/common/common"
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
-import type { Api_outputs } from "~/api/api.infer"
+import type { Api_outputs } from "~/modules/api.infer"
 import { LogoLink } from "./common"
 
 export type Category_model = Api_outputs["category"]["many"][number]
