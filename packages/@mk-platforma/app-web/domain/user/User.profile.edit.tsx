c490f2033ae3db297a7c4fa8c46762dd057091dc
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-05-20 22:46:50 +0200

    +

--- packages/@mk-platforma/app-web/domain/user/User.profile.edit.tsx
+++ packages/@mk-platforma/app-web/domain/user/User.profile.edit.tsx
@@ -1,14 +1,12 @@
 import { Box } from "@mui/material"
-import Api from "~/api/api.client"
 import { Api_outputs } from "~/api/api.infer"
 
 type User = NonNullable<Api_outputs["user"]["single"]>
 
 type Props = {
-  user_initial: User
+  user: User
 }
 
-export default function User_profile_edit({ user_initial }: Props) {
-  const user = Api.user.single.useQuery(user_initial.id, { initialData: user_initial })
-  return <Box>User_profile_edit {user.data?.id}</Box>
+export default function User_profile_edit({ user }: Props) {
+  return <Box>User_profile_edit {user.id}</Box>
 }
