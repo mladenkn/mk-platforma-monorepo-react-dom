c466c002997e890ba8ebcec393c5256eeaf07ef2
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-05-20 22:50:17 +0200

    +

--- packages/@mk-libs/react-common/context.ts
+++ packages/@mk-libs/react-common/context.ts
@@ -1,7 +1,7 @@
 import { createContext as createContext_, useContext as useContext_ } from "react"
 import { asNonNil } from "@mk-libs/common/common"
 
-export function createContext<TValue>() {
+export default function createContext<TValue>() {
   const Context = createContext_<TValue | undefined>(undefined)
   const useContext = () => asNonNil(useContext_(Context))
   return [Context.Provider, useContext] as [typeof Context.Provider, typeof useContext]
