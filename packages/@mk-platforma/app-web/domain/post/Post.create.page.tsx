705fe3842ea43a9b4ae1c43a83ff5de1e4f2b454
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-05-22 16:35:21 +0200

    header-better +

--- packages/@mk-platforma/app-web/domain/post/Post.create.page.tsx
+++ packages/@mk-platforma/app-web/domain/post/Post.create.page.tsx
@@ -1,9 +1,11 @@
 import { useRouter } from "next/router"
 import React from "react"
 import Post_form from "./Post.form"
-import { useTheme } from "@mui/material"
-import { Header_full_common } from "../Header"
+import { IconButton, useTheme } from "@mui/material"
+import { Header, Header_moreOptions } from "../Header"
 import Layout from "../Layout"
+import ArrowBackIosOutlinedIcon from "@mui/icons-material/ArrowBackIosOutlined"
+import { LogoLink } from "../common"
 
 type Props = {}
 
@@ -14,7 +16,15 @@ export default function Post_create_page({}: Props) {
 
   return (
     <Layout
-      header={<Header_full_common />}
+      header={
+        <Header>
+          <IconButton sx={{ color: "white", mr: 1.5 }} onClick={useRouter().back}>
+            <ArrowBackIosOutlinedIcon />
+          </IconButton>
+          <LogoLink />
+          <Header_moreOptions exclude={["other.contact"]} />
+        </Header>
+      }
       content={
         <Post_form
           sx={{
