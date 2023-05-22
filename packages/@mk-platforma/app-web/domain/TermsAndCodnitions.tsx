6d3d331eed2fe9d498208ceba2662c2d0e56a185
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-05-22 16:49:53 +0200

    header-better +

--- packages/@mk-platforma/app-web/domain/TermsAndCodnitions.tsx
+++ packages/@mk-platforma/app-web/domain/TermsAndCodnitions.tsx
@@ -1,13 +1,18 @@
 import { Typography } from "@mui/material"
 import Layout from "./Layout"
+import { Header, Header_moreOptions, Header_home } from "./Header"
 
 export default function TermsAndCodnitions() {
   return (
     <Layout
       header={
-        <Typography variant="h3" sx={{ color: "white", py: 2 }}>
-          Uvjeti i pravila korištenja
-        </Typography>
+        <Header>
+          <Header_home />
+          <Typography variant="h3" sx={{ color: "white", py: 2 }}>
+            Uvjeti i pravila korištenja
+          </Typography>
+          <Header_moreOptions exclude={["other.privacyPolicy"]} />
+        </Header>
       }
       content={
         <Typography variant="body1" sx={{ p: 1, pr: 2 }}>
