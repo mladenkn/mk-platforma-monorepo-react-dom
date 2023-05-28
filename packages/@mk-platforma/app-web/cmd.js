cb9245fb4a555646a53dce5ae4ed8602bb127b0a
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-05-28 23:26:47 +0200

    cmd-more +

--- packages/@mk-platforma/app-web/cmd.js
+++ packages/@mk-platforma/app-web/cmd.js
@@ -33,12 +33,25 @@ const { parseCommand, run, getConnectionString } = require('./cmd.utils')
 const { Command } = require('commander')
 const program = new Command()
 
-program.command('db.prisma')
-  .option('-di --db-instance <string>', 'database instance', 'dev')
+const cs_option = ['-di --db-instance <string>', 'database instance', 'dev']
+const db_command = program.command('db')
+
+db_command
+  .command('prisma')
+  .option(...cs_option)
   .argument('<string...>', 'command passed to prisma')
   .action((str, options) => {
+    console.log(43, options)
     const cs_env = `DATABASE_URL=${getConnectionString(options.dbInstance)}`
     run(`${cs_env} prisma ${str.join(' ')}`)
+  })
+
+db_command
+  .command('seed')
+  .option(...cs_option)
+  .action(options => {
+    const cs_env = `DATABASE_URL=${getConnectionString(options.dbInstance)}`
+    run(`${cs_env} pnpm _exe-ts ./data.gen/db.seed.ts`)
   })
 
 program.parse()
