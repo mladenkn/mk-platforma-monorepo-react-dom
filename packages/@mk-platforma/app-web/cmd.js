9c23ce2684da2c07f50bf8b0a504c9a809a62d35
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-05-28 23:33:39 +0200

    cmd-more +

--- packages/@mk-platforma/app-web/cmd.js
+++ packages/@mk-platforma/app-web/cmd.js
@@ -1,34 +1,4 @@
-const { parseCommand, run, getConnectionString } = require('./cmd.utils')
-
-// const parsed = parseCommand()
-
-// const cs_env = () => `DATABASE_URL=${getConnectionString(parsed.env)}`
-
-// switch(parsed.command){
-//   case 'db.prisma':
-//     run(`${cs_env()} prisma ${parsed._unknown.join(' ')}`)
-//     break;
-  
-//   case 'db.seed':
-//     run(`${cs_env()} pnpm _exe-ts ./data.gen/db.seed.ts`)
-//     break;
-  
-//   case 'db.truncate':
-//     run(`${cs_env()} prisma db execute --file './db.truncate.sql'`)
-//     break;
-  
-//   case 'db.reset':
-//     run(
-//       `${cs_env()} prisma db execute --file './db.truncate.sql'`,
-//       `${cs_env()} prisma db push --accept-data-loss`,
-//       `${cs_env()} pnpm _exe-ts ./data.gen/db.seed.ts`,
-//     )
-//     break;
-  
-//   default:
-//     console.log('Unsupported subcommand')
-//     process.exit(1)
-// }
+const { run, getConnectionString } = require('./cmd.utils')
 
 const { Command } = require('commander')
 const program = new Command()
@@ -59,6 +29,18 @@ db_command
   .action(options => {
     const cs_env = `DATABASE_URL=${getConnectionString(options.dbInstance)}`
     run(`${cs_env} prisma db execute --file './db.truncate.sql'`)
+  })
+
+db_command
+  .command('reset')
+  .option(...cs_option)
+  .action(options => {
+    const cs_env = `DATABASE_URL=${getConnectionString(options.dbInstance)}`
+    run(
+      `${cs_env()} prisma db execute --file './db.truncate.sql'`,
+      `${cs_env()} prisma db push --accept-data-loss`,
+      `${cs_env()} pnpm _exe-ts ./data.gen/db.seed.ts`,
+    )
   })
 
 program.parse()
