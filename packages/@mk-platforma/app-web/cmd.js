36797860797f2fb7fa4a7d1cd2d7a3ff3e0710a5
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-05-28 23:14:33 +0200

    cmd-more +

--- packages/@mk-platforma/app-web/cmd.js
+++ packages/@mk-platforma/app-web/cmd.js
@@ -1,31 +1,44 @@
 const { parseCommand, run, getConnectionString } = require('./cmd.utils')
 
-const parsed = parseCommand()
+// const parsed = parseCommand()
 
-const cs_env = () => `DATABASE_URL=${getConnectionString(parsed.env)}`
+// const cs_env = () => `DATABASE_URL=${getConnectionString(parsed.env)}`
 
-switch(parsed.command){
-  case 'db.prisma':
-    run(`${cs_env()} prisma ${parsed._unknown.join(' ')}`)
-    break;
+// switch(parsed.command){
+//   case 'db.prisma':
+//     run(`${cs_env()} prisma ${parsed._unknown.join(' ')}`)
+//     break;
   
-  case 'db.seed':
-    run(`${cs_env()} pnpm _exe-ts ./data.gen/db.seed.ts`)
-    break;
+//   case 'db.seed':
+//     run(`${cs_env()} pnpm _exe-ts ./data.gen/db.seed.ts`)
+//     break;
   
-  case 'db.truncate':
-    run(`${cs_env()} prisma db execute --file './db.truncate.sql'`)
-    break;
+//   case 'db.truncate':
+//     run(`${cs_env()} prisma db execute --file './db.truncate.sql'`)
+//     break;
   
-  case 'db.reset':
-    run(
-      `${cs_env()} prisma db execute --file './db.truncate.sql'`,
-      `${cs_env()} prisma db push --accept-data-loss`,
-      `${cs_env()} pnpm _exe-ts ./data.gen/db.seed.ts`,
-    )
-    break;
+//   case 'db.reset':
+//     run(
+//       `${cs_env()} prisma db execute --file './db.truncate.sql'`,
+//       `${cs_env()} prisma db push --accept-data-loss`,
+//       `${cs_env()} pnpm _exe-ts ./data.gen/db.seed.ts`,
+//     )
+//     break;
   
-  default:
-    console.log('Unsupported subcommand')
-    process.exit(1)
-}
+//   default:
+//     console.log('Unsupported subcommand')
+//     process.exit(1)
+// }
+
+const { Command } = require('commander')
+const program = new Command()
+
+program.command('db.prisma')
+  .option('-di --db-instance <string>', 'database instance', 'dev')
+  .argument('<string...>', 'command passed to prisma')
+  .action((str, options) => {
+    const cs_env = `DATABASE_URL=${getConnectionString(options.dbInstance)}`
+    run(`${cs_env} prisma ${str.join(' ')}`)
+  })
+
+program.parse()
