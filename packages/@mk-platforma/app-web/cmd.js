b5342e9bfc6f7cf5a557a9884c5606876abac469
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-05-28 23:50:41 +0200

    cmd-more +

--- packages/@mk-platforma/app-web/cmd.js
+++ packages/@mk-platforma/app-web/cmd.js
@@ -9,18 +9,20 @@ const db_command = name => _db_command.command(name).option(...cs_option)
 
 db_command('prisma')
   .argument('<string...>', 'command passed to prisma')
-  .action((str, options) => {
+  .action((str, options) =>
     run(
       { DATABASE_URL: getConnectionString(options.dbInstance) },
       `prisma ${str.join(' ')}`
     )
-  })
+  )
 
 db_command('seed')
-  .action(options => {
-    const cs_env = `DATABASE_URL=${getConnectionString(options.dbInstance)}`
-    run(`${cs_env} pnpm _exe-ts ./data.gen/db.seed.ts`)
-  })
+  .action(options =>
+    run(
+      { DATABASE_URL: getConnectionString(options.dbInstance) },
+      `pnpm _exe-ts ./data.gen/db.seed.ts`
+    )
+  )
 
 db_command('truncate')
   .action(options => {
