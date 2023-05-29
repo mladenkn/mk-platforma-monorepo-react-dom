9bed7fab9cc3879762ab964870428582ca01f608
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-05-29 11:51:26 +0200

    cmd-more +

--- packages/@mk-platforma/app-web/cmd.js
+++ packages/@mk-platforma/app-web/cmd.js
@@ -1,49 +1,34 @@
+const { match } = require('ts-pattern')
 const { parseCommand, run, getConnectionString } = require('./cmd.utils')
 
 const parsed = parseCommand()
 const dbInstance = parsed["db-instance"]
 
-switch(parsed.command){
-  case "dev":
-    run(
-      { DATABASE_URL: getConnectionString(dbInstance) },
-      `next dev`
-    )
-    break;
+const run_args = match(parsed.command)
+  .with('dev', () => [
+    { DATABASE_URL: getConnectionString(dbInstance) },
+    `next dev`
+  ])
+  .with('db.prisma', () => [
+    { DATABASE_URL: getConnectionString(dbInstance) },
+    `prisma ${parsed._unknown.join(' ')}`
+  ])
+  .with('db.seed', () => [
+    { DATABASE_URL: getConnectionString(dbInstance) },
+    `pnpm _exe-ts ./data.gen/db.seed.ts`
+  ])
+  .with('db.truncate', () => [
+    { DATABASE_URL: getConnectionString(dbInstance) },
+    `prisma db execute --file './db.truncate.sql`
+  ])
+  .with('db.reset', () => [
+    { DATABASE_URL: getConnectionString(dbInstance) },
+    [
+      `prisma db execute --file './db.truncate.sql'`,
+      `prisma db push --accept-data-loss`,
+      `pnpm _exe-ts ./data.gen/db.seed.ts`,
+    ]
+  ])
+  .exhaustive()
 
-  case 'db.prisma':
-    run(
-      { DATABASE_URL: getConnectionString(dbInstance) },
-      `prisma ${parsed._unknown.join(' ')}`
-    )
-    break;
-  
-  case 'db.seed':
-    run(
-      { DATABASE_URL: getConnectionString(dbInstance) },
-      `pnpm _exe-ts ./data.gen/db.seed.ts`
-    )
-    break;
-  
-  case 'db.truncate':
-    run(
-      { DATABASE_URL: getConnectionString(dbInstance) },
-      `prisma db execute --file './db.truncate.sql`
-    )
-    break;
-  
-  case 'db.reset':
-    run(
-      { DATABASE_URL: getConnectionString(dbInstance) },
-      [
-        `prisma db execute --file './db.truncate.sql'`,
-        `prisma db push --accept-data-loss`,
-        `pnpm _exe-ts ./data.gen/db.seed.ts`,
-      ]
-    )
-    break;
-  
-  default:
-    console.log('Unsupported subcommand')
-    process.exit(1)
-}
+run(...run_args)
