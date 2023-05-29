1b31d9981c8a6007e2cd740411ac988b9cb420af
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-05-29 15:34:42 +0200

    cmd-more +

---
+++ packages/@mk-platforma/app-web/cmd.utils.ts
@@ -0,0 +1,99 @@
+import commandLineArgs from "command-line-args"
+import { spawn, exec } from "child_process"
+import { match, P } from "ts-pattern"
+
+const options = [
+  { name: "command", defaultOption: true },
+  { name: "db-instance", type: String },
+]
+
+export function parseCommand() {
+  return commandLineArgs(options, { stopAtFirstUnknown: true })
+}
+
+export async function run(...cmd: string[]) {
+  const [env, commands] = match(cmd)
+    .with([{} as any, P.array(P.string)], args => [args[0], args[1]])
+    .with([{} as any, P.string], args => [args[0], [args[1]]])
+    .with(P.array(P.string), cmd => [{}, cmd])
+    .with(P.string, cmd => [{}, [cmd]])
+    .run()
+
+  try {
+    for (const command of commands) {
+      const result: any = await run_single(command, env)
+      if (result.code !== 0) {
+        result.error && console.error(result.error?.message || "Error")
+        process.exit(result.code)
+      }
+    }
+  } catch (error: any) {
+    console.error(`Error ${error?.message}`)
+    process.exit(1)
+  }
+}
+
+function run_single(command: string, env: Record<string, string>) {
+  if (command.startsWith("pnpm exec ts-node")) return run_withExec(command, env)
+  else return run_withSpawn(command, env)
+}
+
+function run_withSpawn(command: string, env: Record<string, string>) {
+  const command_words = command.split(" ")
+  const cmd = spawn(command_words[0], command_words.slice(1), {
+    env: { ...process.env, ...env },
+    stdio: command_words[0] === "psql" ? "inherit" : undefined,
+  })
+  return new Promise((resolve, reject) => {
+    cmd.stdout?.on?.("data", data => process.stdout.write(data.toString()))
+    cmd.stderr?.on?.("data", data => process.stderr.write(data.toString()))
+    cmd.on?.("error", error => {
+      reject({ error, code: -1 })
+      process.stderr.write(error.message)
+    })
+    cmd.on?.("close", code => resolve({ code }))
+  })
+}
+
+function run_withExec(command: string, env: Record<string, string>) {
+  const command_ = `${objectToEnvString(env)} ${command}`
+  return new Promise((resolve, reject) => {
+    exec(command_, (error, stdout, stderr) => {
+      if (error) {
+        console.error(error)
+        reject({ error, code: -1 })
+      }
+      console.log(stdout ? stdout : stderr)
+      resolve({ code: 1 })
+    })
+  })
+}
+
+function objectToEnvString(obj: Record<string, string>) {
+  return Object.entries(obj)
+    .map(([key, value]) => `${key}=${value}`)
+    .join(" ")
+}
+
+export function getConnectionString(env: string) {
+  switch (env) {
+    case "dev":
+      return "postgresql://postgres:postgres@localhost:5432/za_brata"
+    case "test":
+      return "postgresql://postgres:s9Z4LVTQYpYvGdLdhzyJ@containers-us-west-3.railway.app:7548/railway"
+    default:
+      throw new Error(`Unsupported env: ${env}`)
+  }
+}
+
+function createCommand_exeTs(more: string) {
+  const options = JSON.stringify({
+    module: "CommonJS",
+    jsx: "react",
+    baseUrl: "./",
+    paths: {
+      "~/*": ["./*"],
+    },
+  }).replace(/"/g, '\\"')
+  return `pnpm exec ts-node -r tsconfig-paths/register --compiler-options ${options} ${more}`
+}
