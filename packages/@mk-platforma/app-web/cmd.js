52097d3f6da137ee47b477cc69626144d4879d75
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-05-29 11:57:43 +0200

    cmd-more +

--- packages/@mk-platforma/app-web/cmd.js
+++ packages/@mk-platforma/app-web/cmd.js
@@ -5,22 +5,27 @@ const parsed = parseCommand()
 const dbInstance = parsed["db-instance"]
 
 const run_args = match(parsed.command)
+  
   .with('dev', () => [
     { DATABASE_URL: getConnectionString(dbInstance) },
     `next dev`
   ])
+
   .with('db.prisma', () => [
     { DATABASE_URL: getConnectionString(dbInstance) },
     `prisma ${parsed._unknown.join(' ')}`
   ])
+
   .with('db.seed', () => [
     { DATABASE_URL: getConnectionString(dbInstance) },
     `pnpm _exe-ts ./data.gen/db.seed.ts`
   ])
+
   .with('db.truncate', () => [
     { DATABASE_URL: getConnectionString(dbInstance) },
     `prisma db execute --file './db.truncate.sql`
   ])
+
   .with('db.reset', () => [
     { DATABASE_URL: getConnectionString(dbInstance) },
     [
@@ -29,6 +34,15 @@ const run_args = match(parsed.command)
       `pnpm _exe-ts ./data.gen/db.seed.ts`,
     ]
   ])
+
+  .with('start.prod', () => [
+    {
+      DATABASE_URL: getConnectionString(dbInstance),
+      NEXTAUTH_SECRET: "FPCsMhz7xn+fdf59xGd1O0xiOqHFgxO0iU8xiWGvNxc="
+    },
+    "next start"
+  ])
+
   .exhaustive()
 
 run(...run_args)
