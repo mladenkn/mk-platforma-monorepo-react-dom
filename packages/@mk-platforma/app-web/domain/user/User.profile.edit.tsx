d36b9a51bd77cdc1816409ab650428169e3c7869
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-05-30 21:19:20 +0200

    +

--- packages/@mk-platforma/app-web/domain/user/User.profile.edit.tsx
+++ packages/@mk-platforma/app-web/domain/user/User.profile.edit.tsx
@@ -1,9 +1,10 @@
-import { Paper, Typography } from "@mui/material"
+import { IconButton, Paper, TextField, Typography, Box } from "@mui/material"
 import Api from "~/api_/api.client"
 import { Api_outputs } from "~/api_/api.infer"
 import Layout from "../Layout"
 import { Header, Header_back, Header_moreOptions } from "../Header"
 import { LogoLink } from "../common"
+import DoneIcon from "@mui/icons-material/Done"
 
 type User = NonNullable<Api_outputs["user"]["single"]>
 
@@ -23,8 +24,14 @@ export default function User_profile_edit({ user_initial }: Props) {
         </Header>
       }
       content={
-        <Paper sx={{ width: "100%", p: 2, m: 1 }}>
+        <Paper sx={{ width: "100%", p: 2, pb: 3.5, m: 1 }}>
           <Typography variant="h4">Uređivanje profila</Typography>
+          <Box sx={{ display: "flex", mt: 3 }}>
+            <TextField label="Korisničko ime" />
+            <IconButton sx={{ ml: 2 }}>
+              <DoneIcon />
+            </IconButton>
+          </Box>
         </Paper>
       }
     />
