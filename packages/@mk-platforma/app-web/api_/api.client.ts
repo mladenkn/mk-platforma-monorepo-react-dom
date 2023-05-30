a770ba308b0a70e35c8ad9b64f5b1d0b9c01afb5
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-05-30 21:58:19 +0200

    env +

--- packages/@mk-platforma/app-web/api_/api.client.ts
+++ packages/@mk-platforma/app-web/api_/api.client.ts
@@ -4,23 +4,6 @@ import type { ApiRouter_type } from "./api.root"
 import { NextPageContext } from "next"
 import superjson from "superjson"
 
-function getBaseUrl() {
-  if (typeof window !== "undefined")
-    // browser should use relative path
-    return ""
-
-  if (process.env.VERCEL_URL)
-    // reference for vercel.com
-    return `https://${process.env.VERCEL_URL}`
-
-  if (process.env.RENDER_INTERNAL_HOSTNAME)
-    // reference for render.com
-    return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`
-
-  // assume localhost
-  return `http://localhost:${process.env.PORT ?? 3000}`
-}
-
 const Api: CreateTRPCNext<ApiRouter_type, NextPageContext, unknown> =
   createTRPCNext<ApiRouter_type>({
     config({ ctx }) {
@@ -31,7 +14,7 @@ const Api: CreateTRPCNext<ApiRouter_type, NextPageContext, unknown> =
              * If you want to use SSR, you need to use the server's full URL
              * @link https://trpc.io/docs/ssr
              **/
-            url: `${getBaseUrl()}/api/trpc`, // možda se može izbacit ovo base, jer neman SSR
+            url: "/api/trpc",
           }),
         ],
         /**
