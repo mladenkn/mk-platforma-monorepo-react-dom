9c5f3af90d2a58f08146eb721223e5042424eb4c
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-05-30 23:23:33 +0200

    better-get-ss-props +

--- packages/@mk-platforma/app-web/pages/profile/[id].ts
+++ packages/@mk-platforma/app-web/pages/profile/[id].ts
@@ -6,17 +6,23 @@ import { user_ss_get } from "../api/auth/[...nextauth]"
 import { typeCheck } from "@mk-libs/common/common"
 import { ComponentProps } from "react"
 import { create_getServerSideProps } from "~/create.getServerSideProps"
+import { z } from "zod"
 
-const a = create_getServerSideProps(async (ctx, session, params) => {
-  const user_id = parseInt(ctx.query.id as string)
-  const user = await Api_ss({ db, user: session.user! }).user.single_withPosts(user_id)
-  if (user)
-    return {
-      props: typeCheck<ComponentProps<typeof User_profile>>({
-        user_initial: user,
-      }),
-    }
-  else return { notFound: true }
-})
+const getServerSideProps = create_getServerSideProps(
+  {
+    queryParams: z.object({ id: z.number() }),
+  },
+  async (ctx, session, params) => {
+    const user_id = parseInt(ctx.query.id as string)
+    const user = await Api_ss({ db, user: session.user! }).user.single_withPosts(user_id)
+    if (user)
+      return {
+        props: typeCheck<ComponentProps<typeof User_profile>>({
+          user_initial: user,
+        }),
+      }
+    else return { notFound: true }
+  }
+)
 
 export default User_profile
