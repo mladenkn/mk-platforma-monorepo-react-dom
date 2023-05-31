76c94e5388b5666e98dda221dd5406719de7fd7d
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-05-31 12:49:38 +0200

    better-get-ss-props +

--- packages/@mk-platforma/app-web/pages/profile/edit.tsx
+++ packages/@mk-platforma/app-web/pages/profile/edit.tsx
@@ -1,8 +1,8 @@
 import { asNonNil, undefinedToNullsDeep } from "@mk-libs/common/common"
-import { create_get_ss_props } from "~/create.getServerSideProps"
+import create_get_ss_props from "~/create.getServerSideProps"
 import User_profile_edit from "~/domain/user/User.profile.edit"
 
-export const getServerSideProps = create_get_ss_props(async ctx => ({
+export const getServerSideProps = create_get_ss_props({}, async ctx => ({
   props: {
     user_initial: undefinedToNullsDeep(asNonNil(ctx.user)),
   },
