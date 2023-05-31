c5be4c70619e38a6d4cf9c5e3ec2ab08521d151d
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-05-31 15:16:52 +0200

    +

--- packages/@mk-platforma/app-web/domain/post/Post.list.page.data.initial.ts
+++ packages/@mk-platforma/app-web/domain/post/Post.list.page.data.initial.ts
@@ -8,18 +8,17 @@ const Post_list_page_data_initial = create_get_ss_props(
   {
     queryParams: z.object({ category: z.string().nullish() }),
   },
-  async ({ api, db }, params, nextContext) => {
+  async ({ api }, params, nextContext) => {
     const category_label = params.category ? (params.category as Category_labelType) : undefined
-
-    const category = category_label
-      ? await db.category.findFirst({ where: { label: category_label } })
-      : null
 
     const location = getCookie_ss(nextContext.req.headers.cookie || "", "Post_list__location")
     const location_radius = getCookie_ss(
       nextContext.req.headers.cookie || "",
       "Post_list__location_radius"
     )
+
+    const categories_all = await api.category.many()
+    const category = categories_all.find(c => c.label === category_label)
 
     const posts_initial = await api.post.list.fieldSet_main({
       categories: category ? [category.id] : undefined,
@@ -30,7 +29,7 @@ const Post_list_page_data_initial = create_get_ss_props(
     const props: PostList_section_Props = {
       selectedCategory_initial: category,
       posts_initial: posts_initial.items,
-      categories_initial: await api.category.many(),
+      categories_initial: categories_all,
       location_initial: location || null,
       location_radius_initial: location_radius || null,
     }
