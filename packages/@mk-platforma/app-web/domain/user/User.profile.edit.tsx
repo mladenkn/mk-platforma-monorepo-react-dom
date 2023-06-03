64a9143408d5291f38ec863820d1af72e40c599f
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-06-03 23:36:24 +0200

    +

--- packages/@mk-platforma/app-web/domain/user/User.profile.edit.tsx
+++ packages/@mk-platforma/app-web/domain/user/User.profile.edit.tsx
@@ -7,6 +7,7 @@ import { LogoLink } from "../common"
 import DoneIcon from "@mui/icons-material/Done"
 import { asNonNil } from "~/../../@mk-libs/common/common"
 import { useState } from "react"
+import { useRouter } from "next/router"
 
 type User = NonNullable<Api_outputs["user"]["single"]>
 
@@ -19,12 +20,19 @@ export default function User_profile_edit({ user_initial }: Props) {
   const user_data = asNonNil(user.data)
   const [user_name, set__user_name] = useState(user_data.name || "")
 
-  const mutation = Api.user.update.useMutation()
-  function updateUser() {
-    mutation.mutateAsync({ name: user_name })
+  const router = useRouter()
+  const mutation = Api.user.update.useMutation({
+    onSuccess() {
+      router.push(`/profile/${user_data.id}`)
+    },
+  })
+  async function updateUser() {
+    if (user_data.name !== user_name) {
+      mutation.mutate({ name: user_name })
+    }
   }
 
-  const { palette } = useTheme()
+  const { palette, typography } = useTheme()
 
   return (
     <Layout
@@ -37,17 +45,19 @@ export default function User_profile_edit({ user_initial }: Props) {
       }
       content={
         <Paper sx={{ width: "100%", p: 2, pb: 3.5, m: 1 }}>
-          <Typography variant="h4">Uređivanje profila</Typography>
-          <Box sx={{ display: "flex", mt: 3 }}>
-            <TextField
-              label="Korisničko ime"
-              value={user_name}
-              onChange={e => set__user_name(e.target.value)}
-            />
+          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+            <Typography variant="h4">Uređivanje profila</Typography>
             <IconButton sx={{ ml: 2 }} onClick={updateUser}>
-              <DoneIcon />
+              <DoneIcon sx={{ fontSize: typography.h4 }} />
             </IconButton>
           </Box>
+
+          <TextField
+            sx={{ mt: 3 }}
+            label="Korisničko ime"
+            value={user_name}
+            onChange={e => set__user_name(e.target.value)}
+          />
           {mutation.isError && (
             <Typography sx={{ color: palette.error.main }}>{mutation.error.message}</Typography>
           )}
