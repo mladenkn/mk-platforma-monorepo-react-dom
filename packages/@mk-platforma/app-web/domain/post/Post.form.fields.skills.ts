8a673cad24c81961c4f453d44278dd61793a1ed6
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-06-07 11:32:35 +0200

    Post-form-fields-skills +

--- packages/@mk-platforma/app-web/domain/post/Post.form.fields.skills.ts
+++ packages/@mk-platforma/app-web/domain/post/Post.form.fields.skills.ts
@@ -1,3 +1,11 @@
-type Props = {}
+type Skill = {
+  label: string
+  level: number
+}
+
+type Props = {
+  value: Skill[]
+  onChange(v: Skill[]): void
+}
 
 export default function Post_form_fields_skills({}: Props) {}
