285602333297457472595847157afd9597a68ad1
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-06-07 12:00:34 +0200

    Post-form-fields-skills +

--- packages/@mk-platforma/app-web/domain/post/Post.form.fields.skills.tsx
+++ packages/@mk-platforma/app-web/domain/post/Post.form.fields.skills.tsx
@@ -1,5 +1,6 @@
 import { Box, SxProps, TextField, Typography, useTheme, IconButton } from "@mui/material"
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline"
+import HandymanIcon from "@mui/icons-material/Handyman"
 
 type Skill = {
   label: string
@@ -16,9 +17,10 @@ export default function Post_form_fields_skills({ sx }: Props) {
   const { typography } = useTheme()
   return (
     <Box sx={sx}>
-      <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+      <Box sx={{ display: "flex", alignItems: "center", opacity: 0.8 }}>
+        <HandymanIcon sx={{ mr: 1.5 }} />
         <Typography
-          sx={{ fontSize: typography.h6, fontWeight: typography.fontWeightRegular, opacity: 0.8 }}
+          sx={{ fontSize: typography.h6, fontWeight: typography.fontWeightRegular, mr: 26 }}
         >
           Vještine
         </Typography>
