c998ff92a711dcd539229d9f3172d141b5ca2d00
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-06-11 09:24:08 +0200

    post-cu-separate +

--- packages/@mk-platforma/app-web/domain/post/Post.api.cu.input.ts
+++ packages/@mk-platforma/app-web/domain/post/Post.api.cu.input.ts
@@ -5,7 +5,6 @@ import {
   PostSchema,
   Post_ExpertEndorsementSchema,
   CategorySchema,
-  Post_ExpertEndorsement_skillSchema,
 } from "~/prisma/generated/zod"
 
 export const Post_api_create_input = PostSchema.pick({
@@ -33,31 +32,8 @@ export const Post_api_create_input = PostSchema.pick({
   location: LocationSchema.pick({ id: true }).nullish(),
 })
 
-const create = Post_api_create_input.shape
-const create_expert = Post_ExpertEndorsementSchema.shape
-export const Post_api_update_input = z.object({
+export const Post_api_update_input = Post_api_create_input.extend({
   id: PostSchema.shape.id,
-  title: create.title.optional(),
-  description: create.title.nullish(),
-  contact: create.title.optional(),
-  categories: create.categories.optional(),
-  images: create.images,
-  location: create.location,
-  isDeleted: PostSchema.shape.isDeleted.optional(),
-  expertEndorsement: z
-    .object({
-      firstName: create_expert.firstName,
-      lastName: create_expert.lastName,
-      skills: z
-        .array(
-          Post_ExpertEndorsement_skillSchema.pick({
-            label: true,
-            level: true,
-          })
-        )
-        .optional(),
-    })
-    .nullish(),
 })
 
 export const Post_api_cu_input = Post_api_create_input.or(Post_api_update_input)
--- packages/@mk-platforma/app-web/domain/post/Post.api.update.ts
+++ packages/@mk-platforma/app-web/domain/post/Post.api.update.ts
@@ -4,13 +4,12 @@ import { Post_api_update_input } from "./Post.api.cu.input"
 import { getRandomElement } from "@mk-libs/common/array"
 import { avatarStyles } from "~/domain/user/User.common"
 import "@mk-libs/common/server-only"
-import { TRPCError } from "@trpc/server"
 
 const Post_api_update = authorizedRoute(u => u.canMutate && !!u.name)
   .input(Post_api_update_input)
   .mutation(({ ctx, input }) =>
-    ctx.db.$transaction(async tx => {
-      const post = await tx.post.update({
+    ctx.db.$transaction(tx =>
+      tx.post.update({
         where: {
           id: input.id,
         },
@@ -23,7 +22,7 @@ const Post_api_update = authorizedRoute(u => u.canMutate && !!u.name)
           expertEndorsement: true,
         },
         data: {
-          ...shallowPick(input, "title", "description", "contact", "isDeleted"),
+          ...shallowPick(input, "title", "description", "contact"),
           author: {
             connect: {
               id: ctx.user.id,
@@ -58,31 +57,7 @@ const Post_api_update = authorizedRoute(u => u.canMutate && !!u.name)
             : undefined,
         },
       })
-      const notValid =
-        (post.categories.some(c => c.label === "job_demand") && !post.expertEndorsement) ||
-        post.categories.length > 1
-      if (notValid)
-        throw new TRPCError({
-          code: "BAD_REQUEST",
-          message: "Updated post was not at valid state",
-        })
-
-      const shouldRemoveExpert =
-        post.categories.every(c => c.label !== "job_demand") && post.expertEndorsement
-      if (shouldRemoveExpert)
-        await tx.post.update({
-          where: {
-            id: post.id,
-          },
-          data: {
-            expertEndorsement: {
-              delete: true,
-            },
-          },
-        })
-
-      return post
-    })
+    )
   )
 
 export default Post_api_update
