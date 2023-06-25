465afa81def986bc92c6ec397afe7cbb5b9112ad
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-06-25 19:28:09 +0200

    playwright +

--- packages/@mk-platforma/app-web/e2e/example.spec.ts
+++ packages/@mk-platforma/app-web/e2e/example.spec.ts
@@ -27,3 +27,12 @@ test("get started link", async ({ page }) => {
   // Expects the URL to contain intro.
   await expect(page).toHaveURL(/.*intro/)
 })
+
+test("first", async ({ page }) => {
+  await page.goto("http://localhost:3000")
+  await page.locator("_react=Header_moreOptions").click()
+  await page.locator("_react=Header_moreOptions").locator("_react=PostAddIcon").click()
+  await expect(page).toHaveURL(/.*signin/)
+  await page.locator("input#input-email-for-email-provider").fill("mladen.knezovic.1993@gmail.com")
+  await page.locator("button#submitButton").click()
+})
