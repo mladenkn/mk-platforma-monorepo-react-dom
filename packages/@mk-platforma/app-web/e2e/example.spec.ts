a14d139e2dbb2fe6c7b04c55b4a83424b287db05
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-06-26 12:58:16 +0200

    playwright +

--- packages/@mk-platforma/app-web/e2e/example.spec.ts
+++ packages/@mk-platforma/app-web/e2e/example.spec.ts
@@ -6,4 +6,9 @@ test("first", async ({ page }) => {
   await page.locator("_react=Header_moreOptions").locator("_react=PostAddIcon").click()
   await page.waitForURL(/.*post\/create/)
   await expect(page).toHaveURL(/.*post\/create/)
+
+  const fields = page.locator("_react=Post_form_fields")
+  await fields.locator("[name='title']").fill("post 1 title")
+  await fields.locator("[name='description']").fill("post 1 description")
+  await fields.locator("[name='contact']").fill("584484")
 })
