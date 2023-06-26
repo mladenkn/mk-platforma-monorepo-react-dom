115406b3c615e888c485522902704933eb216bf4
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-06-26 12:46:19 +0200

    .

--- packages/@mk-platforma/app-web/e2e/example.spec.ts
+++ packages/@mk-platforma/app-web/e2e/example.spec.ts
@@ -1,33 +1,5 @@
 import { test, expect } from "@playwright/test"
 
-// test("should navigate to the about page", async ({ page }) => {
-//   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
-//   await page.goto("http://localhost:3000/")
-//   // Find an element with the text 'About Page' and click on it
-//   await page.click("text=About")
-//   // The new URL should be "/about" (baseURL is used there)
-//   await expect(page).toHaveURL("http://localhost:3000/about")
-//   // The new page should contain an h1 with "About Page"
-//   await expect(page.locator("h1")).toContainText("About Page")
-// })
-
-test("has title", async ({ page }) => {
-  await page.goto("https://playwright.dev/")
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/)
-})
-
-test("get started link", async ({ page }) => {
-  await page.goto("https://playwright.dev/")
-
-  // Click the get started link.
-  await page.getByRole("link", { name: "Get started" }).click()
-
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/)
-})
-
 test("first", async ({ page }) => {
   await page.goto("http://localhost:3000")
   await page.locator("_react=Header_moreOptions").click()
