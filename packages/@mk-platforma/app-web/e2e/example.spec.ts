58786aeba9f030ae230ddcf84938cac010ded0d7
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-06-26 17:59:35 +0200

    playwright +

--- packages/@mk-platforma/app-web/e2e/example.spec.ts
+++ packages/@mk-platforma/app-web/e2e/example.spec.ts
@@ -7,14 +7,22 @@ test("first", async ({ page }) => {
   await page.waitForURL(/.*post\/create/)
   await expect(page).toHaveURL(/.*post\/create/)
 
+  const newPost = {
+    title: "post 1 title",
+    description: "post 1 description",
+    contact: "5185184",
+    category: "Hrana",
+    location: "Split",
+  }
+
   const fields = page.locator("_react=Post_form_fields")
-  await fields.locator("[name='title']").fill("post 1 title")
-  await fields.locator("[name='description']").fill("post 1 description")
-  await fields.locator("[name='contact']").fill("584484")
+  await fields.locator("[name='title']").fill(newPost.title)
+  await fields.locator("[name='description']").fill(newPost.description)
+  await fields.locator("[name='contact']").fill(newPost.contact)
 
   await fields.locator("[name='category']").click()
-  await page.locator(".category-dropdown-popper").getByText("Hrana").click()
+  await page.locator(".category-dropdown-popper").getByText(newPost.category).click()
 
   await fields.locator("[name='location']").click()
-  await page.locator(".location-dropdown-popper").getByText("Split").click()
+  await page.locator(".location-dropdown-popper").getByText(newPost.location).click()
 })
