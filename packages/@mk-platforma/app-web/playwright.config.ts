4108ec26609a8d1414a9b4b24e855e5134c24469
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-06-28 11:49:51 +0200

    playwright +

--- packages/@mk-platforma/app-web/playwright.config.ts
+++ packages/@mk-platforma/app-web/playwright.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from "@playwright/test"
 
 export default defineConfig({
-  timeout: 30000,
-  globalTimeout: 600000,
+  timeout: 300000,
+  globalTimeout: 6000000,
   testDir: "./tests",
   use: {
-    baseURL: "http://localhost:3000",
+    baseURL: "http://localhost:3010",
+  },
+  webServer: {
+    command: "pnpm _c dev -p 3010",
+    url: "http://localhost:3010",
+    reuseExistingServer: !process.env.CI,
   },
 })
