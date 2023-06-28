53b82304136fc7df24f899fae11bf3b405f8f2d0
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-06-28 09:58:28 +0200

    playwright +

--- packages/@mk-platforma/app-web/e2e/example.spec.ts
+++ packages/@mk-platforma/app-web/e2e/example.spec.ts
@@ -2,10 +2,14 @@ import { test, expect } from "@playwright/test"
 
 const baseUrl = "http://localhost:3000"
 
-test("first", async ({ page }) => {
+test("Post CRUD", async ({ page }) => {
+  // home
   await page.goto(baseUrl)
   await page.locator("_react=Header_moreOptions").click()
   await page.locator("_react=Header_moreOptions").locator("_react=PostAddIcon").click()
+  // end home
+
+  // create
   await page.waitForURL(/.*post\/create/)
   await expect(page).toHaveURL(/.*post\/create/)
 
@@ -29,7 +33,9 @@ test("first", async ({ page }) => {
   await page.locator(".location-dropdown-popper").getByText(newPost.location).click()
 
   await form.locator("button[type=submit]").click()
+  // end create
 
+  // details
   await page.waitForURL(/\/post\/\d+$/)
   await expect(page).toHaveURL(/\/post\/\d+$/)
 
@@ -40,22 +46,29 @@ test("first", async ({ page }) => {
   await expect(post_component).toContainText(newPost.description)
   await expect(post_component).toContainText(newPost.contact)
   await expect(post_component).toContainText(newPost.location)
+  // end details
 
+  // list
   await page.goto(baseUrl)
   await page.waitForURL(baseUrl)
 
   const post_list_item = page.locator(`_react=Post_listItem[id = ${newPost_id}]`)
   await expect(post_list_item).toContainText(newPost.title)
   await expect(post_list_item).toContainText(newPost.location)
-
   await post_list_item.click()
+  // end list
+
+  // details
   await page.waitForURL(`${baseUrl}/post/${newPost_id}`)
   await expect(page).toHaveURL(`${baseUrl}/post/${newPost_id}`)
 
   const editLink = page.locator(`a[href="/post/edit/${newPost_id}"]`)
   await expect(editLink).toBeVisible()
-
   await editLink.click()
+  // end details
+
+  // edit
   await page.waitForURL(`${baseUrl}/post/edit/${newPost_id}`)
   await expect(page).toHaveURL(`${baseUrl}/post/edit/${newPost_id}`)
+  // end edit
 })
