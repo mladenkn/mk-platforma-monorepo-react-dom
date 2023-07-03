806cab1a925b93d42494eba25060f3932fcad9b8
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-07-03 12:36:50 +0200

    +

--- packages/@mk-platforma/app-web/playwright.config.ts
+++ packages/@mk-platforma/app-web/playwright.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "@playwright/test"
 
 export default defineConfig({
-  timeout: 300000,
-  globalTimeout: 6000000,
+  // timeout: 300000,
+  // globalTimeout: 6000000,
   testDir: "./tests",
   use: {
     baseURL: "http://localhost:3010",
