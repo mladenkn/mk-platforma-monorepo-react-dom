73f2632831884ad8464ccfa3cf40bf3a4c39280b
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-07-04 14:31:23 +0200

    login +

---
+++ packages/@mk-platforma/app-web/pages/login.tsx
@@ -0,0 +1,19 @@
+import { GetServerSidePropsContext } from "next"
+import { user_ss_get } from "./api/auth/[...nextauth]"
+import { signIn } from "next-auth/react"
+
+export async function getServerSideProps({ req, res }: GetServerSidePropsContext) {
+  const user = await user_ss_get(req, res)
+  if (user) {
+    return {
+      redirect: {
+        destination: `/`,
+        permanent: false,
+      },
+    }
+  } else return {}
+}
+
+export default function Login() {
+  signIn()
+}
