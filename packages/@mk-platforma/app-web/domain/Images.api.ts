b82076f55f14413282783e6adf9b43a5f37de715
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-07-04 23:18:08 +0200

    images-save-all +

--- packages/@mk-platforma/app-web/domain/Images.api.ts
+++ packages/@mk-platforma/app-web/domain/Images.api.ts
@@ -1,16 +1,22 @@
 import { authorizedRoute, router } from "~/api_/api.server.utils"
 import { ImageSchema } from "../prisma/generated/zod"
 import "@mk-libs/common/server-only"
+import { z } from "zod"
 
-const input_zod = ImageSchema.pick({
-  uploadthing_key: true,
-  url: true,
-})
+const input_zod = z.array(
+  ImageSchema.pick({
+    uploadthing_key: true,
+    url: true,
+  })
+)
 
 const Image_api = router({
   create: authorizedRoute(u => u.canMutate)
     .input(input_zod)
-    .mutation(({ ctx, input }) => ctx.db.image.create({ data: input })),
+    .mutation(async ({ ctx, input }) => {
+      const images = await Promise.all(input.map(image => ctx.db.image.create({ data: image })))
+      return images
+    }),
 })
 
 export default Image_api
