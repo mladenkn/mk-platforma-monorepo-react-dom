1eb4b31c498d9bfc32350d3f1b6208bdebbef635
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-07-05 15:15:44 +0200

    post.form +

--- packages/@mk-platforma/app-web/domain/post/Post.api.cu.input.ts
+++ packages/@mk-platforma/app-web/domain/post/Post.api.cu.input.ts
@@ -7,39 +7,42 @@ import {
   CategorySchema,
 } from "~/prisma/generated/zod"
 
-export const Post_api_create_input = PostSchema.pick({
-  title: true,
-  description: true,
-  contact: true,
-}).extend({
-  categories: z.array(CategorySchema.pick({ id: true })).length(1),
-  expertEndorsement: Post_ExpertEndorsementSchema.pick({
-    firstName: true,
-    lastName: true,
+export const Post_api_create_input = z
+  .object({
+    title: z.string().min(1),
+    description: z.string().min(1),
+    contact: z.string().min(1),
   })
-    .extend({
-      skills: z
-        .array(
-          z.object({
-            id: z.number().optional(),
-            label: z.string(),
-            level: z.number().min(1).max(5).nullish(),
-          })
-        )
-        .nullish(),
-    })
-    .nullish(),
-  images: z
-    .array(ImageSchema.pick({ isMain: true, id: true, url: true }))
-    .optional()
-    .refine(images => (images?.length ? images?.filter(i => i.isMain).length === 1 : true), {
-      message: "Mora biti jedna glavna slika",
-      params: {
-        code: "ONE_MAIN_IMAGE",
-      },
-    }),
-  location: LocationSchema.pick({ id: true }).nullish(),
-})
+  .extend({
+    categories: z.array(CategorySchema.pick({ id: true })).length(1),
+    expertEndorsement: z
+      .object({
+        firstName: z.string().min(1),
+        lastName: z.string().min(1),
+      })
+      .extend({
+        skills: z
+          .array(
+            z.object({
+              id: z.number().optional(),
+              label: z.string(),
+              level: z.number().min(1).max(5).nullish(),
+            })
+          )
+          .nullish(),
+      })
+      .nullish(),
+    images: z
+      .array(ImageSchema.pick({ isMain: true, id: true, url: true }))
+      .optional()
+      .refine(images => (images?.length ? images?.filter(i => i.isMain).length === 1 : true), {
+        message: "Mora biti jedna glavna slika",
+        params: {
+          code: "ONE_MAIN_IMAGE",
+        },
+      }),
+    location: LocationSchema.pick({ id: true }).nullish(),
+  })
 
 export const Post_api_update_input = Post_api_create_input.extend({
   id: PostSchema.shape.id,
