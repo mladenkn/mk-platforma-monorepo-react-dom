a3fced47ced34adb7c921ba08573ab1656abc48e
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-07-05 15:10:54 +0200

    login-custom +

--- packages/@mk-platforma/app-web/domain/Login.tsx
+++ packages/@mk-platforma/app-web/domain/Login.tsx
@@ -1,9 +1,21 @@
 import { Button, Container, Box, TextField, Paper, useTheme } from "@mui/material"
 import { LogoLink } from "./common"
+import { Formik } from "formik"
+import { z } from "zod"
+import { toFormikValidationSchema } from "zod-formik-adapter"
 
 type Props = {
   csrfToken?: string
 }
+
+const initialValues = {
+  email: "",
+  password: "",
+}
+const form_zod = z.object({
+  email: z.string().min(1),
+  password: z.string().min(1),
+})
 
 export default function Login({ csrfToken }: Props) {
   const theme = useTheme()
@@ -13,21 +25,45 @@ export default function Login({ csrfToken }: Props) {
         <LogoLink />
       </Box>
       <Paper sx={{ py: 4, px: 2 }}>
-        <form method="post" action="/api/auth/signin/email">
-          <Box sx={{ display: "flex", flexDirection: "column" }}>
-            <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-            <TextField
-              sx={{ mb: 4, maxWidth: 500 }}
-              label="Email"
-              type="email"
-              id="email"
-              name="email"
-            />
-            <TextField sx={{ maxWidth: 500, mb: 4 }} label="Šifra" name="password" />
-            {/* <Typography sx={{ ml: 0.5, mt: 1.5, mb: 4 }}>Hint: Šifra je "maca i miš"</Typography> */}
-            <Button type="submit">Prijavi se</Button>
-          </Box>
-        </form>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={() => {}}
+          validationSchema={toFormikValidationSchema(form_zod)}
+          validateOnMount
+        >
+          {({ handleChange, isValid, handleBlur, values, errors, touched }) => (
+            <form method="post" action="/api/auth/signin/email">
+              <Box sx={{ display: "flex", flexDirection: "column" }}>
+                <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
+                <TextField
+                  sx={{ mb: 4, maxWidth: 500 }}
+                  label="Email"
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  error={touched.email && Boolean(errors.email)}
+                  helperText={touched.email && errors.email}
+                />
+                <TextField
+                  sx={{ maxWidth: 500, mb: 4 }}
+                  label="Šifra"
+                  name="password"
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  error={touched.password && Boolean(errors.password)}
+                  helperText={touched.password && errors.password}
+                />
+                <Button disabled={!isValid} type="submit">
+                  Prijavi se
+                </Button>
+              </Box>
+            </form>
+          )}
+        </Formik>
       </Paper>
     </Container>
   )
