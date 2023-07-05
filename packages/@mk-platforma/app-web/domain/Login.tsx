1770c61af1bdd1e45e28ac0738b1a79f1f66f364
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-07-05 12:38:17 +0200

    login-custom +

--- packages/@mk-platforma/app-web/domain/Login.tsx
+++ packages/@mk-platforma/app-web/domain/Login.tsx
@@ -1,16 +1,21 @@
+import { Button, Input, Container, Box, Typography, TextField, Paper } from "@mui/material"
+
 type Props = {
   csrfToken?: string
 }
 
 export default function Login({ csrfToken }: Props) {
   return (
-    <form method="post" action="/api/auth/signin/email">
-      <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-      <label>
-        Email address
-        <input type="email" id="email" name="email" />
-      </label>
-      <button type="submit">Sign in with Email</button>
-    </form>
+    <Container sx={{ p: 3 }} maxWidth="xs">
+      <Paper sx={{ p: 2 }}>
+        <form method="post" action="/api/auth/signin/email">
+          <Box sx={{ display: "flex", flexDirection: "column" }}>
+            <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
+            <TextField sx={{ mb: 3 }} label="Email" type="email" id="email" name="email" />
+            <Button type="submit">Prijavi se</Button>
+          </Box>
+        </form>
+      </Paper>
+    </Container>
   )
 }
