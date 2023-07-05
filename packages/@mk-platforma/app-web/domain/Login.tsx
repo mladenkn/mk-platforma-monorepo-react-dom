ec14f5369960da098df8808885223003ce32eaef
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-07-05 12:49:22 +0200

    login-custom +

--- packages/@mk-platforma/app-web/domain/Login.tsx
+++ packages/@mk-platforma/app-web/domain/Login.tsx
@@ -1,17 +1,34 @@
-import { Button, Input, Container, Box, Typography, TextField, Paper } from "@mui/material"
+import { Button, Container, Box, Typography, TextField, Paper, useTheme } from "@mui/material"
 
 type Props = {
   csrfToken?: string
 }
 
 export default function Login({ csrfToken }: Props) {
+  const theme = useTheme()
   return (
-    <Container sx={{ p: 3 }} maxWidth="xs">
-      <Paper sx={{ p: 2 }}>
+    <Container maxWidth="md">
+      <Box sx={{ background: theme.palette.primary.main, px: 2, py: 1 }}>
+        <Typography sx={{ color: "white" }} variant="h2" fontWeight={400}>
+          ZaBrata
+        </Typography>
+        <Box sx={{ color: "white" }}>
+          <Typography variant="h4" fontWeight={400}>
+            Loza kontribucionizma
+          </Typography>
+        </Box>
+      </Box>
+      <Paper sx={{ py: 4, px: 2 }}>
         <form method="post" action="/api/auth/signin/email">
           <Box sx={{ display: "flex", flexDirection: "column" }}>
             <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-            <TextField sx={{ mb: 3 }} label="Email" type="email" id="email" name="email" />
+            <TextField
+              sx={{ mb: 4, maxWidth: 500 }}
+              label="Email"
+              type="email"
+              id="email"
+              name="email"
+            />
             <Button type="submit">Prijavi se</Button>
           </Box>
         </form>
