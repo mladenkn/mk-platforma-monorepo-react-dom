cca58f181bf4814e20719885e557bbb4e1205abc
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-07-05 12:32:13 +0200

    login-custom +

--- packages/@mk-platforma/app-web/domain/Login.tsx
+++ packages/@mk-platforma/app-web/domain/Login.tsx
@@ -1,3 +1,16 @@
-export default function Login() {
-  return <div>Login</div>
+type Props = {
+  csrfToken?: string
+}
+
+export default function Login({ csrfToken }: Props) {
+  return (
+    <form method="post" action="/api/auth/signin/email">
+      <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
+      <label>
+        Email address
+        <input type="email" id="email" name="email" />
+      </label>
+      <button type="submit">Sign in with Email</button>
+    </form>
+  )
 }
