4bea0530728cbd534dcd1819a8ba750b84153be6
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-07-13 23:09:03 +0200

    .

--- packages/@mk-platforma/app-web/pages/index.tsx
+++ packages/@mk-platforma/app-web/pages/index.tsx
@@ -2,5 +2,7 @@ import Post_list_page from "~/domain/post/Post.list.page"
 import Post_list_page_props_initial_get from "~/domain/post/Post.list.page.data.initial"
 import { ss_props_get_create_next } from "~/ss.props"
 
+console.log(process.env.NODE_ENV)
+
 export const getServerSideProps = ss_props_get_create_next(Post_list_page_props_initial_get)
 export default Post_list_page
