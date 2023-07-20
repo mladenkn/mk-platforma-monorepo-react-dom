74acdc62ac4574e01fbc30efb2a414efbea4429e
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-07-20 13:06:09 +0200

    comment.list +

--- packages/@mk-platforma/app-web/domain/Comment.list.tsx
+++ packages/@mk-platforma/app-web/domain/Comment.list.tsx
@@ -4,6 +4,7 @@ import { Comment_list_item } from "./Comment.list.item"
 import SendIcon from "@mui/icons-material/Send"
 import { useState } from "react"
 import { use_currentUser } from "~/utils.client"
+import { Warning_noUsername, use_noUsername_isDisplayed } from "./common"
 
 type Props = { post_id: number; canComment: boolean }
 
@@ -26,21 +27,39 @@ export default function Comment_list({ post_id, canComment }: Props) {
   const user = use_currentUser()
   const avatarStyle = user.data?.avatarStyle as object | undefined
 
+  const warning_noUsername_isDisplayed = use_noUsername_isDisplayed()
+
   return (
     <>
       {canComment && (
-        <Paper sx={{ borderRadius: 2, mt: 2, p: 1, display: "flex", gap: 1 }}>
-          <Avatar children={user.data?.name?.[0]} sx={{ mr: 1, ...avatarStyle }} />
-          <Input
-            sx={{ flex: 1 }}
-            placeholder="Komentiraj"
-            multiline
-            value={newComment_input}
-            onChange={e => set_newComment_input(e.target.value)}
-          />
-          <IconButton disabled={!newComment_input} onClick={comment_create_mutate} color="primary">
-            <SendIcon />
-          </IconButton>
+        <Paper
+          sx={{
+            borderRadius: 2,
+            mt: 2,
+            p: 1,
+            display: "flex",
+            flexDirection: "column",
+            gap: 1,
+          }}
+        >
+          {warning_noUsername_isDisplayed && <Warning_noUsername withSetAction />}
+          <Box sx={{ display: "flex", gap: 1 }}>
+            <Avatar children={user.data?.name?.[0]} sx={{ mr: 1, ...avatarStyle }} />
+            <Input
+              sx={{ flex: 1 }}
+              placeholder="Komentiraj"
+              multiline
+              value={newComment_input}
+              onChange={e => set_newComment_input(e.target.value)}
+            />
+            <IconButton
+              disabled={!newComment_input}
+              onClick={comment_create_mutate}
+              color="primary"
+            >
+              <SendIcon />
+            </IconButton>
+          </Box>
         </Paper>
       )}
       {comments.data?.length ? (
