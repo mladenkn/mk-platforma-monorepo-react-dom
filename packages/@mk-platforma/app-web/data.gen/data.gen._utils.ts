f7ad64c48be01fe99a0f47b320c6151452b37402
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-07-20 12:35:15 +0200

    seed-prod  +

--- packages/@mk-platforma/app-web/data.gen/data.gen._utils.ts
+++ packages/@mk-platforma/app-web/data.gen/data.gen._utils.ts
@@ -1,5 +1,5 @@
 import { Category_labelType } from "../prisma/generated/zod"
-import { WithId } from "./db.seed"
+import { WithId } from "./data.gen.seed"
 
 export type PostGeneratorParams = {
   categories: { id: number; label: Category_labelType }[]
