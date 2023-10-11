f9b313c1c2544cf9d1f19042d6ec6d996eff06b6
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-10-11 09:00:18 +0200

    drizzle +

--- packages/@mk-platforma/app-web/domain/category/Category.api.ts
+++ packages/@mk-platforma/app-web/domain/category/Category.api.ts
@@ -1,51 +1,13 @@
 import { z } from "zod"
-import { SuperData_mapper, SuperData_query } from "~/api_/api.SuperData"
 import { publicProcedure, router } from "~/api_/api.server.utils"
-import { Post_queryChunks_search } from "../post/Post.api.abstract"
 import "@mk-libs/common/server-only"
 import { eq } from "drizzle-orm"
 import { category } from "~/drizzle/schema"
 import { assertIsNonNil } from "@mk-libs/common/common"
 
-const Category_api_many = SuperData_mapper(
-  z
-    .object({
-      search: z.string().optional(),
-      parent: z
-        .object({
-          id: z.number().optional(),
-        })
-        .optional()
-        .nullable(),
-    })
-    .optional(),
-  async (_, input) => ({
-    where: {
-      OR: [
-        {
-          posts: {
-            some: input?.search ? Post_queryChunks_search(input.search) : {},
-          },
-        },
-        {
-          children: {
-            some: {
-              posts: {
-                some: input?.search ? Post_queryChunks_search(input.search) : {},
-              },
-            },
-          },
-        },
-      ],
-      parent: input?.parent,
-    },
-  }),
-)
-
 const Category_api = router({
-  many: SuperData_query(Category_api_many, ({ db }, output1) =>
+  many: publicProcedure.query(({ ctx: { db }, input }) =>
     db.category.findMany({
-      ...output1,
       select: Category_select,
     }),
   ),
@@ -56,7 +18,6 @@ const Category_api = router({
     })
     assertIsNonNil(c) // TODO: fix
     return c
-    // return { ...c, children: [] as (typeof c)[] } // TODO: add children
   }),
   // single2: publicProcedure.input(z.number()).query(async ({ ctx, input }) => {
   //   const c = await ctx.db.category.findUnique({ where: { id: input }, select: Category_select })
