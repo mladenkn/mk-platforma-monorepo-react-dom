69d649c1e91c044993c6db6cd94b2b55bfe79303
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-10-11 10:41:24 +0200

    +

--- packages/@mk-platforma/app-web/domain/category/Category.types.ts
+++ packages/@mk-platforma/app-web/domain/category/Category.types.ts
@@ -1,17 +1,22 @@
-export type Category_label =
-  | "job"
-  | "job_demand"
-  | "accommodation"
-  | "accommodation_demand"
-  | "sellable"
-  | "sellable_demand"
-  | "sellable_food"
-  | "sellable_clothes"
-  | "sellable_furniture"
-  | "sellable_tool"
-  | "sellable_gadget"
-  | "sellable_buildingMaterial"
-  | "gathering"
-  | "gathering_spirituality"
-  | "gathering_work"
-  | "gathering_hangout"
+import { z } from "zod"
+
+export const Category_label_zod = z.enum([
+  "job",
+  "job_demand",
+  "accommodation",
+  "accommodation_demand",
+  "sellable",
+  "sellable_demand",
+  "sellable_food",
+  "sellable_clothes",
+  "sellable_furniture",
+  "sellable_tool",
+  "sellable_gadget",
+  "sellable_buildingMaterial",
+  "gathering",
+  "gathering_spirituality",
+  "gathering_work",
+  "gathering_hangout",
+])
+
+export type Category_label = z.infer<typeof Category_label_zod>
