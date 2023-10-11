37824d09d6b38c160d0799d2b2b67b3d29398193
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-10-11 09:48:26 +0200

    drizzle +

--- packages/@mk-platforma/app-web/domain/category/Category.api.ts
+++ packages/@mk-platforma/app-web/domain/category/Category.api.ts
@@ -4,16 +4,40 @@ import "@mk-libs/common/server-only"
 import { eq } from "drizzle-orm"
 import { category } from "~/drizzle/schema"
 import { assertIsNonNil } from "@mk-libs/common/common"
+import { alias } from "drizzle-orm/pg-core"
+
+const category_parent = alias(category, "parent")
 
 const Category_api = router({
-  many: publicProcedure.query(({ ctx: { db_drizzle } }) =>
+  many2: publicProcedure.query(({ ctx: { db_drizzle } }) =>
     db_drizzle.query.category.findMany(Category_select2),
   ),
-  many2: publicProcedure.query(({ ctx: { db } }) =>
-    db.category.findMany({
+  many: publicProcedure.query(async ({ ctx: { db, db_drizzle } }) => {
+    const c = await db_drizzle
+      .select({
+        id: category.id,
+        label: category.label,
+        parent: {
+          id: category.id,
+          label: category.label,
+        },
+      })
+      .from(category)
+      .leftJoin(category_parent, eq(category_parent.id, category.parentId))
+
+    console.log(19, c)
+
+    return await db.category.findMany({
       select: Category_select,
-    }),
-  ),
+    })
+  }),
+  // many3: publicProcedure.query(async ({ ctx: { db_drizzle } }) => {
+  //   const c = await db_drizzle
+  //     .select({ id: category.id, label: category.label })
+  //     .from(category)
+  //     .leftJoin(category, eq(category.id, category.parentId))
+  //   return c
+  // }),
   single: publicProcedure.input(z.number()).query(async ({ ctx, input }) => {
     const c = await ctx.db_drizzle.query.category.findFirst({
       where: eq(category.id, input),
