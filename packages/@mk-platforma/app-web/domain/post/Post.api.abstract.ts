70c1407d50ee859932c629dc20ee69cc2d9c5182
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-10-12 19:09:44 +0200

    disabled post query by location

--- packages/@mk-platforma/app-web/domain/post/Post.api.abstract.ts
+++ packages/@mk-platforma/app-web/domain/post/Post.api.abstract.ts
@@ -12,16 +12,16 @@ export const Post_list_many = SuperData_mapper(
     location_radius: z.number().optional().default(50),
   }),
   async ({ db }, input) => {
-    const locations = await eva(async () => {
-      if (input.location) {
-        const { longitude, latitude } = await db.location
-          .findUnique({ where: { id: input.location } })
-          .then(asNonNil)
-        return await db.$queryRaw<{ id: number }[]>`
-          SELECT id FROM "Location" WHERE ST_DWithin(ST_MakePoint(longitude, latitude), ST_MakePoint(${longitude}, ${latitude})::geography, ${input.location_radius} * 1000)
-        `
-      }
-    })
+    // const locations = await eva(async () => {
+    //   if (input.location) {
+    //     const { longitude, latitude } = await db.location
+    //       .findUnique({ where: { id: input.location } })
+    //       .then(asNonNil)
+    //     return await db.$queryRaw<{ id: number }[]>`
+    //       SELECT id FROM "Location" WHERE ST_DWithin(ST_MakePoint(longitude, latitude), ST_MakePoint(${longitude}, ${latitude})::geography, ${input.location_radius} * 1000)
+    //     `
+    //   }
+    // })
     return {
       where: {
         categories: input.categories?.length
@@ -32,11 +32,11 @@ export const Post_list_many = SuperData_mapper(
             }
           : undefined,
         ...(input?.search ? Post_queryChunks_search(input.search) : {}),
-        location: locations && {
-          id: {
-            in: locations.map(({ id }) => id),
-          },
-        },
+        // location: locations && {
+        //   id: {
+        //     in: locations.map(({ id }) => id),
+        //   },
+        // },
         isDeleted: false,
       },
       orderBy: {
