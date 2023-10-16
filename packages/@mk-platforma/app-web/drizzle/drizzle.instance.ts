0c2fa04f85b4c7e94e3ba0f9ea88512500f41c2f
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-10-16 22:20:24 +0200

    drizzle-more +

--- packages/@mk-platforma/app-web/drizzle/drizzle.instance.ts
+++ packages/@mk-platforma/app-web/drizzle/drizzle.instance.ts
@@ -1,11 +1,3 @@
-import env from "../env.mjs"
-import { Drizzle_instance, drizzle_connect } from "./drizzle.utils"
+import { drizzle_connect } from "./drizzle.utils"
 
-const globalForDrizzle = global as unknown as {
-  instance?: Drizzle_instance
-}
-
-const db_drizzle = globalForDrizzle.instance ?? drizzle_connect()[0]
-export default db_drizzle
-
-if (env.NODE_ENV !== "production") globalForDrizzle.instance = db_drizzle
+export const db_drizzle = drizzle_connect()[0]
