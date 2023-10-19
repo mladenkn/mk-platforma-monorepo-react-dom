0c3576e2985bfd5894cb508b0b4d9a2ab7fb5608
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-10-19 12:41:22 +0200

    sqlite +

--- packages/@mk-platforma/app-web/domain/user/User.schema.ts
+++ packages/@mk-platforma/app-web/domain/user/User.schema.ts
@@ -1,13 +1,4 @@
-import {
-  pgTable,
-  uniqueIndex,
-  integer,
-  text,
-  varchar,
-  timestamp,
-  serial,
-  boolean,
-} from "drizzle-orm/pg-core"
+import { pgTable, uniqueIndex, varchar, serial, boolean } from "drizzle-orm/pg-core"
 
 import { relations } from "drizzle-orm"
 import { AvatarStyle, AvataryStyle_zod } from "~/domain/user/User.types"
@@ -36,65 +27,65 @@ export const UserRelations = relations(User, ({ many }) => ({
   posts: many(Post),
 }))
 
-export const Account = pgTable(
-  "Account",
-  {
-    id: text("id").primaryKey().notNull(),
-    userId: integer("userId")
-      .notNull()
-      .references(() => User.id, { onDelete: "cascade", onUpdate: "cascade" }),
-    type: text("type").notNull(),
-    provider: text("provider").notNull(),
-    providerAccountId: text("providerAccountId").notNull(),
-    refreshToken: text("refresh_token"),
-    accessToken: text("access_token"),
-    expiresAt: integer("expires_at"),
-    tokenType: text("token_type"),
-    scope: text("scope"),
-    idToken: text("id_token"),
-    sessionState: text("session_state"),
-  },
-  table => {
-    return {
-      providerProviderAccountIdKey: uniqueIndex("Account_provider_providerAccountId_key").on(
-        table.provider,
-        table.providerAccountId,
-      ),
-    }
-  },
-)
+// export const Account = pgTable(
+//   "Account",
+//   {
+//     id: text("id").primaryKey().notNull(),
+//     userId: integer("userId")
+//       .notNull()
+//       .references(() => User.id, { onDelete: "cascade", onUpdate: "cascade" }),
+//     type: text("type").notNull(),
+//     provider: text("provider").notNull(),
+//     providerAccountId: text("providerAccountId").notNull(),
+//     refreshToken: text("refresh_token"),
+//     accessToken: text("access_token"),
+//     expiresAt: integer("expires_at"),
+//     tokenType: text("token_type"),
+//     scope: text("scope"),
+//     idToken: text("id_token"),
+//     sessionState: text("session_state"),
+//   },
+//   table => {
+//     return {
+//       providerProviderAccountIdKey: uniqueIndex("Account_provider_providerAccountId_key").on(
+//         table.provider,
+//         table.providerAccountId,
+//       ),
+//     }
+//   },
+// )
 
-export const VerificationToken = pgTable(
-  "VerificationToken",
-  {
-    identifier: text("identifier").notNull(),
-    token: varchar("token", { length: 128 }).notNull(),
-    expires: timestamp("expires", { precision: 3, mode: "string" }).notNull(),
-  },
-  table => {
-    return {
-      tokenKey: uniqueIndex("VerificationToken_token_key").on(table.token),
-      identifierTokenKey: uniqueIndex("VerificationToken_identifier_token_key").on(
-        table.identifier,
-        table.token,
-      ),
-    }
-  },
-)
+// export const VerificationToken = pgTable(
+//   "VerificationToken",
+//   {
+//     identifier: text("identifier").notNull(),
+//     token: varchar("token", { length: 128 }).notNull(),
+//     expires: timestamp("expires", { precision: 3, mode: "string" }).notNull(),
+//   },
+//   table => {
+//     return {
+//       tokenKey: uniqueIndex("VerificationToken_token_key").on(table.token),
+//       identifierTokenKey: uniqueIndex("VerificationToken_identifier_token_key").on(
+//         table.identifier,
+//         table.token,
+//       ),
+//     }
+//   },
+// )
 
-export const Session = pgTable(
-  "Session",
-  {
-    id: text("id").primaryKey().notNull(),
-    sessionToken: varchar("sessionToken", { length: 128 }).notNull(),
-    userId: integer("userId")
-      .notNull()
-      .references(() => User.id, { onDelete: "cascade", onUpdate: "cascade" }),
-    expires: timestamp("expires", { precision: 3, mode: "string" }).notNull(),
-  },
-  table => {
-    return {
-      sessionTokenKey: uniqueIndex("Session_sessionToken_key").on(table.sessionToken),
-    }
-  },
-)
+// export const Session = pgTable(
+//   "Session",
+//   {
+//     id: text("id").primaryKey().notNull(),
+//     sessionToken: varchar("sessionToken", { length: 128 }).notNull(),
+//     userId: integer("userId")
+//       .notNull()
+//       .references(() => User.id, { onDelete: "cascade", onUpdate: "cascade" }),
+//     expires: timestamp("expires", { precision: 3, mode: "string" }).notNull(),
+//   },
+//   table => {
+//     return {
+//       sessionTokenKey: uniqueIndex("Session_sessionToken_key").on(table.sessionToken),
+//     }
+//   },
+// )
