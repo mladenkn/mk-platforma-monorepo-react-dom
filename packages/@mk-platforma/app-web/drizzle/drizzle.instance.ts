dad8686ed25bad5bb2081c0ae2ac8d5a77c344c5
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-10-31 10:21:04 +0100

    content-types +

--- packages/@mk-platforma/app-web/drizzle/drizzle.instance.ts
+++ packages/@mk-platforma/app-web/drizzle/drizzle.instance.ts
@@ -1,8 +1,6 @@
 import postgres from "postgres"
 import { drizzle } from "drizzle-orm/postgres-js"
-import * as Post_schema from "~/domain/post/Post.schema"
-import * as User_schema from "~/domain/user/User.schema"
-import * as Category_schema from "~/domain/category/Category.schema"
+import * as schema from "./drizzle.schema"
 import { asNonNil } from "@mk-libs/common/common"
 
 export default function drizzle_connect() {
@@ -10,7 +8,7 @@ export default function drizzle_connect() {
     ssl: "require",
     max: 1,
   })
-  return drizzle(queryClient, { schema: { ...Post_schema, ...User_schema, ...Category_schema } })
+  return drizzle(queryClient, { schema })
 }
 
 export type Drizzle_instance = ReturnType<typeof drizzle_connect>
