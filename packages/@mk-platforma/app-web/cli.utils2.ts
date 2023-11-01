04bf48afa6862c6a96200ded706fdc89e4296553
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-11-01 19:51:40 +0100

    cli-better +

---
+++ packages/@mk-platforma/app-web/cli.utils2.ts
@@ -0,0 +1,58 @@
+import commandLineArgs from "command-line-args"
+import { Api_ss_type } from "./api_/api.root"
+import { Drizzle_instance } from "~/drizzle/drizzle.instance"
+import { z } from "zod"
+
+export type Context = {
+  api: Api_ss_type
+  db: Drizzle_instance
+  run(cmd: string): Promise<unknown>
+}
+
+export type Command<
+  TName extends string,
+  TEnv extends Record<string, string>,
+  TParamsZod extends z.AnyZodObject = z.AnyZodObject,
+> = {
+  name: TName
+  params?: TParamsZod
+  env?: TEnv
+  resolve:
+    | ((c: Context & { params: z.infer<TParamsZod> }) => Promise<unknown> | unknown | string)
+    | string
+}
+
+type runProgram_options = {
+  commands: Command<string, {}>[]
+}
+
+export function runProgram({ commands }: runProgram_options) {}
+
+export function getConnectionString(env: string) {
+  switch (env) {
+    case "dev":
+      return "postgresql://postgres:postgres@localhost:5432/za_brata"
+    case "test.local":
+      return "postgresql://postgres:postgres@localhost:5432/za_brata_test"
+    case "staging":
+      return "postgresql://postgres:E18xcmX5bA2y8pPu85KF@containers-us-west-116.railway.app:7999/railway"
+    case "prod":
+      return "postgres://default:dLkKj9hoY7WT@ep-long-rain-221377-pooler.us-east-1.postgres.vercel-storage.com/verceldb?pgbouncer=true&connect_timeout=15"
+    case "neon-main": // ovo je na vercelu
+      return "postgres://mladenkn:EWT4n5pMetaB@ep-shy-paper-831120.eu-central-1.aws.neon.tech/neondb?sslmode=require"
+    case "neon-staging":
+      return "postgres://mladenkn:EWT4n5pMetaB@ep-lingering-fog-424091.eu-central-1.aws.neon.tech/neondb?sslmode=require"
+    default:
+      throw new Error(`Unsupported env: ${env}`)
+  }
+}
+
+export function getConnectionString_cli() {
+  const options = [
+    { name: "command", defaultOption: true },
+    { name: "db-instance", type: String },
+  ]
+  const parsed = commandLineArgs(options, { stopAtFirstUnknown: true })
+  const dbInstance = parsed["db-instance"] || "dev"
+  return getConnectionString(dbInstance)
+}
--- packages/@mk-platforma/app-web/cli2.ts
+++ packages/@mk-platforma/app-web/cli2.ts
@@ -1,36 +1,16 @@
-import { Api_ss, Api_ss_type } from "./api_/api.root"
-import drizzle_connect, { Drizzle_instance } from "~/drizzle/drizzle.instance"
-import { getConnectionString } from "./cli.utils"
 import data_seed_fakeRandomized from "./data.seed.fakeRandomized/data.seed.fakeRandomized"
+import { Command, getConnectionString_cli, runProgram } from "./cli.utils2"
+import { z } from "zod"
 
-type Params_base = Record<string, string> & { env?: Record<string, string> }
-
-type Context = {
-  api: Api_ss_type
-  db: Drizzle_instance
-  run(cmd: string): Promise<unknown>
-}
-
-type Command<
-  TName extends string,
-  TEnv extends Record<string, string>,
-  TParams extends Params_base,
-> = {
-  name: TName
-  params?: TParams
-  env?: TEnv
-  resolve(c: Context): Promise<unknown> | unknown
-}
-
-const commands = [
+const commands: Command<string, {}>[] = [
   {
     name: "dev",
-    resolve: ({ run }) => run("next dev"),
+    resolve: "next dev",
   },
   {
     name: "db.reset",
     resolve({ db, run }) {
-      run(`psql ${_getConnectionString()} --file=./db.truncate.sql`)
+      run(`psql ${getConnectionString_cli()} --file=./db.truncate.sql`)
       run("drizzle-kit push:pg --config=./drizzle/drizzle.config.ts")
       run("prisma db pull")
       run("prisma generate")
@@ -39,15 +19,11 @@ const commands = [
   },
   {
     name: "location.many",
-    resolve({ api }) {
-      const query = parsed._unknown?.[0] || ""
-      return api.location.many({ query }).then(console.log).catch(console.error)
+    params: z.object({ query: z.string() }),
+    resolve({ api, params }) {
+      return api.location.many({ query: params.query }).then(console.log).catch(console.error)
     },
   },
-] satisfies Command<string, {}, {}>[]
+]
 
-const parsed = null as any
-function _getConnectionString() {
-  const dbInstance = parsed["db-instance"] || "dev"
-  return getConnectionString(dbInstance)
-}
+runProgram({ commands })
