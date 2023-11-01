311a33ad7b8a74546dc1f3cc70095edfe96ce02c
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-11-01 19:29:40 +0100

    cli-better +

--- packages/@mk-platforma/app-web/cli2.ts
+++ packages/@mk-platforma/app-web/cli2.ts
@@ -1,5 +1,7 @@
 import { Api_ss, Api_ss_type } from "./api_/api.root"
 import drizzle_connect, { Drizzle_instance } from "~/drizzle/drizzle.instance"
+import { getConnectionString } from "./cli.utils"
+import data_seed_fakeRandomized from "./data.seed.fakeRandomized/data.seed.fakeRandomized"
 
 type Params_base = Record<string, string> & { env?: Record<string, string> }
 
@@ -23,14 +25,29 @@ type Command<
 const commands = [
   {
     name: "dev",
-    resolve() {},
+    resolve: ({ run }) => run("next dev"),
   },
   {
     name: "db.reset",
-    resolve() {},
+    resolve({ db, run }) {
+      run(`psql ${_getConnectionString()} --file=./db.truncate.sql`)
+      run("drizzle-kit push:pg --config=./drizzle/drizzle.config.ts")
+      run("prisma db pull")
+      run("prisma generate")
+      data_seed_fakeRandomized(db)
+    },
   },
   {
     name: "location.many",
-    resolve() {},
+    resolve({ api }) {
+      const query = parsed._unknown?.[0] || ""
+      return api.location.many({ query }).then(console.log).catch(console.error)
+    },
   },
 ] satisfies Command<string, {}, {}>[]
+
+const parsed = null as any
+function _getConnectionString() {
+  const dbInstance = parsed["db-instance"] || "dev"
+  return getConnectionString(dbInstance)
+}
