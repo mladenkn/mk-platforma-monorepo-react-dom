b198b5549f012c618404318196f3ef3fd64e8d6d
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-11-01 19:24:23 +0100

    cli-better +

--- packages/@mk-platforma/app-web/cli2.ts
+++ packages/@mk-platforma/app-web/cli2.ts
@@ -1,4 +1,13 @@
+import { Api_ss, Api_ss_type } from "./api_/api.root"
+import drizzle_connect, { Drizzle_instance } from "~/drizzle/drizzle.instance"
+
 type Params_base = Record<string, string> & { env?: Record<string, string> }
+
+type Context = {
+  api: Api_ss_type
+  db: Drizzle_instance
+  run(cmd: string): Promise<unknown>
+}
 
 type Command<
   TName extends string,
@@ -8,4 +17,20 @@ type Command<
   name: TName
   params?: TParams
   env?: TEnv
+  resolve(c: Context): Promise<unknown> | unknown
 }
+
+const commands = [
+  {
+    name: "dev",
+    resolve() {},
+  },
+  {
+    name: "db.reset",
+    resolve() {},
+  },
+  {
+    name: "location.many",
+    resolve() {},
+  },
+] satisfies Command<string, {}, {}>[]
