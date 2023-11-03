17fcb99e6b9a9c24d7d382ac736cc82575cb7fd2
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-11-03 18:23:22 +0100

    cli-better-5 +

--- packages/@mk-platforma/app-web/drizzle/drizzle.config.ts
+++ packages/@mk-platforma/app-web/drizzle/drizzle.config.ts
@@ -1,11 +1,11 @@
+import { asNonNil } from "@mk-libs/common/common"
 import type { Config } from "drizzle-kit"
-import { cli_getConnectionString } from "~/cli.utils"
 
 export default {
   schema: "./drizzle/drizzle.schema.ts",
   // out: "./drizzle",
   driver: "pg",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL || cli_getConnectionString("dev"),
+    connectionString: asNonNil(process.env.DATABASE_URL),
   },
 } satisfies Config
