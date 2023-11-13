aca64ee39766298b2828452846c7da162c4ed022
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-11-13 22:28:50 +0100

    data-seed-fakeRandomized-clean +

--- packages/@mk-platforma/app-web/framework/data.seed.lib.ts
+++ packages/@mk-platforma/app-web/framework/data.seed.lib.ts
@@ -1,20 +1,19 @@
 import { Drizzle_instance } from "~/drizzle/drizzle.instance"
+import fs from "fs"
+import path from "path"
 
 type Db = Drizzle_instance
 
 export default async function doCleanSeed_lib(db: Db, folderPath: string) {
-  const modules_paths = [
-    "/home/mladen/projekti/mk-platforma-monorepo-react-dom/packages/@mk-platforma/app-web/data.seed.fakeRandomized.clean/data.seed.fr.1.users.ts",
-    "/home/mladen/projekti/mk-platforma-monorepo-react-dom/packages/@mk-platforma/app-web/data.seed.fakeRandomized.clean/data.seed.fr1.categories.clean.ts",
-    "/home/mladen/projekti/mk-platforma-monorepo-react-dom/packages/@mk-platforma/app-web/data.seed.fakeRandomized.clean/data.seed.fr1.locations.ts",
-    "/home/mladen/projekti/mk-platforma-monorepo-react-dom/packages/@mk-platforma/app-web/data.seed.fakeRandomized.clean/data.seed.fr2.posts.ts",
-  ]
-  const a = modules_paths
+  const modules_paths = fs
+    .readdirSync(folderPath)
+    .map(file_name => path.join(folderPath, file_name))
+  const seeders = modules_paths
     .flatMap(path => Object.values(require(path)))
     .filter(sth => typeof sth === "function" && "dbSeeder" in sth)
     .map((sth: any) => {
       if (!sth.dbSeeder.order) sth.dbSeeder.order = Number.MAX_VALUE
       return sth
     })
-  console.log(13, a)
+  console.log(13, seeders)
 }
