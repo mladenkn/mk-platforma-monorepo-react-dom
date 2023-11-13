0c913f23b82f42439d63018b9d5a8598c321aae1
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-11-13 22:18:45 +0100

    data-seed-fakeRandomized-clean +

--- packages/@mk-platforma/app-web/framework/data.seed.lib.ts
+++ packages/@mk-platforma/app-web/framework/data.seed.lib.ts
@@ -2,4 +2,13 @@ import { Drizzle_instance } from "~/drizzle/drizzle.instance"
 
 type Db = Drizzle_instance
 
-export default async function doCleanSeed_lib(db: Db, folderPath: string) {}
+export default async function doCleanSeed_lib(db: Db, folderPath: string) {
+  const modules_paths = [
+    "/home/mladen/projekti/mk-platforma-monorepo-react-dom/packages/@mk-platforma/app-web/data.seed.fakeRandomized.clean/data.seed.fr.1.users.ts",
+    "/home/mladen/projekti/mk-platforma-monorepo-react-dom/packages/@mk-platforma/app-web/data.seed.fakeRandomized.clean/data.seed.fr1.categories.clean.ts",
+    "/home/mladen/projekti/mk-platforma-monorepo-react-dom/packages/@mk-platforma/app-web/data.seed.fakeRandomized.clean/data.seed.fr1.locations.ts",
+    "/home/mladen/projekti/mk-platforma-monorepo-react-dom/packages/@mk-platforma/app-web/data.seed.fakeRandomized.clean/data.seed.fr2.posts.ts",
+  ]
+  const a = modules_paths.flatMap(path => Object.values(require(path)))
+  console.log(13, a)
+}
