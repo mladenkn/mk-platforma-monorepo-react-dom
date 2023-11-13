a81d95d4a6710de32b2ba84224ae291c58f3527b
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-11-13 22:25:09 +0100

    data-seed-fakeRandomized-clean +

--- packages/@mk-platforma/app-web/data.seed.fakeRandomized.clean/data.seed.fr2.posts.ts
+++ packages/@mk-platforma/app-web/data.seed.fakeRandomized.clean/data.seed.fr2.posts.ts
@@ -2,4 +2,4 @@ import { Drizzle_instance } from "~/drizzle/drizzle.instance"
 
 export default function seedPosts(db: Drizzle_instance) {}
 
-seedPosts.dbSeeder = { order: 1 }
+seedPosts.dbSeeder = { order: 2 }
--- packages/@mk-platforma/app-web/framework/data.seed.lib.ts
+++ packages/@mk-platforma/app-web/framework/data.seed.lib.ts
@@ -9,6 +9,12 @@ export default async function doCleanSeed_lib(db: Db, folderPath: string) {
     "/home/mladen/projekti/mk-platforma-monorepo-react-dom/packages/@mk-platforma/app-web/data.seed.fakeRandomized.clean/data.seed.fr1.locations.ts",
     "/home/mladen/projekti/mk-platforma-monorepo-react-dom/packages/@mk-platforma/app-web/data.seed.fakeRandomized.clean/data.seed.fr2.posts.ts",
   ]
-  const a = modules_paths.flatMap(path => Object.values(require(path)))
+  const a = modules_paths
+    .flatMap(path => Object.values(require(path)))
+    .filter(sth => typeof sth === "function" && "dbSeeder" in sth)
+    .map((sth: any) => {
+      if (!sth.dbSeeder.order) sth.dbSeeder.order = Number.MAX_VALUE
+      return sth
+    })
   console.log(13, a)
 }
