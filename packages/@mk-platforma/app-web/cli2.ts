83f1708e0878998b380a5b204ac409fc3ae9dea6
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-11-13 17:25:54 +0100

    cli-better-5 +

--- packages/@mk-platforma/app-web/cli2.ts
+++ packages/@mk-platforma/app-web/cli2.ts
@@ -1,9 +1,21 @@
 import data_seed_fakeRandomized from "./data.seed.fakeRandomized/data.seed.fakeRandomized"
 import { cli_command_base, cli_run } from "./cli.utils2"
 import { z } from "zod"
-import { cli_createContext, cli_getConnectionString } from "./cli.utils"
+import { cli_getConnectionString } from "./cli.utils"
 import { location_api_google__search } from "./domain/Location.api.google"
 import { asString } from "@mk-libs/common/common"
+import drizzle_connect, { Drizzle_instance } from "~/drizzle/drizzle.instance"
+import { Api_ss } from "~/api_/api.root"
+import { eq } from "drizzle-orm"
+import { User } from "~/drizzle/drizzle.schema"
+
+async function api_create(db: Drizzle_instance) {
+  const user = await db.query.User.findFirst({ where: eq(User.canMutate, true) }).then(
+    u => u || { id: -1, canMutate: false, name: "" },
+  )
+  const apiContext = { user, getCookie: () => null, db }
+  return Api_ss(apiContext)
+}
 
 const command = cli_command_base({
   base_params: z.object({
@@ -12,8 +24,7 @@ const command = cli_command_base({
   async base_resolve({ dbInstance }) {
     const db_connectionString = cli_getConnectionString(dbInstance || "dev")
     process.env.DATABASE_URL = db_connectionString
-    const base = await cli_createContext()
-    return { ...base, db_connectionString }
+    return { db_connectionString }
   },
 })
 
@@ -28,11 +39,11 @@ const commands = [
       await run(`psql ${db_connectionString} --file=./db.truncate.sql`)
       await run("drizzle-kit push:pg --config=./drizzle/drizzle.config.ts")
     },
-    async resolve({ db, run }) {
+    async resolve({ run }) {
       await run("prisma db pull")
       await run("prisma generate")
       console.log("Seeding data")
-      await data_seed_fakeRandomized(db)
+      await data_seed_fakeRandomized(drizzle_connect())
     },
   }),
   command({
@@ -44,8 +55,10 @@ const commands = [
   command({
     name: "location.many",
     params: z.object({ query: z.string() }),
-    resolve: ({ api }, params) =>
-      api.location.many({ query: params.query }).then(console.log).catch(console.error),
+    resolve: async (_, params) => {
+      const api = await api_create(drizzle_connect())
+      return api.location.many({ query: params.query }).then(console.log).catch(console.error)
+    },
   }),
   command({
     name: "location.google.find",
