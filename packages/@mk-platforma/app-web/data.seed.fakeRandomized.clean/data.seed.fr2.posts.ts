45563be2ad44b692db63447d92d360086d7df265
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-11-13 22:53:58 +0100

    data-seed-fakeRandomized-clean +

--- packages/@mk-platforma/app-web/data.seed.fakeRandomized.clean/data.seed.fr2.posts.ts
+++ packages/@mk-platforma/app-web/data.seed.fakeRandomized.clean/data.seed.fr2.posts.ts
@@ -1,5 +1,57 @@
+import { faker } from "@faker-js/faker"
+import generatePosts from "~/data.seed.fakeRandomized/post.all.gen"
 import { Drizzle_instance } from "~/drizzle/drizzle.instance"
+import { Category, Location, User, Comment } from "~/drizzle/drizzle.schema"
+import { Api_ss } from "~/api_/api.root"
+import { eva } from "@mk-libs/common/common"
 
-export default function seedPosts(db: Drizzle_instance) {}
+export default async function seedPosts(db: Drizzle_instance) {
+  const categories = await db.select().from(Category)
+  const locations = await db.select().from(Location)
+  const users = await db.select().from(User)
+  const posts = faker.helpers.shuffle(generatePosts({ categories, locations }))
+  await _seedPosts(db, posts, users)
+}
 
 seedPosts.dbSeeder = { order: 2 }
+
+async function _seedPosts(
+  db: Drizzle_instance,
+  posts: ReturnType<typeof generatePosts>,
+  users: { id: number; name?: string | null }[],
+) {
+  for (const post of posts) {
+    const user = faker.helpers.arrayElement(users)
+
+    const api = Api_ss({
+      user: { id: user.id, name: user.name || "seed user", canMutate: true },
+      getCookie: (() => {}) as any,
+      db: db,
+    })
+
+    const images = await eva(async () => {
+      if (!post.images?.length) return undefined
+      const images_created = await api.image.create(post.images!)
+      const images_withIsMain = images_created.map(i => ({
+        ...i,
+        isMain: false,
+      }))
+      const mainImage = faker.helpers.arrayElement(images_withIsMain)
+      mainImage.isMain = true
+      return images_withIsMain
+    })
+
+    const post_created = await api.post.create({
+      ...post,
+      images,
+    })
+
+    for (const comment of post.comments) {
+      await db.insert(Comment).values({
+        content: comment.content,
+        post_id: post_created.id,
+        author_id: faker.helpers.arrayElement(users)!.id,
+      })
+    }
+  }
+}
