58997060a5745e011ba8f264cf844568794e3842
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-11-14 11:35:08 +0100

    data-seed-fakeRandomized-clean +

--- packages/@mk-platforma/app-web/data.seed.fakeRandomized.clean/data.seed.fr.locations.ts
+++ packages/@mk-platforma/app-web/data.seed.fakeRandomized.clean/data.seed.fr.locations.ts
@@ -2,9 +2,9 @@ import { Drizzle_instance } from "~/drizzle/drizzle.instance"
 import locations_json from "~/data.seed.common/data.locations.json"
 import { Location } from "~/domain/post/Post.schema"
 
-export default function seedLocations(db: Drizzle_instance) {
+export default async function seedLocations(db: Drizzle_instance) {
   const mapped = locations_json.map(l => ({ ...l, googleId: l.google_id }))
-  return db.insert(Location).values(mapped).returning() // TODO: fali onConflictDoUpdate
+  return await db.insert(Location).values(mapped).returning() // TODO: fali onConflictDoUpdate
 }
 
 seedLocations.dbSeeder = { order: 1 }
