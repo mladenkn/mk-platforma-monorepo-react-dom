6563cebd6a35be01ffc55f019622a0595add3d0a
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-11-14 11:45:00 +0100

    mk-libs -

--- packages/@mk-libs/react-common/types.ts
+++
@@ -1,7 +0,0 @@
-import { ComponentProps, JSXElementConstructor } from "react"
-import { Required } from "utility-types"
-
-export type OverridableProps<
-  TComponent extends keyof JSX.IntrinsicElements | JSXElementConstructor<any>,
-  TRequiredProps extends keyof ComponentProps<TComponent>,
-> = Required<Partial<ComponentProps<TComponent>>, TRequiredProps>
--- packages/@mk-libs/react-common/use.history.uniques.ts
+++ packages/@mk-libs/react-common/use.history.uniques.ts
@@ -1,19 +1,14 @@
-import { useRef } from "react"
-import useEffect from "./use-effect"
+import { useEffect, useRef } from "react"
 import { last } from "lodash"
 
 export default function use_history_uniques<T>(value: T) {
   const history = useRef<T[]>([])
   const history_lastItem = last(history.current)
-  useEffect(
-    () => {
-      if (history_lastItem !== value) {
-        history.current.push(value)
-      }
-    },
-    [value],
-    { runOnFirstRender: true },
-  )
+  useEffect(() => {
+    if (history_lastItem !== value) {
+      history.current.push(value)
+    }
+  }, [value])
   if (history_lastItem !== value) {
     return [...history.current, value]
   } else {
