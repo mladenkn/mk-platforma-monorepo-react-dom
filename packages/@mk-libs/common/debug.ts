8589702af7467644ae07891e1734101633aac157
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-11-14 21:45:26 +0100

    common/debug +

--- packages/@mk-libs/common/debug.ts
+++ packages/@mk-libs/common/debug.ts
@@ -33,10 +33,36 @@ import differenceInMilliseconds from "date-fns/differenceInMilliseconds"
 //   return new Error().stack?.split("\n")[2]
 // }
 
-export async function measurePerformance<T>(promise: () => Promise<T>) {
+export async function measurePerf_async<T>(promise: () => Promise<T>) {
   const begining = new Date()
   const result = await promise()
   const end = new Date()
   const difference = differenceInMilliseconds(end, begining)
   return [result, difference] as const
+}
+
+export function measurePerf<T>(func: () => T) {
+  const begining = new Date()
+  const result = func()
+  const end = new Date()
+  const difference = differenceInMilliseconds(end, begining)
+  return [result, difference] as const
+}
+
+export function withAsyncPerfLogging<T extends (...args: unknown[]) => Promise<unknown>>(func: T) {
+  return async (...args: unknown[]) => {
+    const [result, timeDiff] = await measurePerf(() => func(args))
+    // @ts-ignore
+    console.log(`Performance of ${func.name}: ${timeDiff}`)
+    return result
+  }
+}
+
+export function withPerfLogging<TResult, T extends (...args: unknown[]) => TResult>(func: T) {
+  return (...args: unknown[]): TResult => {
+    const [result, timeDiff] = measurePerf(() => func(args))
+    // @ts-ignore
+    console.log(`Performance of ${func.name}: ${timeDiff}`)
+    return result
+  }
 }
