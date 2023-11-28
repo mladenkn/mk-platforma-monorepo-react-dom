e5b9c76f7e72c96aaef202368c1bf8beceef2e4e
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-11-28 22:47:58 +0100

    data-seed-better +

--- packages/@mk-platforma/app-web/cli.ts
+++ packages/@mk-platforma/app-web/cli.ts
@@ -7,7 +7,7 @@ import drizzle_connect, { Drizzle_instance } from "~/drizzle/drizzle.instance"
 import { Api_ss } from "~/api_/api.root"
 import { eq } from "drizzle-orm"
 import { User } from "~/drizzle/drizzle.schema"
-import data_seed_fakeRandomized from "./data.seed.fakeRandomized/data.seed.fakeRandomized"
+import data_seed_fakeRandomized from "./data.seed.fakeRandomized/data.seed.fr"
 
 async function api_create(db: Drizzle_instance) {
   const user = await db.query.User.findFirst({ where: eq(User.canMutate, true) }).then(
