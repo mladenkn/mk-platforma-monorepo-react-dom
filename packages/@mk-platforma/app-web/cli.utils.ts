083ecb0033c891f42d4930e7aee19f1187507256
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2024-01-05 23:15:51 +0100

    env +

--- packages/@mk-platforma/app-web/cli.utils.ts
+++ packages/@mk-platforma/app-web/cli.utils.ts
@@ -88,22 +88,3 @@ async function cli_runCommand(command: string, env?: Record<string, string>) {
     })
   })
 }
-
-export function cli_getConnectionString(env: string) {
-  switch (env) {
-    case "dev":
-      return "postgresql://postgres:postgres@localhost:5432/za_brata"
-    case "test.local":
-      return "postgresql://postgres:postgres@localhost:5432/za_brata_test"
-    case "staging":
-      return "postgresql://postgres:E18xcmX5bA2y8pPu85KF@containers-us-west-116.railway.app:7999/railway"
-    case "prod":
-      return "postgres://default:dLkKj9hoY7WT@ep-long-rain-221377-pooler.us-east-1.postgres.vercel-storage.com/verceldb?pgbouncer=true&connect_timeout=15"
-    case "neon-main": // ovo je na vercelu
-      return "postgres://mladenkn:EWT4n5pMetaB@ep-shy-paper-831120.eu-central-1.aws.neon.tech/neondb?sslmode=require"
-    case "neon-staging":
-      return "postgres://mladenkn:EWT4n5pMetaB@ep-lingering-fog-424091.eu-central-1.aws.neon.tech/neondb?sslmode=require"
-    default:
-      throw new Error(`Unsupported env: ${env}`)
-  }
-}
