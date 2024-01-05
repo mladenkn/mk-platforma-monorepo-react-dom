aa0986ff8a8f6b672d22d58aff5ae88facd5c224
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2024-01-05 22:25:41 +0100

    env-next-config +

--- packages/@mk-platforma/app-web/domain/post/Post.listItem.tsx
+++ packages/@mk-platforma/app-web/domain/post/Post.listItem.tsx
@@ -1,7 +1,6 @@
 import { Box, Typography, useTheme, Paper } from "@mui/material"
 import LocationIcon from "@mui/icons-material/LocationOn"
 import React from "react"
-import Image from "next/image"
 
 type Post_common_listItem_props = {
   id: number
@@ -26,7 +25,7 @@ export function Post_listItem({ id, title, location, image }: Post_common_listIt
       }}
     >
       {image && (
-        <Image
+        <img
           src={image.url}
           width={100}
           height={100}
---
+++ packages/@mk-platforma/app-web/env.private.js
@@ -0,0 +1,3 @@
+module.exports.env = {
+  DATABASE_URL: "postgresql://postgres:postgres@localhost:5432/za_brata"
+}
